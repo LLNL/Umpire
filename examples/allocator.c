4e8b5358175ab7df3e9a5e925b80e454b2d46f70
David Beckingsale <davidbeckingsale@gmail.com>
2017-10-06 09:15:20 -0700

    Update C interface to support copy

--- examples/allocator.c
+++ examples/allocator.c
@@ -5,19 +5,33 @@ int main(int argc, char* argv[]) {
   UMPIRE_resourcemanager* rm = UMPIRE_resourcemanager_get();
   UMPIRE_allocator* allocator = UMPIRE_resourcemanager_get_allocator(rm, "HOST");
 
-  double* allocation;
+  double* alloc_one;
+  double* alloc_two;
 
-  printf("allocation = %x\n", allocation);
+  int ELEMS = 100;
+  int i;
 
   printf("Allocating memory on HOST...");
-  allocation = (double*) UMPIRE_allocator_allocate(allocator, sizeof(double)*100);
-  printf("done @%x.\n", allocation);
+  alloc_one = (double*) UMPIRE_allocator_allocate(allocator, sizeof(double)*ELEMS);
+  alloc_two = (double*) UMPIRE_allocator_allocate(allocator, sizeof(double)*ELEMS);
+
+  for (i = 0; i < ELEMS; i++) {
+    alloc_one[i] = 1.0*i;
+  }
+
+  UMPIRE_resourcemanager_copy(rm, alloc_one, alloc_two);
+
+  for (i = 0; i < ELEMS; i++) {
+    if (alloc_one[i] == alloc_two[i]) {
+    } else {
+      printf("Boooooo!\n");
+    }
+  }
 
   printf("Deallocating memory on HOST...");
-  UMPIRE_allocator_deallocate(allocator, allocation);
+  UMPIRE_allocator_deallocate(allocator, alloc_one);
+  UMPIRE_allocator_deallocate(allocator, alloc_two);
   printf("done.\n");
-
-  printf("allocation = %x\n", allocation);
 
   return 0;
 }
