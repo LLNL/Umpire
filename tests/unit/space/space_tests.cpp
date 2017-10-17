1d6f2fba6a89928a160d62dc94a61116f65001f4
David Beckingsale <davidbeckingsale@gmail.com>
2017-10-16 18:14:59 -0700

    Add introspection tests

--- tests/unit/space/space_tests.cpp
+++ tests/unit/space/space_tests.cpp
@@ -30,30 +30,30 @@ TEST(MemorySpace, AllocateDeallocate)
   ASSERT_NE(pointer, nullptr);
 }
 
-// TEST(MemorySpace, GetSize)
-// {
-//   auto alloc = std::shared_ptr<umpire::space::MemorySpace<TestAllocator> >();
-//   double* pointer = (double*)alloc->allocate(10);
-//   ASSERT_EQ(alloc->getCurrentSize(), 10);
-// 
-//   double* pointer_two = (double*)alloc->allocate(10);
-//   ASSERT_EQ(alloc->getCurrentSize(), 20);
-// 
-//   alloc->deallocate(pointer);
-//   ASSERT_EQ(alloc->getCurrentSize(), 10);
-// 
-//   alloc->deallocate(pointer_two);
-//   ASSERT_EQ(alloc->getCurrentSize(), 0);
-// }
-// 
-// TEST(MemorySpace, GetHighWatermark)
-// {
-//   auto alloc = std::shared_ptr<umpire::space::MemorySpace<TestAllocator> >();
-//   double* pointer = (double*)alloc->allocate(10*sizeof(double));
-//   double* pointer_two = (double*)alloc->allocate(30*sizeof(double));
-//   alloc->deallocate(pointer);
-// 
-//   ASSERT_EQ(alloc->getHighWatermark(), 40);
-// 
-//   alloc->deallocate(pointer_two);
-// }
+TEST(MemorySpace, GetSize)
+{
+  auto alloc = std::shared_ptr<umpire::space::MemorySpace<TestAllocator> >();
+  double* pointer = (double*)alloc->allocate(10);
+  ASSERT_EQ(alloc->getCurrentSize(), 10);
+
+  double* pointer_two = (double*)alloc->allocate(10);
+  ASSERT_EQ(alloc->getCurrentSize(), 20);
+
+  alloc->deallocate(pointer);
+  ASSERT_EQ(alloc->getCurrentSize(), 10);
+
+  alloc->deallocate(pointer_two);
+  ASSERT_EQ(alloc->getCurrentSize(), 0);
+}
+
+TEST(MemorySpace, GetHighWatermark)
+{
+  auto alloc = std::shared_ptr<umpire::space::MemorySpace<TestAllocator> >();
+  double* pointer = (double*)alloc->allocate(10*sizeof(double));
+  double* pointer_two = (double*)alloc->allocate(30*sizeof(double));
+  alloc->deallocate(pointer);
+
+  ASSERT_EQ(alloc->getHighWatermark(), 40);
+
+  alloc->deallocate(pointer_two);
+}
