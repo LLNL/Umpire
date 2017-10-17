bde1957d5330a2ec7e4076adb0f1a706f74d9783
David Beckingsale <davidbeckingsale@gmail.com>
2017-10-16 18:22:20 -0700

    Fixup tests for current size and high watermark

--- tests/unit/space/space_tests.cpp
+++ tests/unit/space/space_tests.cpp
@@ -32,8 +32,8 @@ TEST(MemorySpace, AllocateDeallocate)
 
 TEST(MemorySpace, GetSize)
 {
-  auto alloc = std::shared_ptr<umpire::space::MemorySpace<TestAllocator> >();
-  double* pointer = (double*)alloc->allocate(10);
+  auto alloc = std::make_shared<umpire::space::MemorySpace<TestAllocator> >();
+  double* pointer = (double*) alloc->allocate(10);
   ASSERT_EQ(alloc->getCurrentSize(), 10);
 
   double* pointer_two = (double*)alloc->allocate(10);
@@ -48,9 +48,12 @@ TEST(MemorySpace, GetSize)
 
 TEST(MemorySpace, GetHighWatermark)
 {
-  auto alloc = std::shared_ptr<umpire::space::MemorySpace<TestAllocator> >();
-  double* pointer = (double*)alloc->allocate(10*sizeof(double));
-  double* pointer_two = (double*)alloc->allocate(30*sizeof(double));
+  auto alloc = std::make_shared<umpire::space::MemorySpace<TestAllocator> >();
+  ASSERT_EQ(alloc->getHighWatermark(), 0);
+
+  double* pointer = (double*)alloc->allocate(10);
+  double* pointer_two = (double*)alloc->allocate(30);
+
   alloc->deallocate(pointer);
 
   ASSERT_EQ(alloc->getHighWatermark(), 40);
