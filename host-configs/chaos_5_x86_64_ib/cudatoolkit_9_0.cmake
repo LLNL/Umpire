751550aca512d59e3ac80ea913faddd0ec880466
David Beckingsale <davidbeckingsale@gmail.com>
2017-11-09 08:30:16 -0800

    Include CUDA config for gcc

--- host-configs/chaos_5_x86_64_ib/cudatoolkit_9_0.cmake
+++
@@ -1,1 +0,0 @@
-set (CUDA_TOOLKIT_ROOT_DIR "/opt/cudatoolkit-9.0" CACHE PATH "")
--- host-configs/chaos_5_x86_64_ib/gcc_4_9_3.cmake
+++ host-configs/chaos_5_x86_64_ib/gcc_4_9_3.cmake
@@ -1,3 +1,3 @@
 set(CMAKE_CXX_COMPILER "/usr/apps/gnu/4.9.3/bin/g++" CACHE PATH "")
 
-include(${CMAKE_CURRENT_LIST_DIR}/cudatoolkit_9_0.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/cudatoolkit_8_0.cmake)
