8df6c849bc6fc0c22cfdc353b1bf6521694903f4
Marty McFadden <mcfadden8@llnl.gov>
2018-01-11 15:12:39 -0800

    Added support for SLIC

--- src/util/Logger.cpp
+++ src/util/Logger.cpp
@@ -1,22 +1,11 @@
 #include "umpire/util/Logger.hpp"
 
 #include <iostream>   // for std::cout, std::cerr
-#include <stdlib.h>   // for getenv()
-#include <strings.h>  // for strcasecmp()
 
 namespace umpire {
 namespace util {
 
-static const char* env_name = "UMPIRE_LOG_LEVEL";
-static message::Level defaultLevel = message::Info;
 Logger* Logger::s_Logger = nullptr;
-
-static const std::string MessageLevelName[ message::Num_Levels ] = {
-  "ERROR",
-  "WARNING",
-  "INFO",
-  "DEBUG"
-};
 
 Logger::Logger()
 {
@@ -44,7 +33,7 @@ void Logger::logMessage( message::Level level,
     return;   /* short-circuit */
 
   std::cout 
-    << "[" << MessageLevelName[ level ] << "]"
+    << "[" << message::MessageLevelName[ level ] << "]"
     << "[" << fileName  << ":" << line << "]:" 
     << message 
     << std::endl;
@@ -55,20 +44,7 @@ void Logger::initialize()
   if ( s_Logger != nullptr )
     return;
 
-  message::Level level = defaultLevel;
-  char* enval = getenv(env_name);
-
-  if ( enval != NULL ) {
-    for ( int i = 0; i < message::Num_Levels; ++i ) {
-      if ( strcasecmp( enval, MessageLevelName[ i ].c_str() ) == 0 ) {
-        level = (message::Level)i;
-        break;
-      }
-    }
-  }
-
   s_Logger = new Logger();
-  s_Logger->setLoggingMsgLevel(level);
 }
 
 void Logger::finalize()
@@ -79,9 +55,6 @@ void Logger::finalize()
 
 Logger* Logger::getActiveLogger()
 {
-  if ( s_Logger == nullptr )
-    Logger::initialize();
-
   return s_Logger;
 }
 
--- src/util/Macros.hpp
+++ src/util/Macros.hpp
@@ -20,19 +20,64 @@
 #define UMPIRE_LOG( lvl, msg )
 
 #else
+#include <stdlib.h>
+#include <strings.h>
+
+#ifdef USE_SLIC_FOR_UMPIRE_LOG
+#include "slic/Logger.hpp"
+#define UMPIRE_LOG( lvl, msg )                                                                \
+{                                                                                             \
+  axom::slic::Logger* plog = axom::slic::Logger::getActiveLogger();                           \
+  if ( plog == nullptr ) {                                                                    \
+    axom::slic::Logger::initialize();                                                         \
+    plog = axom::slic::Logger::getActiveLogger();                                             \
+    axom::slic::message::Level level;                                                         \
+    level = axom::slic::message::Level::Error;                                                \
+    char* enval = getenv("UMPIRE_LOG_LEVEL");                                                 \
+    if ( enval != NULL ) {                                                                    \
+      for ( int i = 0; i < axom::slic::message::Level::Num_Levels; ++i ) {                    \
+        if ( strcasecmp( enval, axom::slic::message::MessageLevelName[ i ].c_str() ) == 0 ) { \
+          level = (axom::slic::message::Level)i;                                              \
+          break;                                                                              \
+        }                                                                                     \
+      }                                                                                       \
+    }                                                                                         \
+    plog->setLoggingMsgLevel(level);                                                          \
+  }                                                                                           \
+  std::ostringstream local_msg;                                                               \
+  local_msg  << " " << __func__ << " " << msg;                                                \
+  plog->logMessage( axom::slic::message::lvl, local_msg.str(),                                \
+                    std::string(__FILE__), __LINE__);                                         \
+}
+
+#else
 
 #include "umpire/util/Logger.hpp"
-
-#define UMPIRE_LOG( lvl, msg )                                     \
-{                                                                  \
-  std::ostringstream local_msg;                                    \
-  local_msg  << " " << __func__ << " " << msg,                     \
-  umpire::util::Logger::getActiveLogger()->logMessage(             \
-      umpire::util::message::lvl,                                  \
-      local_msg.str(),                                             \
-      std::string(__FILE__),                                       \
-      __LINE__);                                                   \
+#define UMPIRE_LOG( lvl, msg )                                                                  \
+{                                                                                               \
+  umpire::util::Logger* plog = umpire::util::Logger::getActiveLogger();                         \
+  if ( plog == nullptr ) {                                                                      \
+    umpire::util::Logger::initialize();                                                         \
+    plog = umpire::util::Logger::getActiveLogger();                                             \
+    umpire::util::message::Level level;                                                         \
+    level = umpire::util::message::Level::Error;                                                \
+    char* enval = getenv("UMPIRE_LOG_LEVEL");                                                   \
+    if ( enval != NULL ) {                                                                      \
+      for ( int i = 0; i < umpire::util::message::Level::Num_Levels; ++i ) {                    \
+        if ( strcasecmp( enval, umpire::util::message::MessageLevelName[ i ].c_str() ) == 0 ) { \
+          level = (umpire::util::message::Level)i;                                              \
+          break;                                                                                \
+        }                                                                                       \
+      }                                                                                         \
+    }                                                                                           \
+    plog->setLoggingMsgLevel(level);                                                            \
+  }                                                                                             \
+  std::ostringstream local_msg;                                                                 \
+  local_msg  << " " << __func__ << " " << msg;                                                  \
+  plog->logMessage( umpire::util::message::lvl, local_msg.str(),                                \
+                    std::string(__FILE__), __LINE__);                                           \
 }
+#endif //
 
 #endif // defined(NDEBUG)
 
