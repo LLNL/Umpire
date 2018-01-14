0631a780fa3a4dd024d7942d383d5cbd16ff0f94
Marty McFadden <mcfadden8@llnl.gov>
2018-01-14 10:06:31 -0800

    Associating cerr output stream with SLIC log messages

--- src/util/Macros.hpp
+++ src/util/Macros.hpp
@@ -21,20 +21,23 @@
 #define UMPIRE_LOG( lvl, msg )
 
 #else
-#include <stdlib.h>
-#include <strings.h>
-
 #ifdef UMPIRE_ENABLE_SLIC
+#include <stdlib.h>   // for getenv()
+#include <strings.h>  // for strcasecmp()
+#include <string>
+
 #include "slic/Logger.hpp"
+#include "slic/GenericOutputStream.hpp"
 #define UMPIRE_LOG( lvl, msg )                                                                \
 {                                                                                             \
   axom::slic::Logger* plog = axom::slic::Logger::getActiveLogger();                           \
   if ( plog == nullptr ) {                                                                    \
+    static const std::string env_name = "UMPIRE_LOG_LEVEL";                                   \
     axom::slic::Logger::initialize();                                                         \
     plog = axom::slic::Logger::getActiveLogger();                                             \
     axom::slic::message::Level level;                                                         \
     level = axom::slic::message::Level::Error;                                                \
-    char* enval = getenv("UMPIRE_LOG_LEVEL");                                                 \
+    char* enval = getenv(env_name.c_str());                                                   \
     if ( enval != NULL ) {                                                                    \
       for ( int i = 0; i < axom::slic::message::Level::Num_Levels; ++i ) {                    \
         if ( strcasecmp( enval, axom::slic::message::MessageLevelName[ i ].c_str() ) == 0 ) { \
@@ -44,6 +47,12 @@
       }                                                                                       \
     }                                                                                         \
     plog->setLoggingMsgLevel(level);                                                          \
+                                                                                              \
+    std::string console_format = std::string("[<LEVEL>][<FILE>:<LINE>]: <MESSAGE>\n");        \
+    axom::slic::LogStream* console =                                                          \
+      new axom::slic::GenericOutputStream( &std::cerr, console_format );                      \
+    plog->addStreamToAllMsgLevels( console );                                                 \
+                                                                                              \
   }                                                                                           \
   std::ostringstream local_msg;                                                               \
   local_msg  << " " << __func__ << " " << msg;                                                \
