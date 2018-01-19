7f74605f8b550e2698909969bc1719d113dee9a0
Marty McFadden <mcfadden8@llnl.gov>
2018-01-19 13:04:20 -0800

    Added error checks in Cudo copy operations

--- src/op/CudaCopyOperation.cpp
+++ src/op/CudaCopyOperation.cpp
@@ -1,4 +1,5 @@
 #include "CudaCopyOperation.hpp"
+#include "umpire/util/Macros.hpp"
 
 #include <cuda_runtime_api.h>
 
@@ -10,7 +11,14 @@ void CudaCopyOperation::operator()(
     void* dest_ptr,
     size_t length)
 {
-  ::cudaMemcpy(dest_ptr, src_ptr, length, cudaMemcpyDeviceToDevice);
+  cudaError_t error = ::cudaMemcpy(dest_ptr, src_ptr, length, cudaMemcpyDeviceToDevice);
+
+  if (error != cudaSuccess) {
+    UMPIRE_ERROR("cudaMemcpy( dest_ptr = " << dest_ptr
+      << ", src_ptr = " << src_ptr
+      << ", length = " << length
+      << ", cudaMemcpyDeviceToDevice ) failed with error: " << cudaGetErrorString(error));
+  }
 }
 
 } // end of namespace op
