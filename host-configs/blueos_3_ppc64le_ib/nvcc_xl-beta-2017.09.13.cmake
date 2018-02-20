0c188a30657515f5ad5fe90b5a3800d11c4a20b3
David Beckingsale <davidbeckingsale@gmail.com>
2018-02-20 11:46:06 -0800

    Add explicit Fortran library rpath for xlf

--- host-configs/blueos_3_ppc64le_ib/nvcc_xl-beta-2017.09.13.cmake
+++ host-configs/blueos_3_ppc64le_ib/nvcc_xl-beta-2017.09.13.cmake
@@ -1,3 +1,4 @@
 set(CMAKE_CXX_COMPILER "/usr/tce/packages/xl/xl-beta-2017.09.13/bin/xlC_r" CACHE PATH "")
 set(CMAKE_C_COMPILER "/usr/tce/packages/xl/xl-beta-2017.09.13/bin/xlc_r" CACHE PATH "")
-set(CMAKE_C_COMPILER "/usr/tce/packages/xl/xl-beta-2017.09.13/bin/xlf2003_r" CACHE PATH "")
+
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/xl/xl-beta-2017.09.13/bin/xlf2003_r" CACHE PATH "")
