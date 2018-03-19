175ee0fa2c0a81f155408104a957994c90607332
David Beckingsale <davidbeckingsale@gmail.com>
2018-03-19 16:33:23 -0700

    Add license header to host-configs and scripts

--- host-configs/chaos_5_x86_64_ib/clang_3_9_1.cmake
+++ host-configs/chaos_5_x86_64_ib/clang_3_9_1.cmake
@@ -1,3 +1,17 @@
+##############################################################################
+# Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory
+#
+# Created by David Beckingsale, david@llnl.gov
+# LLNL-CODE-747640
+#
+# All rights reserved.
+#
+# This file is part of Umpire.
+#
+# For details, see https://github.com/LLNL/Umpire
+# Please also see the LICENSE file for MIT license.
+##############################################################################
 set(CMAKE_CXX_COMPILER "/usr/global/tools/clang/chaos_5_x86_64_ib/clang-3.9.1/bin/clang++" CACHE PATH "")
 set(CMAKE_C_COMPILER "/usr/global/tools/clang/chaos_5_x86_64_ib/clang-3.9.1/bin/clang" CACHE PATH "")
 set(CMAKE_Fortran_COMPILER "/usr/apps/gnu/4.9.3/bin/gfortran" CACHE PATH "")
