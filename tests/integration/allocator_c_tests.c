09d0c0a3dc0557f95f4cda1dfa74f81dd6d66757
Marty McFadden <mcfadden8@llnl.gov>
2018-04-17 11:51:25 -0700

    Added first integration test for C interface.  Using ENABLE_C within cmake system in addition to ENABLE_FORTRAN

---
+++ tests/integration/allocator_c_tests.c
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory
+//
+// Created by David Beckingsale, david@llnl.gov
+// LLNL-CODE-747640
+//
+// All rights reserved.
+//
+// This file is part of Umpire.
+//
+// For details, see https://github.com/LLNL/Umpire
+// Please also see the LICENSE file for MIT license.
+//////////////////////////////////////////////////////////////////////////////
+#include "gtest/gtest.h"
+
+#include "umpire/umpire.h"
+#include "umpire/config.hpp"
+
+TEST(Allocator, HostAllocator)
+{
+  UMPIRE_resourcemanager* rm = UMPIRE_resourcemanager_get();
+  UMPIRE_allocator* allocator = UMPIRE_resourcemanager_get_allocator(rm, "HOST");
+
+  double* test_alloc = (double*) UMPIRE_allocator_allocate(allocator, 100*sizeof(double));
+
+  ASSERT_NE(nullptr, test_alloc);
+  UMPIRE_allocator_deallocate(allocator, test_alloc);
+}
