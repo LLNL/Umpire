1ac847054aa4afca58cc1b77b8f00ce864768ad9
David Beckingsale <davidbeckingsale@gmail.com>
2018-06-01 13:40:04 -0700

    Set CMAKE_CUDA_COMPILER in host-configs

--- host-configs/toss_3_x86_64_ib/cudatoolkit_9_1.cmake
+++ host-configs/toss_3_x86_64_ib/cudatoolkit_9_1.cmake
@@ -14,3 +14,5 @@
 ##############################################################################
 
 set (CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-9.1.85" CACHE PATH "")
+
+set (CMAKE_CUDA_COMPILER "/usr/tce/packages/cuda/cuda-9.1.85/bin/nvcc" CACHE PATH "")
