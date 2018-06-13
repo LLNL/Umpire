5b799e20a74ccf0df1e732d1821d6b808c47379d
David Beckingsale <davidbeckingsale@gmail.com>
2018-06-13 11:14:41 -0700

    Add operations for calling CudaMemAdvise

---
+++ src/op/CudaAdvisePreferredLocation.cpp
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory
+//
+// Created by David Beckingsale, david@llnl.gov
+// LLNL-CODE-747640
+//
+// All rights reserved.
+//
+// This file is part of Umpire.
+//
+// For details, see https://github.com/LLNL/Umpire
+// Please also see the LICENSE file for MIT license.
+//////////////////////////////////////////////////////////////////////////////
+#include "umpire/op/CudaAdvisePreferredLocation.hpp"
+
+#include <cuda_runtime_api.h>
+
+namespace umpire {
+namespace op {
+
+void
+CudaAdvisePreferredLocation::apply(
+    void* src_ptr,
+    util::AllocationRecord *src_allocation,
+    int val,
+    size_t length)
+{
+  // TODO: get correct device for allocation
+  cudaMemAdvise(src_ptr, cudaMemAdviseSetPreferredLocation, 0);
+}
+
+} // end of namespace op
+} // end of namespace umpire
+
+#endif // UMPIRE_CudaAdvisePreferredLocation_HPP
