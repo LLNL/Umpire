c7b1e1b3c3b129da5ec7ff74fb03a313957d3de9
David Beckingsale <davidbeckingsale@gmail.com>
2018-08-07 08:33:52 -0700

    Bump version number to 0.2.3

--- scripts/make_release_tarball.sh
+++ scripts/make_release_tarball.sh
@@ -15,7 +15,7 @@
 ##############################################################################
 
 TAR_CMD=gtar
-VERSION=0.2.2
+VERSION=0.2.3
 
 git archive --prefix=umpire-${VERSION}/ -o umpire-${VERSION}.tar HEAD 2> /dev/null
 
