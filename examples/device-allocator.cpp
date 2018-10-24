4830de481c9d60e0d180774089382905ed7d6399
David Beckingsale <davidbeckingsale@gmail.com>
2018-10-23 17:08:52 -0700

    Fixup example

--- examples/device-allocator.cpp
+++ examples/device-allocator.cpp
@@ -2,9 +2,9 @@
 #include "umpire/DeviceAllocator.hpp"
 
 __global__
-void my_kernel(umpire::DeviceAllocator alloc, void** data_ptr) {
-  if (threadId.x == 0) {
-    double* data = alloc.allocate(10*sizeof(double));
+void my_kernel(umpire::DeviceAllocator alloc, double** data_ptr) {
+  if (threadIdx.x == 0) {
+    double* data = static_cast<double*>(alloc.allocate(10*sizeof(double)));
     *data_ptr = data;
 
     data[7] = 1024;
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[]) {
   auto allocator = rm.getAllocator("UM");
   auto device_allocator = umpire::DeviceAllocator(allocator, 1024);
 
-  double** ptr_to_data = allocator.allocate(sizeof(double*));
+  double** ptr_to_data = static_cast<double**>(allocator.allocate(sizeof(double*)));
 
   my_kernel<<<1, 16>>>(device_allocator, ptr_to_data);
 
