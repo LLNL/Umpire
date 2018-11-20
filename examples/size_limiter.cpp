3974fac1ce4f63c3e0a0f1fd2e104ee4bc1ff84c
David Beckingsale <davidbeckingsale@gmail.com>
2018-11-20 12:45:35 -0800

    Update example to show how to make size-limited pool

--- examples/size_limiter.cpp
+++ examples/size_limiter.cpp
@@ -16,14 +16,19 @@
 
 #include "umpire/ResourceManager.hpp"
 #include "umpire/strategy/SizeLimiter.hpp"
+#include "umpire/strategy/DynamicPool.hpp"
 
 int main(int, char**)
 {
   auto& rm = umpire::ResourceManager::getInstance();
-  auto alloc = rm.makeAllocator<umpire::strategy::SizeLimiter>(
-      "size_limited_alloc", rm.getAllocator("HOST"), 64);
+  auto size_limited_alloc = rm.makeAllocator<umpire::strategy::SizeLimiter>(
+      "size_limited_alloc", rm.getAllocator("HOST"), 1024);
 
-  void* data = alloc.allocate(1024);
+  auto pool = rm.makeAllocator<umpire::strategy::DynamicPool>(
+      "pool", size_limited_alloc, 64, 64);
+
+  // This will throw an exception because the pool is limited to 1024 bytes.
+  void* data = pool.allocate(2048);
 
   return 0;
 }
