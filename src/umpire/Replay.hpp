3eb6d5ccf18024fff129975e3ca3c2a59ee2cfa2
Marty McFadden <mcfadden8@llnl.gov>
2019-01-15 16:18:54 -0600

    First attempt to get it to compile (spoiler alert, it does not)

--- src/umpire/Replay.hpp
+++ src/umpire/Replay.hpp
@@ -44,7 +44,8 @@ public:
   template <typename T, typename... Args>
   static std::string printReplayAllocator(T&& firstArg, Args&&... args) {
     std::stringstream ss;
-    ss << "," << firstArg << printReplayAllocator(std::forward<Args>(args)...);
+    ss << ",";
+    ss << firstArg << printReplayAllocator(std::forward<Args>(args)...);
     return ss.str();
   }
 private:
