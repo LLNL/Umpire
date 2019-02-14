f3d54f41b3f5841e2dce6b5088d7313a847452f3
Johann Dahm <johann.dahm@gmail.com>
2019-02-14 15:58:55 -0800

    Add findAllocationRecord to ResourceManager

--- tests/unit/resource_manager_tests.cpp
+++ tests/unit/resource_manager_tests.cpp
@@ -22,3 +22,22 @@ TEST(ResourceManager, Constructor) {
   (void) rm;
   SUCCEED();
 }
+
+TEST(ResourceManager, findAllocationRecord)
+{
+  auto& rm = umpire::ResourceManager::getInstance();
+
+  auto alloc = rm.getAllocator("HOST");
+
+  const size_t size = 10 * sizeof(double);
+  const size_t offset = 2 * sizeof(double); // < size
+
+  double* ptr = static_cast<double*>(alloc.allocate(size));
+  const umpire::util::AllocationRecord* rec = rm.findAllocationRecord(ptr + offset);
+
+  ASSERT_THROW(rm.findAllocationRecord(nullptr), umpire::util::Exception);
+
+  ASSERT_EQ(ptr, rec->m_ptr);
+
+  alloc.deallocate(ptr);
+}
