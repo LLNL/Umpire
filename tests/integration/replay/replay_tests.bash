8df0ac535ee65244268ad55f0679af0ef85ba1a6
David Beckingsale <davidbeckingsale@gmail.com>
2019-02-25 09:39:09 -0800

    Add missing license info

--- tests/integration/replay/replay_tests.bash
+++ tests/integration/replay/replay_tests.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
+##############################################################################
+# Copyright (c) 2018-2019, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory
+#
+# Created by David Beckingsale, david@llnl.gov
+# LLNL-CODE-747640
+#
+# All rights reserved.
+#
+# This file is part of Umpire.
+#
+# For details, see https://github.com/LLNL/Umpire
+# Please also see the LICENSE file for MIT license.
+##############################################################################
 replay_tests_dir=$1
 tools_dir=$2
 testprogram=$replay_tests_dir/replay_tests
