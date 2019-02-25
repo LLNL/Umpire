2938d881fe923b8474a094cf3e375087790ff2a3
David Beckingsale <davidbeckingsale@gmail.com>
2019-02-25 09:30:26 -0800

    Update version numbers

--- scripts/make_release_tarball.sh
+++ scripts/make_release_tarball.sh
@@ -15,7 +15,7 @@
 ##############################################################################
 
 TAR_CMD=gtar
-VERSION=0.3.1
+VERSION=0.3.2
 
 git archive --prefix=umpire-${VERSION}/ -o umpire-${VERSION}.tar HEAD 2> /dev/null
 
