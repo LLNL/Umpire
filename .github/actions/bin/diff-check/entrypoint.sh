77af8c2dc17d790884613ee5fd26c89e710c1f01
David Beckingsale <davidbeckingsale@gmail.com>
2019-04-03 13:11:16 -0700

    Exit with code 255

--- .github/actions/bin/diff-check/entrypoint.sh
+++ .github/actions/bin/diff-check/entrypoint.sh
@@ -4,7 +4,7 @@ for var in "$@"; do
     echo "Running git diff for $var'..."
     if sh -c "git diff --exit-code $var"; then
         echo "$var wasn't changed, failing!"
-        exit -1
+        exit 255 
     else
         exit_code=$?
         echo "$var was changed, passing!"
