039eb603559088d2f85587fddcce026464fa0a37
David Beckingsale <davidbeckingsale@gmail.com>
2019-04-05 10:27:32 -0700

    Only run modernize checks

--- .github/actions/clang-tidy/entrypoint.sh
+++ .github/actions/clang-tidy/entrypoint.sh
@@ -6,4 +6,23 @@ mkdir build && cd build
 
 cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DENABLE_CUDA=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=On ..
 
-/run-clang-tidy.py -j 8 -header-filter='.*' -checks='*' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-avoid-bind' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-deprecated-headers' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-loop-convert' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-make-shared' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-make-unique' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-pass-by-value' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-raw-string-literal' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-redundant-void-arg' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-replace-auto-ptr' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-shrink-to-fit' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-auto' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-bool-literals' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-default-member-init' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-emplace' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-equals-default' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-equals-delete' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-nullptr' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-override' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-transparent-functors' -fix
+/run-clang-tidy.py -header-filter='.*' -checks='-*,modernize-use-using' -fix
