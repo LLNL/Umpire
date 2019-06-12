e5f55e74f768fb4013a6247bfaaf7de88a14297d
David Beckingsale <davidbeckingsale@gmail.com>
2019-06-12 14:22:25 -0700

    Add make_unique header

---
+++ src/umpire/util/make_unique.hpp
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018-2019, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory
+//
+// Created by David Beckingsale, david@llnl.gov
+// LLNL-CODE-747640
+//
+// All rights reserved.
+//
+// This file is part of Umpire.
+//
+// For details, see https://github.com/LLNL/Umpire
+// Please also see the LICENSE file for MIT license.
+//////////////////////////////////////////////////////////////////////////////
+#ifndef UMPIRE_make_unique_HPP
+#define UMPIRE_make_unique_HPP
+
+#include <memory>
+
+namespace umpire {
+namespace util {
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+} // end of namespace util
+} // end of namespace umpire
+
+#endif
