8aa7437eae4818cab9a58273e6174b80af7dbd77
David Beckingsale <davidbeckingsale@gmail.com>
2019-06-12 15:31:06 -0700

    Don't show all leak kinds

--- cmake/SetupCMakeBasics.cmake
+++ cmake/SetupCMakeBasics.cmake
@@ -1,5 +1,5 @@
 if(ENABLE_TESTS)
-    set(MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --gen-suppressions=all --show-leak-kinds=all")
+    set(MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --gen-suppressions=all")
     file(TO_CMAKE_PATH "${CMAKE_SOURCE_DIR}/cmake/valgrind.supp" MEMORYCHECK_SUPPRESSIONS_FILE)
     include(CTest)
     message(STATUS "Memcheck suppressions file: ${MEMORYCHECK_SUPPRESSIONS_FILE}")
