824ebaf05f9e39df49c41e9201dbf3b8e27f6b63
Johann Dahm <johann.dahm@ibm.com>
2019-07-22 20:33:35 -0700

    Format lines

--- src/umpire/util/AllocationMap.cpp
+++ src/umpire/util/AllocationMap.cpp
@@ -36,7 +36,8 @@ AllocationMap::RecordList::~RecordList()
   }
 }
 
-void AllocationMap::RecordList::push_back(const AllocationRecord& rec)
+void
+AllocationMap::RecordList::push_back(const AllocationRecord& rec)
 {
   RecordBlock* curr = static_cast<RecordBlock*>(m_map.m_block_pool.allocate());
   curr->prev = m_tail;
@@ -45,7 +46,8 @@ void AllocationMap::RecordList::push_back(const AllocationRecord& rec)
   m_length++;
 }
 
-AllocationRecord AllocationMap::RecordList::pop_back()
+AllocationRecord
+AllocationMap::RecordList::pop_back()
 {
   if (m_length == 0) {
     UMPIRE_ERROR("pop_back() called, but m_length == 0");
@@ -64,32 +66,55 @@ AllocationRecord AllocationMap::RecordList::pop_back()
   return ret;
 }
 
-AllocationMap::RecordList::ConstIterator AllocationMap::RecordList::begin() const
+AllocationMap::RecordList::ConstIterator
+AllocationMap::RecordList::begin() const
 {
   return AllocationMap::RecordList::ConstIterator{this, iterator_begin{}};
 }
 
-AllocationMap::RecordList::ConstIterator AllocationMap::RecordList::end() const
+AllocationMap::RecordList::ConstIterator
+AllocationMap::RecordList::end() const
 {
   return AllocationMap::RecordList::ConstIterator{this, iterator_end{}};
 }
 
-std::size_t AllocationMap::RecordList::size() const { return m_length; }
-bool AllocationMap::RecordList::empty() const { return size() == 0; }
-AllocationRecord* AllocationMap::RecordList::back() { return &m_tail->rec; }
-const AllocationRecord* AllocationMap::RecordList::back() const { return &m_tail->rec; }
+std::size_t
+AllocationMap::RecordList::size() const
+{
+  return m_length;
+}
+
+bool
+AllocationMap::RecordList::empty() const
+{
+  return size() == 0;
+}
+
+AllocationRecord*
+AllocationMap::RecordList::back()
+{
+  return &m_tail->rec;
+}
+
+const AllocationRecord*
+AllocationMap::RecordList::back() const
+{
+  return &m_tail->rec;
+}
 
 AllocationMap::RecordList::ConstIterator::ConstIterator()
   : m_list(nullptr), m_curr(nullptr)
 {
 }
 
-AllocationMap::RecordList::ConstIterator::ConstIterator(const RecordList* list, iterator_begin)
+AllocationMap::RecordList::ConstIterator::ConstIterator(
+  const RecordList* list, iterator_begin)
   : m_list(list), m_curr(m_list->m_tail)
 {
 }
 
-AllocationMap::RecordList::ConstIterator::ConstIterator(const RecordList* list, iterator_end)
+AllocationMap::RecordList::ConstIterator::ConstIterator(
+  const RecordList* list, iterator_end)
   : m_list(list), m_curr(nullptr)
 {
 }
@@ -107,26 +132,32 @@ AllocationMap::RecordList::ConstIterator::operator->()
   return &m_curr->rec;
 }
 
-AllocationMap::RecordList::ConstIterator& AllocationMap::RecordList::ConstIterator::operator++()
+AllocationMap::RecordList::ConstIterator&
+AllocationMap::RecordList::ConstIterator::operator++()
 {
   if (!m_curr) UMPIRE_ERROR("Cannot dereference nullptr");
   m_curr = m_curr->prev;
   return *this;
 }
 
-AllocationMap::RecordList::ConstIterator AllocationMap::RecordList::ConstIterator::operator++(int)
+AllocationMap::RecordList::ConstIterator
+AllocationMap::RecordList::ConstIterator::operator++(int)
 {
   ConstIterator tmp{*this};
   this->operator++();
   return tmp;
 }
 
-bool AllocationMap::RecordList::ConstIterator::operator==(const AllocationMap::RecordList::ConstIterator& other) const
+bool
+AllocationMap::RecordList::ConstIterator::operator==(
+  const AllocationMap::RecordList::ConstIterator& other) const
 {
   return m_list == other.m_list && m_curr == other.m_curr;
 }
 
-bool AllocationMap::RecordList::ConstIterator::operator!=(const AllocationMap::RecordList::ConstIterator& other) const
+bool
+AllocationMap::RecordList::ConstIterator::operator!=(
+  const AllocationMap::RecordList::ConstIterator& other) const
 {
   return !(*this == other);
 }
@@ -140,7 +171,8 @@ AllocationMap::AllocationMap() :
 {
 }
 
-void AllocationMap::insert(void* ptr, AllocationRecord record)
+void
+AllocationMap::insert(void* ptr, AllocationRecord record)
 {
   std::lock_guard<std::mutex> lock(m_mutex);
 
@@ -161,7 +193,8 @@ void AllocationMap::insert(void* ptr, AllocationRecord record)
   ++m_size;
 }
 
-const AllocationRecord* AllocationMap::find(void* ptr) const
+const AllocationRecord*
+AllocationMap::find(void* ptr) const
 {
   std::lock_guard<std::mutex> lock(m_mutex);
 
@@ -180,12 +213,14 @@ const AllocationRecord* AllocationMap::find(void* ptr) const
   }
 }
 
-AllocationRecord* AllocationMap::find(void* ptr)
+AllocationRecord*
+AllocationMap::find(void* ptr)
 {
   return const_cast<AllocationRecord*>(const_cast<const AllocationMap*>(this)->find(ptr));
 }
 
-const AllocationRecord* AllocationMap::doFindRecord(void* ptr) const noexcept
+const AllocationRecord*
+AllocationMap::doFindRecord(void* ptr) const noexcept
 {
   const AllocationRecord* alloc_record = nullptr;
 
@@ -214,7 +249,8 @@ const AllocationRecord* AllocationMap::doFindRecord(void* ptr) const noexcept
   return alloc_record;
 }
 
-const AllocationRecord* AllocationMap::findRecord(void* ptr) const noexcept
+const AllocationRecord*
+AllocationMap::findRecord(void* ptr) const noexcept
 {
   std::lock_guard<std::mutex> lock(m_mutex);
 
@@ -222,13 +258,15 @@ const AllocationRecord* AllocationMap::findRecord(void* ptr) const noexcept
   return doFindRecord(ptr);
 }
 
-AllocationRecord* AllocationMap::findRecord(void* ptr) noexcept
+AllocationRecord*
+AllocationMap::findRecord(void* ptr) noexcept
 {
   return const_cast<AllocationRecord*>(const_cast<const AllocationMap*>(this)->findRecord(ptr));
 }
 
 
-AllocationRecord AllocationMap::remove(void* ptr)
+AllocationRecord
+AllocationMap::remove(void* ptr)
 {
   std::lock_guard<std::mutex> lock(m_mutex);
 
@@ -252,13 +290,15 @@ AllocationRecord AllocationMap::remove(void* ptr)
   return ret;
 }
 
-bool AllocationMap::contains(void* ptr) const
+bool
+AllocationMap::contains(void* ptr) const
 {
   UMPIRE_LOG(Debug, "Searching for " << ptr);
   return (findRecord(ptr) != nullptr);
 }
 
-void AllocationMap::clear()
+void
+AllocationMap::clear()
 {
   std::lock_guard<std::mutex> lock(m_mutex);
 
@@ -268,7 +308,8 @@ void AllocationMap::clear()
   m_size = 0;
 }
 
-std::size_t AllocationMap::size() const
+std::size_t
+AllocationMap::size() const
 {
   return m_size;
 }
@@ -286,9 +327,10 @@ AllocationMap::print(const std::function<bool (const AllocationRecord&)>&& pred,
     while (iter != end) {
       if (pred(*iter)) {
         any_match = true;
+        auto end_ptr = static_cast<unsigned char*>(iter->ptr)+iter->size;
         ss << iter->size <<
           " [ " << reinterpret_cast<void*>(iter->ptr) <<
-          " -- " << reinterpret_cast<void*>(static_cast<unsigned char*>(iter->ptr)+iter->size) <<
+          " -- " << reinterpret_cast<void*>(end_ptr) <<
           " ] " << std::endl;
       }
       ++iter;
@@ -301,27 +343,29 @@ AllocationMap::print(const std::function<bool (const AllocationRecord&)>&& pred,
   }
 }
 
-void AllocationMap::printAll(std::ostream& os) const
+void
+AllocationMap::printAll(std::ostream& os) const
 {
   os << "🔍 Printing allocation map contents..." << std::endl;
-
   print([] (const AllocationRecord&) { return true; }, os);
-
   os << "done." << std::endl;
 }
 
 
-AllocationMap::ConstIterator AllocationMap::begin() const
+AllocationMap::ConstIterator
+AllocationMap::begin() const
 {
   return AllocationMap::ConstIterator{this, iterator_begin{}};
 }
 
-AllocationMap::ConstIterator AllocationMap::end() const
+AllocationMap::ConstIterator
+AllocationMap::end() const
 {
   return AllocationMap::ConstIterator{this, iterator_end{}};
 }
 
-AllocationMap::ConstIterator::ConstIterator(const AllocationMap* map, iterator_begin) :
+AllocationMap::ConstIterator::ConstIterator(
+  const AllocationMap* map, iterator_begin) :
   m_outer_iter(map->m_map.begin()),
   m_inner_iter(m_outer_iter->first ? m_outer_iter->second->begin() : InnerIter{}),
   m_inner_end(m_outer_iter->first ? m_outer_iter->second->end() : InnerIter{}),
@@ -329,7 +373,8 @@ AllocationMap::ConstIterator::ConstIterator(const AllocationMap* map, iterator_b
 {
 }
 
-AllocationMap::ConstIterator::ConstIterator(const AllocationMap* map, iterator_end) :
+AllocationMap::ConstIterator::ConstIterator(
+  const AllocationMap* map, iterator_end) :
   m_outer_iter(map->m_map.end()),
   m_inner_iter(InnerIter{}),
   m_inner_end(InnerIter{}),
@@ -349,7 +394,8 @@ AllocationMap::ConstIterator::operator->()
   return m_inner_iter.operator->();
 }
 
-AllocationMap::ConstIterator& AllocationMap::ConstIterator::operator++()
+AllocationMap::ConstIterator&
+AllocationMap::ConstIterator::operator++()
 {
   ++m_inner_iter;
   if (m_inner_iter == m_inner_end) {
@@ -364,19 +410,26 @@ AllocationMap::ConstIterator& AllocationMap::ConstIterator::operator++()
   return *this;
 }
 
-AllocationMap::ConstIterator AllocationMap::ConstIterator::operator++(int)
+AllocationMap::ConstIterator
+AllocationMap::ConstIterator::operator++(int)
 {
   ConstIterator tmp{*this};
   ++(*this);
   return tmp;
 }
 
-bool AllocationMap::ConstIterator::operator==(const AllocationMap::ConstIterator& other) const
+bool
+AllocationMap::ConstIterator::operator==(
+  const AllocationMap::ConstIterator& other) const
 {
-  return m_outer_iter == other.m_outer_iter && m_inner_iter == other.m_inner_iter;
+  return
+    m_outer_iter == other.m_outer_iter &&
+    m_inner_iter == other.m_inner_iter;
 }
 
-bool AllocationMap::ConstIterator::operator!=(const AllocationMap::ConstIterator& other) const
+bool
+AllocationMap::ConstIterator::operator!=(
+  const AllocationMap::ConstIterator& other) const
 {
   return !(*this == other);
 }
