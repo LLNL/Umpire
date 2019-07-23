f2297d25740c57bd3dde12e7112a7992179267bb
Johann Dahm <johann.dahm@ibm.com>
2019-07-22 20:59:35 -0700

    GCC 4.9.3 fix and add fixed pool

--- tests/integration/static_initialization_test.cpp
+++ tests/integration/static_initialization_test.cpp
@@ -1,17 +1,20 @@
 #include "umpire/ResourceManager.hpp"
 #include "umpire/strategy/DynamicPool.hpp"
 
-static auto& rm{umpire::ResourceManager::getInstance()};
+static auto& rm = umpire::ResourceManager::getInstance();
 
-static auto alloc{rm.getAllocator("HOST")};
-static auto pool_alloc{rm.makeAllocator<umpire::strategy::DynamicPool>("host_pool", alloc)};
+static auto alloc = rm.getAllocator("HOST");
+static auto dyn_pool_alloc = rm.makeAllocator<umpire::strategy::DynamicPool>("host_dyn_pool", alloc);
+static auto fixed_pool_alloc = rm.makeAllocator<umpire::strategy::FixedPool>("host_fixed_pool", alloc, 512);
 
-static void* data_1{alloc.allocate(512)};
-static void* data_2{pool_alloc.allocate(512)};
+static void* alloc_data{alloc.allocate(512)};
+static void* dyn_pool_data{dyn_pool_alloc.allocate(512)};
+static void* fixed_pool_data{fixed_pool_alloc.allocate(512)};
 
 int main()
 {
-  alloc.deallocate(data_1);
-  pool_alloc.deallocate(data_2);
+  alloc.deallocate(alloc_data);
+  dyn_pool_alloc.deallocate(dyn_pool_data);
+  fixed_pool_alloc.deallocate(fixed_pool_data);
   return 0;
 }
