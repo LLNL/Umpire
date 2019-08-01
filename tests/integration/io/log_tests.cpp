e8ae515f9321f638f8934e7ab02388752a81243d
David Beckingsale <davidbeckingsale@gmail.com>
2019-08-01 10:39:09 -0700

    Add test for UMPIRE_LOG_LEVEL env var

---
+++ tests/integration/io/log_tests.cpp
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include "umpire/ResourceManager.hpp"
+#include "umpire/util/Macros.hpp"
+
+int main(int UMPIRE_UNUSED_ARG(argc), char** UMPIRE_UNUSED_ARG(argv)) {
+  auto& rm = umpire::ResourceManager::getInstance();
+  UMPIRE_USE_VAR(rm);
+
+  return 0;
+}
