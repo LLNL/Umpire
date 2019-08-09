136a8fcc703de62a33da6b2c8163d8e91ca8bd19
David Beckingsale <davidbeckingsale@gmail.com>
2019-08-09 13:28:26 -0700

    Add PGI 19.7 config

---
+++ host-configs/toss_3_x86_64_ib/pgi_19_7.cmake
@@ -0,0 +1,9 @@
+##############################################################################
+# Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
+# project contributors. See the COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (MIT)
+##############################################################################
+set(CMAKE_C_COMPILER "/usr/tce/packages/pgi/pgi-19.7/linux86-64/19.7/bin/pgcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/pgi/pgi-19.7/linux86-64/19.7/bin/pgc++" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/pgi/pgi-19.7/linux86-64/19.7/bin/pgfortran" CACHE PATH "")
