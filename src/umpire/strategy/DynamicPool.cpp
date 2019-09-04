df11d5edac9d19e128d20df8c7cff68f973959ab
David Beckingsale <davidbeckingsale@gmail.com>
2019-09-04 12:21:55 -0700

    Fixup bug in dynamic pool where leftover blocks were leaked

--- src/umpire/strategy/DynamicPool.cpp
+++ src/umpire/strategy/DynamicPool.cpp
@@ -33,7 +33,7 @@ DynamicPool::DynamicPool(const std::string& name,
                          const int align_bytes) noexcept :
   AllocationStrategy(name, id),
   m_allocator{allocator.getAllocationStrategy()},
-  m_min_alloc_bytes{min_alloc_bytes},
+  m_min_alloc_bytes{round_up(min_alloc_bytes, align_bytes)},
   m_align_bytes{align_bytes},
   m_coalesce_heuristic{coalesce_heuristic},
   m_used_map{},
@@ -42,8 +42,8 @@ DynamicPool::DynamicPool(const std::string& name,
   m_actual_bytes{initial_alloc_bytes},
   m_highwatermark{0}
 {
-  insertFree(m_allocator->allocate(initial_alloc_bytes),
-             initial_alloc_bytes, true, initial_alloc_bytes);
+  const std::size_t bytes{round_up(initial_alloc_bytes, align_bytes)};
+  insertFree(m_allocator->allocate(bytes), bytes, true, bytes);
 }
 
 DynamicPool::~DynamicPool()
@@ -180,16 +180,16 @@ void* DynamicPool::allocate(std::size_t bytes)
 
     m_curr_bytes += rounded_bytes;
 
-    const int64_t left_bytes{static_cast<int64_t>(
-        free_size - rounded_bytes)};
+    const std::size_t left_bytes{free_size - rounded_bytes};
 
-    if (left_bytes > m_align_bytes) {
+    if (left_bytes > 0) {
       insertFree(static_cast<unsigned char*>(ptr) + rounded_bytes, left_bytes,
                  false, whole_bytes);
     }
   } else {
     // Allocate new block -- note this does not check whether alignment is met
     const std::size_t alloc_bytes{std::max(rounded_bytes, m_min_alloc_bytes)};
+    std::cout << "Allocating " << alloc_bytes << " from resource for " << rounded_bytes << std::endl;
     ptr = allocateFromResource(alloc_bytes);
 
     // Add used
@@ -197,11 +197,10 @@ void* DynamicPool::allocate(std::size_t bytes)
     m_actual_bytes += alloc_bytes;
     m_curr_bytes += rounded_bytes;
 
-    const int64_t left_bytes{static_cast<int64_t>(
-        m_min_alloc_bytes - rounded_bytes)};
+    const std::size_t left_bytes{alloc_bytes - rounded_bytes};
 
     // Add free
-    if (left_bytes > m_align_bytes)
+    if (left_bytes > 0)
       insertFree(static_cast<unsigned char*>(ptr) + rounded_bytes, left_bytes,
                  false, alloc_bytes);
   }
