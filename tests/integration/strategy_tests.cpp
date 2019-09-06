50cfc0439c81d8da9fea63d1b85f3a0958907e65
David Beckingsale <davidbeckingsale@gmail.com>
2019-09-06 10:27:28 -0700

    Fix test for AllocationAdvisor

--- tests/integration/strategy_tests.cpp
+++ tests/integration/strategy_tests.cpp
@@ -103,7 +103,7 @@ void StrategyTest<umpire::strategy::AllocationAdvisor>::SetUp()
       m_allocator = new umpire::Allocator(
           rm.makeAllocator<umpire::strategy::AllocationAdvisor>(
             name, 
-            rm.getAllocator("HOST"),
+            rm.getAllocator("DEVICE"),
             "READ_MOSTLY"));
 }
 #endif
