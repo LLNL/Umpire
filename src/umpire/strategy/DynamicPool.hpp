360c09da5257321a3c83b03cc2a84a0ffedd7012
David Beckingsale <davidbeckingsale@gmail.com>
2019-09-11 13:31:10 -0700

    Re-add DynamicPool with alias declaration

---
+++ src/umpire/strategy/DynamicPool.hpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018-2019, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory
+//
+// Created by David Beckingsale, david@llnl.gov
+// LLNL-CODE-747640
+//
+// All rights reserved.
+//
+// This file is part of Umpire.
+//
+// For details, see https://github.com/LLNL/Umpire
+// Please also see the LICENSE file for MIT license.
+//////////////////////////////////////////////////////////////////////////////
+#ifndef UMPIRE_DynamicPool_HPP
+#define UMPIRE_DynamicPool_HPP
+
+#include "umpire/strategy/DynamicPoolMap.hpp"
+
+namespace umpire {
+namespace strategy {
+
+using DynamicPool = DynamicPoolMap;
+
+} // end of namespace strategy
+} // end of namespace umpire
+
+#endif
