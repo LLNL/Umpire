7028eba487328a3aec985cd09887c477727ecc1a
David Beckingsale <davidbeckingsale@gmail.com>
2019-09-12 13:01:53 -0700

    Correct license info

--- src/umpire/strategy/StdAllocator.hpp
+++ src/umpire/strategy/StdAllocator.hpp
@@ -1,3 +1,9 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
 #ifndef _STDALLOCATOR_HPP
 #define _STDALLOCATOR_HPP
 
