bcc1f375ed3692476dc3370879b025af7e516451
Marty McFadden <mcfadden8@llnl.gov>
2019-10-01 14:18:37 -0700

    Debugging added

--- scripts/bamboo/build_and_test.sh
+++ scripts/bamboo/build_and_test.sh
@@ -14,8 +14,12 @@ cd ${BUILD_DIR}
 
 export UMPIRE_COMPILER=${1:-gcc_4_9_3}
 export UMPIRE_BUILD_TYPE=${2:-Release}
+dirname $0
 UMPIRE_SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo "UMPIRE_SCRIPT_PATH = ${UMPIRE_SCRIPT_PATH}"
+dirname ${UMPIRE_SCRIPT_PATH}
 export UMPIRE_SOURCE_DIR="$( cd "$(dirname "${UMPIRE_SCRIPT_PATH}")" ; git rev-parse --show-toplevel )"
+echo "UMPIRE_SOURCE_DIR = ${UMPIRE_SOURCE_DIR}"
 
 if [[ $HOSTNAME == *manta* ]] || [[ $HOSTNAME == *ansel* ]]; then
   echo "lalloc 1 ${UMPIRE_SCRIPT_PATH}/run_build_and_test.sh"
