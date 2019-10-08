e8780b8db5a6c4857b739827ecc3f903eb6fd55e
Marty McFadden <mcfadden8@llnl.gov>
2019-10-08 12:54:50 -0700

    Ignoring two compiler-dependendant runtime frames in backtrace test comparisons

--- tests/integration/backtrace/backtrace_tests_runner.py
+++ tests/integration/backtrace/backtrace_tests_runner.py
@@ -14,14 +14,14 @@ errors = 0
 def compare(good_output, test_output):
     global errors
 
-    index = 0
-    for test_line in test_output[1:]:
-        test_words = test_line.split()
-        good_words = good_output[index].split()
+    index = 1
+    for good_line in good_output[1:]:
+        good_words = good_line.split()
+        test_words = test_output[index].split()
         index = index + 1
-        if len(test_words) >= 2:
-            if test_words[2].split('(')[0] != good_words[2].split('(')[0]:
-                print 'MISCOMPARE LINE: {} "{}" != "{}"'.format(index+1, test_words[2].split('(')[0], good_words[2].split('(')[0])
+        if len(good_words) >= 2:
+            if good_words[2].split('(')[0] != test_words[2].split('(')[0]:
+                print 'MISCOMPARE LINE: {} "{}" != "{}"'.format(index+1, good_words[2].split('(')[0], test_words[2].split('(')[0])
                 errors = -1
 
 def run_test():
@@ -31,7 +31,7 @@ def run_test():
 
     good_output_file = sys.argv[1]
     with open(good_output_file, 'r') as good_file:
-        good_output = good_file.read().splitlines()[1:]
+        good_output = good_file.read().splitlines()
 
     compare(good_output, test_output)
 
