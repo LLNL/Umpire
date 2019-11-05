59a93f6951f0d597e925db1140584da6c5b15969
Marty McFadden <mcfadden8@llnl.gov>
2019-11-05 13:04:04 -0800

    resizing file correctly now after compilation.

--- tools/replay/ReplayFile.cpp
+++ tools/replay/ReplayFile.cpp
@@ -21,13 +21,12 @@ ReplayFile::ReplayFile( std::string in_file_name )
   : m_in_file_name{in_file_name}, m_bin_file_name{m_in_file_name + m_bin_suffix}
 {
   struct stat sbuf;
+  const int prot = PROT_READ|PROT_WRITE;
+  int flags;
 
   if (stat(m_in_file_name.c_str(), &sbuf))
       REPLAY_ERROR( "Unable to open " << m_in_file_name );
 
-  //
-  // Assume our binary file will be <= size of the text file
-  //
   max_file_size = sizeof(ReplayFile::Header) + sbuf.st_size;
 
   m_fd = open(  m_bin_file_name.c_str()
@@ -40,10 +39,11 @@ ReplayFile::ReplayFile( std::string in_file_name )
   if (fstat(m_fd, &sbuf))
     REPLAY_ERROR( "Unable to determine size of: " << m_bin_file_name);
 
-  int flags;
-  const int prot = PROT_READ|PROT_WRITE;
-
-  if (sbuf.st_size < max_file_size) {
+  if (sbuf.st_size > static_cast<off_t>(sizeof(Header))) {
+    m_compile_needed = false;
+    flags = MAP_PRIVATE;  // Writes won't make it to backing store
+  }
+  else {
     m_compile_needed = true;
     flags = MAP_SHARED;   // Writes will make it to backing store
 
@@ -52,10 +52,6 @@ ReplayFile::ReplayFile( std::string in_file_name )
 
     if (write(m_fd, "", 1) < 0)
       REPLAY_ERROR("write failed to " << m_bin_file_name);
-  }
-  else {
-    m_compile_needed = false;
-    flags = MAP_PRIVATE;  // Writes won't make it to backing store
   }
 
   m_op_tables = static_cast<ReplayFile::Header*>(mmap(nullptr, max_file_size, prot, flags, m_fd, 0));
@@ -67,10 +63,20 @@ ReplayFile::ReplayFile( std::string in_file_name )
 ReplayFile::~ReplayFile()
 {
   if (m_op_tables != nullptr && m_op_tables != MAP_FAILED ) {
+    off_t actual_size = sizeof(Header) + (m_op_tables->num_operations * sizeof(Operation));
+
     munmap(m_op_tables, max_file_size);
     m_op_tables = nullptr;
+
+    if (m_compile_needed == true) {
+      std::cout << "Truncating file to: " << actual_size << std::endl;
+      if (ftruncate(m_fd, actual_size) < 0) {
+        REPLAY_ERROR( "Failed to truncate file size for " << m_bin_file_name);
+      }
+    }
+
+    close(m_fd);
   }
-  close(m_fd);
 }
 
 ReplayFile::Header* ReplayFile::getOperationsTable()
