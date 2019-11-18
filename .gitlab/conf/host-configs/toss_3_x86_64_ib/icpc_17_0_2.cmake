e8eb460c6ef705186b0d00ce2c6ebbb8c942a22c
Adrien M. Bernede <bernede1@llnl.gov>
2019-11-18 09:12:27 -0800

    Removing toolchain host config files

--- .gitlab/conf/host-configs/toss_3_x86_64_ib/icpc_17_0_2.cmake
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
-# project contributors. See the COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (MIT)
-##############################################################################
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/intel/intel-17.0.2/bin/icpc" CACHE PATH "")
-set(CMAKE_C_COMPILER "/usr/tce/packages/intel/intel-17.0.2/bin/icc" CACHE PATH "")
-set(CMAKE_Fortran_COMPILER "/usr/tce/packages/intel/intel-17.0.2/bin/ifort" CACHE PATH "")
