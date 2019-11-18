d9b2d8ccebe764119d841002359dc57db879ae9e
Marty McFadden <mcfadden8@llnl.gov>
2019-11-18 13:47:32 -0800

    Added replay_test support for AllocationAdvisor

--- tests/integration/replay/replay_tests.cpp
+++ tests/integration/replay/replay_tests.cpp
@@ -44,9 +44,28 @@ public:
     };
 
     for ( auto basename : allocators ) {
+      std::string name;
       allocator_names.push_back(basename);
-
       auto base_alloc = rm.getAllocator(basename);
+
+#if defined(UMPIRE_ENABLE_CUDA)
+      if ( basename == "UM" ) {
+        auto device_id = 1;   // device_id
+        auto accessing_alloc = rm.getAllocator("HOST");
+
+        name = basename + "_AllocationAdvisor_spec_";
+        makeAllocator<umpire::strategy::AllocationAdvisor, true>(name+"default_id", base_alloc, "READ_MOSTLY");
+        makeAllocator<umpire::strategy::AllocationAdvisor, true>(name+"with_id", base_alloc, "READ_MOSTLY", device_id);
+        makeAllocator<umpire::strategy::AllocationAdvisor, true>(name+"with_accessing_and_default_id", base_alloc, "READ_MOSTLY", accessing_alloc);
+        makeAllocator<umpire::strategy::AllocationAdvisor, true>(name+"with_accessing_and_id", base_alloc, "PREFERRED_LOCATION", accessing_alloc, device_id);
+        name = basename + "_AllocationAdvisor_no_introspection_spec_";
+        makeAllocator<umpire::strategy::AllocationAdvisor, false>(name+"default_id", base_alloc, "READ_MOSTLY");
+        makeAllocator<umpire::strategy::AllocationAdvisor, false>(name+"with_id", base_alloc, "READ_MOSTLY", device_id);
+        makeAllocator<umpire::strategy::AllocationAdvisor, false>(name+"with_accessing_and_default_id", base_alloc, "READ_MOSTLY", accessing_alloc);
+        makeAllocator<umpire::strategy::AllocationAdvisor, false>(name+"with_accessing_and_id", base_alloc, "PREFERRED_LOCATION", accessing_alloc, device_id);
+    }
+#endif // defined(UMPIRE_ENABLE_CUDA)
+
       auto mpa1 = 512;                // Smallest fixed pool object size in bytes
       auto mpa2 = 1*1024;             // Largest fixed pool object size in bytes
       auto mpa3 = 4 * 1024 * 1024;    // Largest initial size of any fixed pool
@@ -55,7 +74,7 @@ public:
       auto mpa6 = 1 * 1024 * 1024;    // Minimum size of all future allocations in the dynamic pool
       auto mpa7 = 128;                // Size with which to align allocations
       auto mpa8 = umpire::strategy::heuristic_percent_releasable(75); // Heuristic
-      std::string name{basename + "_mixedpool_spec"};
+      name = basename + "_MixedPool_spec_";
       makeAllocator<umpire::strategy::MixedPool, true>(name+"0", base_alloc);
       makeAllocator<umpire::strategy::MixedPool, true>(name+"1", base_alloc , mpa1);
       makeAllocator<umpire::strategy::MixedPool, true>(name+"2", base_alloc, mpa1, mpa2);
@@ -65,78 +84,86 @@ public:
       makeAllocator<umpire::strategy::MixedPool, true>(name+"6", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6);
       makeAllocator<umpire::strategy::MixedPool, true>(name+"7", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6, mpa7);
       makeAllocator<umpire::strategy::MixedPool, true>(name+"8", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6, mpa7, mpa8);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"0", base_alloc);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"1", base_alloc , mpa1);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"2", base_alloc, mpa1, mpa2);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"3", base_alloc, mpa1, mpa2, mpa3);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"4", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"5", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"6", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"7", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6, mpa7);
-      makeAllocator<umpire::strategy::MixedPool, false>(name+"_nointro"+"8", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6, mpa7, mpa8);
+      name = basename + "_MixedPool_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"0", base_alloc);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"1", base_alloc , mpa1);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"2", base_alloc, mpa1, mpa2);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"3", base_alloc, mpa1, mpa2, mpa3);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"4", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"5", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"6", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"7", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6, mpa7);
+      makeAllocator<umpire::strategy::MixedPool, false>(name+"8", base_alloc, mpa1, mpa2, mpa3, mpa4, mpa5, mpa6, mpa7, mpa8);
 
       auto dpa1 = 256 * 1024 * 1024; // min initial allocation size
       auto dpa2 = 1 * 1024 * 1024;   // min allocation size
       auto dpa3 = 128;               // byte alignment
       auto dpa4 = umpire::strategy::heuristic_percent_releasable(50);
-      name = basename + "_dyn_spec";
+      name = basename + "_DynamicPool_spec_";
       makeAllocator<umpire::strategy::DynamicPool, true>(name+"0", base_alloc);
       makeAllocator<umpire::strategy::DynamicPool, true>(name+"1", base_alloc, dpa1);
       makeAllocator<umpire::strategy::DynamicPool, true>(name+"2", base_alloc, dpa1, dpa2);
       makeAllocator<umpire::strategy::DynamicPool, true>(name+"3", base_alloc, dpa1, dpa2, dpa3);
       makeAllocator<umpire::strategy::DynamicPool, true>(name+"4", base_alloc, dpa1, dpa2, dpa3, dpa4);
-      makeAllocator<umpire::strategy::DynamicPool, false>(name+"_nointro"+"0", base_alloc);
-      makeAllocator<umpire::strategy::DynamicPool, false>(name+"_nointro"+"1", base_alloc, dpa1);
-      makeAllocator<umpire::strategy::DynamicPool, false>(name+"_nointro"+"2", base_alloc, dpa1, dpa2);
-      makeAllocator<umpire::strategy::DynamicPool, false>(name+"_nointro"+"3", base_alloc, dpa1, dpa2, dpa3);
-      makeAllocator<umpire::strategy::DynamicPool, false>(name+"_nointro"+"4", base_alloc, dpa1, dpa2, dpa3, dpa4);
+      name = basename + "_DynamicPool_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::DynamicPool, false>(name+"0", base_alloc);
+      makeAllocator<umpire::strategy::DynamicPool, false>(name+"1", base_alloc, dpa1);
+      makeAllocator<umpire::strategy::DynamicPool, false>(name+"2", base_alloc, dpa1, dpa2);
+      makeAllocator<umpire::strategy::DynamicPool, false>(name+"3", base_alloc, dpa1, dpa2, dpa3);
+      makeAllocator<umpire::strategy::DynamicPool, false>(name+"4", base_alloc, dpa1, dpa2, dpa3, dpa4);
 
-      name = basename + "_dyn_pool_map_spec";
+      name = basename + "_DynamicPoolMap_spec_";
       makeAllocator<umpire::strategy::DynamicPoolMap, true>(name+"0", base_alloc);
       makeAllocator<umpire::strategy::DynamicPoolMap, true>(name+"1", base_alloc, dpa1);
       makeAllocator<umpire::strategy::DynamicPoolMap, true>(name+"2", base_alloc, dpa1, dpa2);
       makeAllocator<umpire::strategy::DynamicPoolMap, true>(name+"3", base_alloc, dpa1, dpa2, dpa3);
       makeAllocator<umpire::strategy::DynamicPoolMap, true>(name+"4", base_alloc, dpa1, dpa2, dpa3, dpa4);
-      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"_nointro"+"0", base_alloc);
-      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"_nointro"+"1", base_alloc, dpa1);
-      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"_nointro"+"2", base_alloc, dpa1, dpa2);
-      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"_nointro"+"3", base_alloc, dpa1, dpa2, dpa3);
-      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"_nointro"+"4", base_alloc, dpa1, dpa2, dpa3, dpa4);
+      name = basename + "_DynamicPoolMap_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"0", base_alloc);
+      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"1", base_alloc, dpa1);
+      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"2", base_alloc, dpa1, dpa2);
+      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"3", base_alloc, dpa1, dpa2, dpa3);
+      makeAllocator<umpire::strategy::DynamicPoolMap, false>(name+"4", base_alloc, dpa1, dpa2, dpa3, dpa4);
 
       auto lpa1 = dpa1;
       auto lpa2 = dpa2;
       auto lpa3 = umpire::strategy::heuristic_percent_releasable_list(50);
-      name = basename + "_dyn_pool_list_spec";
+      name = basename + "_DynamicPoolList_spec_";
       makeAllocator<umpire::strategy::DynamicPoolList, true>(name+"0", base_alloc);
       makeAllocator<umpire::strategy::DynamicPoolList, true>(name+"1", base_alloc, lpa1);
       makeAllocator<umpire::strategy::DynamicPoolList, true>(name+"2", base_alloc, lpa1, lpa2);
       makeAllocator<umpire::strategy::DynamicPoolList, true>(name+"3", base_alloc, lpa1, lpa2, lpa3);
-      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"_nointro"+"0", base_alloc);
-      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"_nointro"+"1", base_alloc, lpa1);
-      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"_nointro"+"2", base_alloc, lpa1, lpa2);
-      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"_nointro"+"3", base_alloc, lpa1, lpa2, lpa3);
+      name = basename + "_DynamicPoolList_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"0", base_alloc);
+      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"1", base_alloc, lpa1);
+      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"2", base_alloc, lpa1, lpa2);
+      makeAllocator<umpire::strategy::DynamicPoolList, false>(name+"3", base_alloc, lpa1, lpa2, lpa3);
 
       auto ma1 = 1024; // Capacity
-      name = basename + "_monotonic_allocator_spec";
-      makeAllocator<umpire::strategy::MonotonicAllocationStrategy, true>(name+"1", base_alloc, ma1);
-      makeAllocator<umpire::strategy::MonotonicAllocationStrategy, false>(name+"_nointro"+"1", base_alloc, ma1);
+      name = basename + "_MonotonicAllocationStrategy_spec_";
+      makeAllocator<umpire::strategy::MonotonicAllocationStrategy, true>(name, base_alloc, ma1);
+      name = basename + "_MonotonicAllocationStrategy_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::MonotonicAllocationStrategy, false>(name, base_alloc, ma1);
 
       auto sa1 = 64;  // Slots
-      name = basename + "_slot_allocator_spec";
-      makeAllocator<umpire::strategy::SlotPool, true>(name+"1", base_alloc, sa1);
-      makeAllocator<umpire::strategy::SlotPool, false>(name+"_nointro"+"1", base_alloc, sa1);
+      name = basename + "_SlotPool_spec_";
+      makeAllocator<umpire::strategy::SlotPool, true>(name, base_alloc, sa1);
+      name = basename + "_SlotPool_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::SlotPool, false>(name, base_alloc, sa1);
 
-      name = basename + "_thread_safe_allocator";
-      rm.makeAllocator<umpire::strategy::ThreadSafeAllocator, true>(name+"0", base_alloc);
-      rm.makeAllocator<umpire::strategy::ThreadSafeAllocator, false>(name+"_nointro"+"0", base_alloc);
+      name = basename + "_ThreadSafeAllocator_spec_";
+      rm.makeAllocator<umpire::strategy::ThreadSafeAllocator, true>(name, base_alloc);
+      name = basename + "_ThreadSafeAllocator_no_instrospection_spec_";
+      rm.makeAllocator<umpire::strategy::ThreadSafeAllocator, false>(name, base_alloc);
 
       auto fpa1 = allocation_size; // object_bytes
       auto fpa2 = 1024; // objects_per_pool                        
-      name = basename + "_fixed_pool_allocator_spec";
+      name = basename + "_FixedPool_spec_";
       makeAllocator<umpire::strategy::FixedPool, true>(name+"1", base_alloc, fpa1);
       makeAllocator<umpire::strategy::FixedPool, true>(name+"2", base_alloc, fpa1, fpa2);
-      makeAllocator<umpire::strategy::FixedPool, false>(name+"_nointro"+"1", base_alloc, fpa1);
-      makeAllocator<umpire::strategy::FixedPool, false>(name+"_nointro"+"2", base_alloc, fpa1, fpa2);
+      name = basename + "_FixedPool_no_instrospection_spec_";
+      makeAllocator<umpire::strategy::FixedPool, false>(name+"1", base_alloc, fpa1);
+      makeAllocator<umpire::strategy::FixedPool, false>(name+"2", base_alloc, fpa1, fpa2);
     }
   }
 
