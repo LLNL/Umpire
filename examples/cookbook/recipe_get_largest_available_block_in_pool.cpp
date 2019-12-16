c068d16293a23947b1a0baaa2f672f3f81992e6d
Marty McFadden <mcfadden8@llnl.gov>
2019-12-16 08:12:35 -0800

    Added recipe for getLargestAvailableBlock

---
+++ examples/cookbook/recipe_get_largest_available_block_in_pool.cpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include "umpire/strategy/DynamicPool.hpp"
+
+#include "umpire/Allocator.hpp"
+#include "umpire/ResourceManager.hpp"
+
+#include "umpire/util/Exception.hpp"
+#include "umpire/util/wrap_allocator.hpp"
+
+#include <iostream>
+
+int main(int, char**) {
+  auto& rm = umpire::ResourceManager::getInstance();
+
+  auto pool = rm.makeAllocator<umpire::strategy::DynamicPool>(
+      "pool", rm.getAllocator("HOST"));
+
+  auto dynamic_pool =
+    umpire::util::unwrap_allocator<umpire::strategy::DynamicPool>(pool);
+
+  if ( dynamic_pool == nullptr ) {
+    UMPIRE_ERROR(pool.getName() << " is not a DynamicPool");
+  }
+
+  auto ptr = pool.allocate(1024);
+
+  std::cout
+    << "Largest available block in pool is "
+    << dynamic_pool->getLargestAvailableBlock() << " bytes in size"
+    << std::endl;
+
+  pool.deallocate(ptr);
+
+  return 0;
+}
