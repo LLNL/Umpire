9cf0d5dfe9d6175f321183a0d8aa6bf212de559f
Marty McFadden <mcfadden8@llnl.gov>
2020-01-09 15:33:04 -0800

    Added CI support BLUEOS latest xl and clang compilers

---
+++ .gitlab/conf/host-configs/blueos_3_ppc64le_ib/xl_2019_08_20.cmake
@@ -0,0 +1,10 @@
+##############################################################################
+# Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and Umpire
+# project contributors. See the COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (MIT)
+##############################################################################
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/xl/xl-2019.08.20/bin/xlc++" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/xl/xl-2019.08.20/bin/xlc" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/xl/xl-2019.08.20/bin/xlf2003" CACHE PATH "")
+
