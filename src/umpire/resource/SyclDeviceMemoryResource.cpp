303018a89003e1e7747bc256df7166da5e4baf38
Abhishek Bagusetty <abagusetty@anl.gov>
2020-03-04 14:20:49 -0600

    WIP - sycl support for resources

---
+++ src/umpire/resource/SyclDeviceMemoryResource.cpp
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include "umpire/resource/SyclDeviceMemoryResource.hpp"
+
+#include "umpire/ResourceManager.hpp"
+
+#include "umpire/util/Macros.hpp"
+
+#include <memory>
+#include <sstream>
+
+namespace umpire {
+namespace resource {
+
+SyclDeviceMemoryResource::SyclDeviceMemoryResource(
+    Platform platform, 
+    const std::string& name,
+    int id, 
+    MemoryResourceTraits traits) :
+  MemoryResource(name, id, traits),
+  m_allocator{},
+  m_platform(platform)
+{
+}
+
+void* SyclDeviceMemoryResource::allocate(std::size_t bytes)
+{
+  cl::sycl::gpu_selector gpuSelect;
+  cl::sycl::device sycl_device(gpuSelect);
+  cl::sycl::queue sycl_queue(sycl_device);
+  
+  void* ptr = m_allocator.allocate(bytes, sycl_queue);
+
+  UMPIRE_LOG(Debug, "(bytes=" << bytes << ") returning " << ptr);
+  UMPIRE_RECORD_STATISTIC(getName(), "ptr", reinterpret_cast<uintptr_t>(ptr), "size", bytes, "event", "allocate");
+
+  return ptr;
+}
+
+  void SyclDeviceMemoryResource::deallocate(void* ptr)
+{
+  UMPIRE_LOG(Debug, "(ptr=" << ptr << ")");
+
+  UMPIRE_RECORD_STATISTIC(getName(), "ptr", reinterpret_cast<uintptr_t>(ptr), "size", 0x0, "event", "deallocate");
+
+  m_allocator.deallocate(ptr, queue_t);
+}
+
+std::size_t SyclDeviceMemoryResource::getCurrentSize() const noexcept
+{
+  UMPIRE_LOG(Debug, "() returning " << 0);
+  return 0;
+}
+
+std::size_t SyclDeviceMemoryResource::getHighWatermark() const noexcept
+{
+  UMPIRE_LOG(Debug, "() returning " << 0);
+  return 0;
+}
+
+Platform SyclDeviceMemoryResource::getPlatform() noexcept
+{
+  return m_platform;
+}
+
+} // end of namespace resource
+} // end of namespace umpire
---
+++ src/umpire/resource/SyclDeviceMemoryResource.hpp
@@ -0,0 +1,47 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#ifndef UMPIRE_SyclDeviceMemoryResource_HPP
+#define UMPIRE_SyclDeviceMemoryResource_HPP
+
+#include "umpire/alloc/SyclMallocAllocator.hpp"
+
+#include "umpire/resource/MemoryResource.hpp"
+
+#include "umpire/util/AllocationRecord.hpp"
+#include "umpire/util/Platform.hpp"
+
+namespace umpire {
+namespace resource {
+
+  /*!
+   * \brief Concrete MemoryResource object that uses the template _allocator to
+   * allocate and deallocate memory.
+   */
+class SyclDeviceMemoryResource :
+  public MemoryResource
+{
+  public: 
+    SyclDeviceMemoryResource(Platform platform, const std::string& name, int id, MemoryResourceTraits traits);
+
+    void* allocate(std::size_t bytes);
+    void deallocate(void* ptr);
+
+    std::size_t getCurrentSize() const noexcept;
+    std::size_t getHighWatermark() const noexcept;
+
+    Platform getPlatform() noexcept;
+
+  protected:
+    alloc::SyclMallocAllocator m_allocator;
+
+    Platform m_platform;
+};
+
+} // end of namespace resource
+} // end of namespace umpire
+
+#endif // UMPIRE_SyclDeviceMemoryResource_HPP
