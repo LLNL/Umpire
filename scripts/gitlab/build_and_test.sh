bc6b9aabcc6bf3e0eea94ccaee4cbd01c88c2729
Adrien M. Bernede <bernede1@llnl.gov>
2020-03-17 14:24:35 -0700

    Umpire directory and build_and_test robustness
    ~/.gitlab-runner/builds is getting messy. Adding an umpire directory
    will help.
    
    build_and_test has been improved based on work on RAJA.

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -6,48 +6,73 @@
 # SPDX-License-Identifier: (MIT)
 ##############################################################################
 
-set -e
+set -o errexit
+set -o nounset
 
-hostname
+sys_type=${SYS_TYPE:-""}
+if [[ -z ${sys_type} ]]
+then
+    sys_type=${OSTYPE:-""}
+    if [[ -z ${sys_type} ]]
+    then
+        echo "System type not found (both SYS_TYPE and OSTYPE are undefined)"
+        exit 1
+    fi
+fi
 
-PROJECT_DIRECTORY="$(pwd)"
-BUILD_DIRECTORY="${BUILD_ROOT}/build_${SYS_TYPE}_${COMPILER}"
-CCONF="host-configs/${SYS_TYPE}/${COMPILER}.cmake"
+build_root=${BUILD_ROOT:-""}
+if [[ -z ${build_root} ]]
+then
+    build_root=$(pwd)
+fi
 
-if [[ "${1}" != "--test-only" ]]
+compiler=${COMPILER:-""}
+if [[ -z ${compiler} ]]
+then
+    echo "COMPILER is undefined... aborting"
+    exit 1
+fi
+
+project_dir="$(pwd)"
+build_dir="${build_root}/build_${sys_type}_${compiler}"
+cconf="host-configs/${sys_type}/${compiler}.cmake"
+
+option=${1:-""}
+
+if [[ "${option}" != "--test-only" ]]
 then
     # If building, then delete everything first
 
-    rm -rf ${BUILD_DIRECTORY}
-    mkdir -p ${BUILD_DIRECTORY}
+    rm -rf ${build_dir}
+    mkdir -p ${build_dir}
 fi
 
 # Assert that build directory exist (mainly for --test-only mode)
-if [[ ! -d ${BUILD_DIRECTORY} ]]
+if [[ ! -d ${build_dir} ]]
 then
-    echo "Build directory not found : ${BUILD_DIRECTORY}"
+    echo "Build directory not found : ${build_dir}"
     exit 1
 fi
 
 # Always go to build directory
-cd ${BUILD_DIRECTORY}
+cd ${build_dir}
 
 # Build
-if [[ "${1}" != "--test-only" ]]
+if [[ "${option}" != "--test-only" ]]
 then
     cmake \
-      -C ${PROJECT_DIRECTORY}/.radiuss-ci/gitlab/conf/${CCONF} \
-      -C ${PROJECT_DIRECTORY}/${CCONF} \
-      ${PROJECT_DIRECTORY}
+      -C ${project_dir}/.radiuss-ci/gitlab/conf/${cconf} \
+      -C ${project_dir}/${cconf} \
+      ${project_dir}
     cmake --build . -j 4
 fi
 
 # Test
-if [[ "${1}" != "--build-only" ]]
+if [[ "${option}" != "--build-only" ]]
 then
     ctest -T test 2>&1 | tee tests_output.txt
     no_test_str="No tests were found\!\!\!"
     [[ "$(tail -n 1 tests_output.txt)" == "${no_test_str}" ]] && exit 1
     tree Testing
-    cp Testing/*/Test.xml ${PROJECT_DIRECTORY}
+    cp Testing/*/Test.xml ${project_dir}
 fi
