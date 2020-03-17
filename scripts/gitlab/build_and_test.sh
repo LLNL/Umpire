32b0acde789e73e5150d9159c7dcc554a37e8cbf
Adrien M. Bernede <bernede1@llnl.gov>
2020-03-17 16:49:03 -0700

    In build_and_test: fix and improvement

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -9,6 +9,7 @@
 set -o errexit
 set -o nounset
 
+# Check environment variables
 sys_type=${SYS_TYPE:-""}
 if [[ -z ${sys_type} ]]
 then
@@ -29,40 +30,36 @@ fi
 compiler=${COMPILER:-""}
 if [[ -z ${compiler} ]]
 then
-    echo "COMPILER is undefined... aborting"
-    exit 1
+    echo "COMPILER is undefined... aborting" && exit 1
 fi
 
 project_dir="$(pwd)"
 build_dir="${build_root}/build_${sys_type}_${compiler}"
-cconf="host-configs/${sys_type}/${compiler}.cmake"
-
 option=${1:-""}
-
-if [[ "${option}" != "--test-only" ]]
-then
-    # If building, then delete everything first
-
-    rm -rf ${build_dir}
-    mkdir -p ${build_dir}
-fi
-
-# Assert that build directory exist (mainly for --test-only mode)
-if [[ ! -d ${build_dir} ]]
-then
-    echo "Build directory not found : ${build_dir}"
-    exit 1
-fi
-
-# Always go to build directory
-cd ${build_dir}
 
 # Build
 if [[ "${option}" != "--test-only" ]]
 then
+    # If building, then delete everything first
+    rm -rf ${build_dir} && mkdir -p ${build_dir} && cd ${build_dir}
+
+    conf_suffix="host-configs/${sys_type}/${compiler}.cmake"
+
+    generic_conf="${project_dir}/.radiuss-ci/gitlab/conf/${conf_suffix}"
+    if [[ ! -f ${generic_conf} ]]
+    then
+        echo "ERROR: Host-config file ${generic_conf} does not exist" && exit 1
+    fi
+
+    umpire_conf="${project_dir}/${conf_suffix}"
+    if [[ ! -f ${umpire_conf} ]]
+    then
+        echo "ERROR: Host-config file ${umpire_conf} does not exist" && exit 1
+    fi
+
     cmake \
-      -C ${project_dir}/.radiuss-ci/gitlab/conf/${cconf} \
-      -C ${project_dir}/${cconf} \
+      -C ${generic_conf} \
+      -C ${umpire_conf} \
       ${project_dir}
     cmake --build . -j 4
 fi
@@ -70,9 +67,22 @@ fi
 # Test
 if [[ "${option}" != "--build-only" ]]
 then
+    if [[ ! -d ${build_dir} ]]
+    then
+        echo "ERROR: Build directory not found : ${build_dir}" && exit 1
+    fi
+
+    cd ${build_dir}
+
     ctest -T test 2>&1 | tee tests_output.txt
-    no_test_str="No tests were found\!\!\!"
-    [[ "$(tail -n 1 tests_output.txt)" == "${no_test_str}" ]] && exit 1
+
+    no_test_str="No tests were found!!!"
+    if [[ "$(tail -n 1 tests_output.txt)" == "${no_test_str}" ]]
+    then
+        echo "ERROR: No tests were found" && exit 1
+    fi
+
+    echo "Copying Testing xml reports for export"
     tree Testing
     cp Testing/*/Test.xml ${project_dir}
 fi
