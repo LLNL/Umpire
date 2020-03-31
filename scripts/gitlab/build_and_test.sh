9bb9c4d1df4501b1c5cc6ecc181eebb1a751d694
Adrien M. Bernede <bernede1@llnl.gov>
2020-03-31 14:42:27 -0700

    Improvements

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -36,68 +36,64 @@ then
     echo "CONFIGURATION is undefined... aborting" exit 1
 fi
 
-# 'conf' = toolchain__tuning
-# 'host_config' = filter__tuning
-#   where 'filter' can represent several toolchains
-#   like <nvcc_10_gcc_X> covers any gcc paired with nvcc10
-# 'toolchain' is a unique set of tools, and 'tuning' allows to have
-# several configurations for this set, like <omptarget>.
-
-echo "--- Configuration to match :"
-echo "* ${conf}"
-
-toolchain=${conf/__*/}
-tuning=${conf/${toolchain}/}
-
-# FIRST STEP :
-# Find project host_configs matching the configuration
-host_configs="$(ls host-configs/${sys_type}/ | grep "\.cmake$")"
-echo "--- Available host_configs"
-echo "${host_configs}"
-
-match_count=0
-host_config=""
-
-# Translate file names into pattern to match the host_config
-echo "--- Patterns"
-for hc in ${host_configs}
-do
-    pattern="${hc//X/.*}"
-    pattern="${pattern/.cmake/}"
-    echo "${pattern}"
-
-    if [[ -n "${tuning}" && ! "${pattern}" =~ .*${tuning}$ ]]
-    then
-        continue
-    fi
-
-    if [[ "${conf}" =~ ^${pattern}$ ]]
-    then
-        (( ++match_count ))
-        host_config="${hc}"
-        echo "-> Found Project Conf : ${host_config}"
-    fi
-done
-
-if (( match_count > 1 )) || (( match_count == 0 ))
-then
-    echo "ERROR : none or multiple match(s) ..."
-    exit 1
-fi
-
-# SECOND STEP :
-# Build
-
 project_dir="$(pwd)"
 build_dir="${build_root}/build_${sys_type}_${conf}"
 install_dir="${build_root}/install_${sys_type}_${conf}"
 option=${1:-""}
+
 # Build
 if [[ "${option}" != "--test-only" ]]
 then
+    # 'conf' = toolchain__tuning
+    # 'host_config' = filter__tuning
+    #   where 'filter' can represent several toolchains
+    #   like <nvcc_10_gcc_X> covers any gcc paired with nvcc10
+    # 'toolchain' is a unique set of tools, and 'tuning' allows to have
+    # several configurations for this set, like <omptarget>.
+
+    echo "--- Configuration to match :"
+    echo "* ${conf}"
+
+    toolchain=${conf/__*/}
+    tuning=${conf/${toolchain}/}
+
+    # Find project host_configs matching the configuration
+    host_configs="$(ls host-configs/${sys_type}/ | grep "\.cmake$")"
+    echo "--- Available host_configs"
+    echo "${host_configs}"
+
+    match_count=0
+    host_config=""
+
+    # Translate file names into pattern to match the host_config
+    echo "--- Patterns"
+    for hc in ${host_configs}
+    do
+        pattern="${hc//X/.*}"
+        pattern="${pattern/.cmake/}"
+        echo "${pattern}"
+
+        if [[ -n "${tuning}" && ! "${pattern}" =~ .*${tuning}$ ]]
+        then
+            continue
+        fi
+
+        if [[ "${conf}" =~ ^${pattern}$ ]]
+        then
+            (( ++match_count ))
+            host_config="${hc}"
+            echo "-> Found Project Conf : ${host_config}"
+        fi
+    done
+
+    if (( match_count > 1 )) || (( match_count == 0 ))
+    then
+        echo "ERROR : none or multiple match(s) ..."
+        exit 1
+    fi
+
     # If building, then delete everything first
     rm -rf ${build_dir} && mkdir -p ${build_dir} && cd ${build_dir}
-
 
     generic_conf="${project_dir}/.radiuss-ci/gitlab/conf/host-configs/${sys_type}/${toolchain}.cmake"
     if [[ ! -f ${generic_conf} ]]
