b071fe6dc7cb04d3be4981640eead5f65dd262e7
David Beckingsale <davidbeckingsale@gmail.com>
2020-04-16 12:48:11 -0700

    Add initial aligned allocator and example

---
+++ src/umpire/strategy/AlignedAllocator.cpp
@@ -0,0 +1,80 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include "umpire/strategy/AlignedAllocator.hpp"
+
+#include "umpire/util/Macros.hpp"
+
+namespace umpire {
+namespace strategy {
+
+AlignedAllocator::AlignedAllocator(
+    const std::string& name,
+    int id,
+    Allocator allocator,
+    std::size_t alignment) :
+  AllocationStrategy(name, id),
+  m_allocator(allocator.getAllocationStrategy()),
+  m_alignment{alignment},
+  m_mask{reinterpret_cast<uintptr_t>(~(m_alignment-1))}
+{
+}
+
+void* 
+AlignedAllocator::allocate(std::size_t bytes)
+{
+  std::size_t total_bytes = bytes+sizeof(void*)+m_alignment-1;
+  UMPIRE_LOG(Debug, "requested: " << bytes << " actual: " << bytes+m_alignment-1);
+
+  uintptr_t ptr{reinterpret_cast<uintptr_t>(m_allocator->allocate(total_bytes))};
+  uintptr_t aligned_ptr{reinterpret_cast<uintptr_t>((reinterpret_cast<uintptr_t>(ptr) + sizeof(void*) + (m_alignment-1)) & m_mask)}; 
+  uintptr_t* header = (uintptr_t*) (aligned_ptr - sizeof(void*));
+  *header = ptr;
+
+  std::cout << ptr << std::endl;
+  std::cout << aligned_ptr << std::endl;
+
+  UMPIRE_LOG(Debug, "ptr: " << reinterpret_cast<void*>(ptr) << " aligned: " << reinterpret_cast<void*>(aligned_ptr));
+  return reinterpret_cast<void*>(aligned_ptr);
+}
+
+void 
+AlignedAllocator::deallocate(void* ptr)
+{
+  uintptr_t aligned_ptr{reinterpret_cast<uintptr_t>(ptr)};
+  uintptr_t* header = (uintptr_t*) (aligned_ptr - sizeof(void*));
+  void* base_ptr = reinterpret_cast<void*>(*header);
+  
+  UMPIRE_LOG(Debug, "ptr: " << reinterpret_cast<void*>(ptr) << " base_ptr: " << reinterpret_cast<void*>(base_ptr));
+  return m_allocator->deallocate(base_ptr);
+}
+
+std::size_t 
+AlignedAllocator::getCurrentSize() const noexcept
+{
+  return 0;
+}
+
+std::size_t 
+AlignedAllocator::getHighWatermark() const noexcept
+{
+  return 0;
+}
+
+Platform 
+AlignedAllocator::getPlatform() noexcept
+{
+  return m_allocator->getPlatform();
+}
+
+MemoryResourceTraits
+AlignedAllocator::getTraits() const noexcept
+{
+  return m_allocator->getTraits();
+}
+
+} // end of namespace strategy
+} // end of namespace umpire
---
+++ src/umpire/strategy/AlignedAllocator.hpp
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#ifndef UMPIRE_AlignedAllocator_HPP
+#define UMPIRE_AlignedAllocator_HPP
+
+#include "umpire/strategy/AllocationStrategy.hpp"
+
+#include "umpire/Allocator.hpp"
+
+namespace umpire {
+namespace strategy {
+
+class AlignedAllocator :
+  public AllocationStrategy
+{
+  public:
+    AlignedAllocator(
+        const std::string& name,
+        int id,
+        Allocator allocator,
+        std::size_t alignment); 
+
+    void* allocate(std::size_t bytes) override;
+    void deallocate(void* ptr) override;
+
+    std::size_t getCurrentSize() const noexcept override;
+    std::size_t getHighWatermark() const noexcept override;
+
+    Platform getPlatform() noexcept override;
+
+    MemoryResourceTraits getTraits() const noexcept override;
+
+  protected:
+    strategy::AllocationStrategy* m_allocator;
+
+  private:
+    std::size_t m_alignment;
+    uintptr_t m_mask;
+};
+
+} // end of namespace strategy
+} // end of namespace umpire
+
+#endif
