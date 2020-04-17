f6f7a6a94f5180b394d5a11db036abc4d9c03e48
David Beckingsale <davidbeckingsale@gmail.com>
2020-04-17 07:30:24 -0700

    Fixup with suggestion from @mcfadden8

--- src/umpire/strategy/AllocationTracker.cpp
+++ src/umpire/strategy/AllocationTracker.cpp
@@ -61,10 +61,9 @@ AllocationTracker::getHighWatermark() const noexcept
 std::size_t
 AllocationTracker::getActualSize() const noexcept
 {
-  return (m_allocator->getActualSize() > 0) ? 
-    m_allocator->getActualSize() : m_current_size;
-auto actual_size = m_allocator->getActualSize();
-return actual_size > 0 ? actual_size : m_current_size;
+  auto actual_size = m_allocator->getActualSize();
+  return actual_size > 0 ? actual_size : m_current_size;
+}
 
 std::size_t
 AllocationTracker::getAllocationCount() const noexcept
