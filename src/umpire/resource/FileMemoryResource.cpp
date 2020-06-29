c8e9390f907e19fe0947f6a863714d9a49867d54
Aileen Perez <perezaileen9111@gmail.com>
2020-06-29 15:53:51 -0500

    Added Allocate and Deallocate

--- src/umpire/resource/FileMemoryResource.cpp
+++ src/umpire/resource/FileMemoryResource.cpp
@@ -4,7 +4,25 @@
 //
 // SPDX-License-Identifier: (MIT)
 //////////////////////////////////////////////////////////////////////////////
+#ifndef UMPIRE_FileMemoryResource_INL
+#define UMPIRE_FileMemoryResource_INL
+
 #include "umpire/resource/FileMemoryResource.hpp"
+
+#include "umpire/util/Macros.hpp"
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <memory>
+#include <sstream>
+#include <time.h>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <stdio.h>
+#include <utility>
 
 namespace umpire {
 namespace resource {
@@ -15,17 +33,43 @@ FileMemoryResource::FileMemoryResource(
     int id,
     MemoryResourceTraits traits) :
   MemoryResource(name, id, traits),
-  m_platform{platform}
+  m_platform{platform},
+  m_size_map{}
 {
 }
 
 void* FileMemoryResource::allocate(std::size_t bytes)
 {
-  return NULL;
+  const char * file = std::tmpnam(nullptr);
+  std::size_t SIZE = bytes / sysconf(_SC_PAGE_SIZE);
+  int fd = open(file, O_RDWR | O_CREAT, S_IRWXU);
+
+  if(SIZE == 0)
+    SIZE = sysconf(_SC_PAGE_SIZE);
+  else
+    SIZE = (sysconf(_SC_PAGE_SIZE) * SIZE) + (bytes % sysconf(_SC_PAGE_SIZE));
+
+  truncate(file, SIZE);
+  void* ptr{mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)};
+  
+  std::pair <const char *, std::size_t> INFO;
+  INFO = std::make_pair(file,SIZE);
+  m_size_map.insert(ptr, INFO);
+
+  close(fd);
+  return ptr;
 }
 
 void FileMemoryResource::deallocate(void* ptr)
 {
+  auto iter = m_size_map.find(ptr);
+  auto size = iter->second->second;
+  auto file_name = iter->second->first;
+  m_size_map.erase(ptr);
+
+  munmap(ptr, size);
+
+  remove(file_name);
 }
 
 std::size_t FileMemoryResource::getCurrentSize() const noexcept
@@ -45,3 +89,4 @@ Platform FileMemoryResource::getPlatform() noexcept
 
 } // end of namespace resource
 } // end of namespace umpire
+#endif // UMPIRE_FileMemoryResource_INL
