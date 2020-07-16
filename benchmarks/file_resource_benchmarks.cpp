3c6032fde74c490b351a33b0eb4f2139462b3fad
Aileen Perez <perezaileen9111@gmail.com>
2020-07-16 16:05:53 -0500

    Quick Push

--- benchmarks/file_resource_benchmarks.cpp
+++ benchmarks/file_resource_benchmarks.cpp
@@ -13,69 +13,67 @@
 #include <unistd.h>
 #include <time.h>
 
-const int n = 10;
+const int interations{10000};
 size_t Scalar = 5;
 
 void Copy(std::size_t* A, std::size_t* C){
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < interations; i++)
     {
         A[i] = C[i];
     }   
 }
 
 void Scale(std::size_t* B, std::size_t* C){
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < interations; i++)
     {
         B[i] = C[i] * Scalar;
     }   
 }
 
 void Add(std::size_t* A, std::size_t* B, std::size_t* C){
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < interations; i++)
     {
         C[i] = A[i] + B[i];
     }   
 }
 
-void Triad(std::size_t *A, std::size_t* B, std::size_t* C){
-    for(int i = 0; i < n; i++)
+void Triad(std::size_t* A, std::size_t* B, std::size_t* C){
+    for(int i = 0; i < interations; i++)
     {
         A[i] = B[i] + Scalar * C[i];
     }   
 }
 
-void Allocate(umpire::Allocator alloc, std::size_t* A, std::size_t* B, std::size_t* C){
+void Allocate(umpire::Allocator alloc, std::size_t*& A, std::size_t*& B, std::size_t*& C){
 
-    A = (std::size_t*) alloc.allocate(sizeof(size_t) * n);
-    B = (std::size_t*) alloc.allocate(sizeof(size_t) * n);
-    C = (std::size_t*) alloc.allocate(sizeof(size_t) * n);
+    A = (std::size_t*) alloc.allocate(sizeof(size_t) * interations);
+    B = (std::size_t*) alloc.allocate(sizeof(size_t) * interations);
+    C = (std::size_t*) alloc.allocate(sizeof(size_t) * interations);
 
-    for (int i=0; i<n; i++) {
+    for (int i=0; i<interations; i++) {
         A[i] = (size_t) rand() % 100;
         B[i] = (size_t) rand() % 100;
         C[i] = (size_t) rand() % 100;
     }
 }
 
-void Deallocate(umpire::Allocator alloc, std::size_t** A, std::size_t** B, std::size_t** C){
-    alloc.deallocate( *A );
-    alloc.deallocate( *B );
-    alloc.deallocate( *C );
+void Deallocate(umpire::Allocator alloc, std::size_t* A, std::size_t* B, std::size_t* C){
+    alloc.deallocate( A );
+    alloc.deallocate( B );
+    alloc.deallocate( C );
 }
 
 void benchmark(std::string name){
 
     auto& rm = umpire::ResourceManager::getInstance();
     umpire::Allocator alloc = rm.getAllocator(name);
-    
+
     std::size_t* A = nullptr;
     std::size_t* B = nullptr;
     std::size_t* C = nullptr;
     
-    std::cout << A;
     Allocate(alloc,A,B,C);
-    std::cout << A;
-/*    auto begin_copy = std::chrono::system_clock::now();
+    auto begin_copy = std::chrono::system_clock::now();
     Copy(A,C);
     auto end_copy = std::chrono::system_clock::now();
     Deallocate(alloc,A,B,C);
@@ -99,10 +97,10 @@ void benchmark(std::string name){
     Deallocate(alloc,A,B,C);
 
     std::cout << name << std::endl;
-    std::cout << "    Copy:     " <<  std::chrono::duration<double>(end_copy - begin_copy).count()/n << " seconds/elements in array" <<std::endl;
-    std::cout << "    Scale:    " << std::chrono::duration<double>(end_scale - begin_scale).count()/n << " seconds/elements in array" << std::endl;
-    std::cout << "    Add:      " <<  std::chrono::duration<double>(end_add - begin_add).count()/n << " seconds/elements in array" << std::endl;
-    std::cout << "    Triad:    " << std::chrono::duration<double>(end_triad - begin_triad).count()/n << " seconds/elements in array" << std::endl;  */
+    std::cout << "    Copy:     " <<  std::chrono::duration<double>(end_copy - begin_copy).count()/interations << " sec/elements" <<std::endl;
+    std::cout << "    Scale:    " << std::chrono::duration<double>(end_scale - begin_scale).count()/interations << " sec/elements" << std::endl;
+    std::cout << "    Add:      " <<  std::chrono::duration<double>(end_add - begin_add).count()/interations << " sec/elements" << std::endl;
+    std::cout << "    Triad:    " << std::chrono::duration<double>(end_triad - begin_triad).count()/interations << " sec/elements" << std::endl;
 }
 
 int main(int, char**) {
