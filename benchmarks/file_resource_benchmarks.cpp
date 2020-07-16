d145d6cb6974bd13664011fb474c647dd4343f9f
Aileen Perez <perezaileen9111@gmail.com>
2020-07-16 13:27:52 -0500

    Update to Benchmark Needs Review

--- benchmarks/file_resource_benchmarks.cpp
+++ benchmarks/file_resource_benchmarks.cpp
@@ -7,109 +7,111 @@
 
 #include "umpire/resource/FileMemoryResource.hpp"
 
-#include <limits.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "umpire/ResourceManager.hpp"
+#include "umpire/Allocator.hpp"
+
 #include <unistd.h>
 #include <time.h>
 
 const int n = 10;
 size_t Scalar = 5;
 
-auto alloc = std::make_shared<umpire::resource::FileMemoryResource>(umpire::Platform::host, "TEST", 0, umpire::MemoryResourceTraits{});
-std::size_t* A_pointer;
-std::size_t* B_pointer;
-std::size_t* C_pointer;
-
-std::size_t* A;
-std::size_t* B;
-std::size_t* C;
-
-void Copy(){
+void Copy(std::size_t** A, std::size_t** C){
     for(int i = 0; i < n; i++)
     {
-        A[i] = C[i];
+        (*A)[i] = (*C)[i];
     }   
 }
 
-void Scale(){
+void Scale(std::size_t** B, std::size_t** C){
     for(int i = 0; i < n; i++)
     {
-        B[i] = C[i] * Scalar;
+        (*B)[i] = (*C)[i] * Scalar;
     }   
 }
 
-void Add(){
+void Add(std::size_t** A, std::size_t** B, std::size_t** C){
     for(int i = 0; i < n; i++)
     {
-        C[i] = A[i] + B[i];
+        (*C)[i] = (*A)[i] + (*B)[i];
     }   
 }
 
-void Triad(){
+void Triad(std::size_t** A, std::size_t** B, std::size_t** C){
     for(int i = 0; i < n; i++)
     {
-        A[i] = B[i] + Scalar * C[i];
+        (*A)[i] = (*B)[i] + Scalar * (*C)[i];
     }   
 }
 
-void Allocate(){
-    A_pointer = (std::size_t*) alloc->allocate(sysconf(_SC_PAGE_SIZE) * (sizeof(size_t) * 3 * n) );
-    B_pointer = (std::size_t*) alloc->allocate(sysconf(_SC_PAGE_SIZE) * (sizeof(size_t) * 3 * n) );
-    C_pointer = (std::size_t*) alloc->allocate(sysconf(_SC_PAGE_SIZE) * (sizeof(size_t) * 3 * n) );
+void Allocate(umpire::Allocator alloc, std::size_t** A, std::size_t** B, std::size_t** C){
 
-    A = A_pointer;
-    B = B_pointer;
-    C = C_pointer;
-
-    A = new size_t[n];
-    B = new size_t[n];
-    C = new size_t[n];
+    *A = (std::size_t*) alloc.allocate(sizeof(size_t) * n);
+    *B = (std::size_t*) alloc.allocate(sizeof(size_t) * n);
+    *C = (std::size_t*) alloc.allocate(sizeof(size_t) * n);
 
     for (int i=0; i<n; i++) {
-        A[i] = (size_t) rand() % 100;
-        B[i] = (size_t) rand() % 100;
-        C[i] = (size_t) rand() % 100;
+        (*A)[i] = (size_t) rand() % 100;
+        (*B)[i] = (size_t) rand() % 100;
+        (*C)[i] = (size_t) rand() % 100;
     }
 }
 
-void Deallocate(){
-    alloc->deallocate( A_pointer );
-    alloc->deallocate( B_pointer );
-    alloc->deallocate( C_pointer );
+void Deallocate(umpire::Allocator alloc, std::size_t** A, std::size_t** B, std::size_t** C){
+    alloc.deallocate( *A );
+    alloc.deallocate( *B );
+    alloc.deallocate( *C );
+}
+
+void benchmark(std::string name){
+
+    auto& rm = umpire::ResourceManager::getInstance();
+    umpire::Allocator alloc = rm.getAllocator(name);
+
+    std::size_t* A = nullptr;
+    std::size_t* B = nullptr;
+    std::size_t* C = nullptr;
+    
+    Allocate(alloc,&A,&B,&C);
+    auto begin_copy = std::chrono::system_clock::now();
+    Copy(&A,&C);
+    auto end_copy = std::chrono::system_clock::now();
+    Deallocate(alloc,&A,&B,&C);
+
+    Allocate(alloc,&A,&B,&C);
+    auto begin_scale = std::chrono::system_clock::now();
+    Scale(&B,&C);
+    auto end_scale = std::chrono::system_clock::now();
+    Deallocate(alloc,&A,&B,&C);
+
+    Allocate(alloc,&A,&B,&C);
+    auto begin_add = std::chrono::system_clock::now();
+    Add(&A,&B,&C);
+    auto end_add = std::chrono::system_clock::now();
+    Deallocate(alloc,&A,&B,&C);
+
+    Allocate(alloc,&A,&B,&C);
+    auto begin_triad = std::chrono::system_clock::now();
+    Triad(&A,&B,&C);
+    auto end_triad = std::chrono::system_clock::now();
+    Deallocate(alloc,&A,&B,&C);
+
+    std::cout << name << std::endl;
+    std::cout << "    Copy:     " <<  std::chrono::duration<double>(end_copy - begin_copy).count()/n << " sec/elements" <<std::endl;
+    std::cout << "    Scale:    " << std::chrono::duration<double>(end_scale - begin_scale).count()/n << " sec/elements" << std::endl;
+    std::cout << "    Add:      " <<  std::chrono::duration<double>(end_add - begin_add).count()/n << " sec/elements" << std::endl;
+    std::cout << "    Triad:    " << std::chrono::duration<double>(end_triad - begin_triad).count()/n << " sec/elements" << std::endl;
 }
 
 int main(int, char**) {
+  benchmark("HOST");
+  benchmark("FILE");
 
-    Allocate();
-    auto begin_copy = std::chrono::system_clock::now();
-    Copy();
-    auto end_copy = std::chrono::system_clock::now();
-    Deallocate();
-
-    Allocate();
-    auto begin_scale = std::chrono::system_clock::now();
-    Scale();
-    auto end_scale = std::chrono::system_clock::now();
-    Deallocate();
-
-    Allocate();
-    auto begin_add = std::chrono::system_clock::now();
-    Add();
-    auto end_add = std::chrono::system_clock::now();
-    Deallocate();
-
-    Allocate();
-    auto begin_triad = std::chrono::system_clock::now();
-    Triad();
-    auto end_triad = std::chrono::system_clock::now();
-    Deallocate();
-
-    std::cout << "    Copy:     " <<  std::chrono::duration<double>(end_copy - begin_copy).count()/1 << std::endl;
-    std::cout << "    Scale:    " << std::chrono::duration<double>(end_scale - begin_scale).count()/1 << std::endl;
-    std::cout << "    Add:      " <<  std::chrono::duration<double>(end_add - begin_add).count()/1 << std::endl;
-    std::cout << "    Triad:    " << std::chrono::duration<double>(end_triad - begin_triad).count()/1 << std::endl; 
-    
-    return 0;
+#if defined(UMPIRE_ENABLE_CUDA)
+  benchmark("DEVICE");
+  benchmark("UM");
+#endif
+#if defined(UMPIRE_ENABLE_HIP)
+  benchmark("DEVICE");
+#endif
 }
