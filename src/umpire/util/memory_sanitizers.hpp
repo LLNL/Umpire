191c3aa42d8da64f3b06705e923ed8fbc15bb418
Marty McFadden <mcfadden8@llnl.gov>
2020-07-23 16:53:47 -0700

    using guards recommended by clang documentation for sanatizers

--- src/umpire/util/memory_sanitizers.hpp
+++ src/umpire/util/memory_sanitizers.hpp
@@ -9,10 +9,12 @@
 
 #include "umpire/config.hpp"
 
-#if (defined(__clang__) || defined(__GNUC__))
+#if defined(__clang__) || defined(__GNUC__)
 
 #include <sanitizer/asan_interface.h>
 
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer)
 #define UMPIRE_POISON_MEMORY_REGION(allocator, ptr, size) \
   if (allocator->getPlatform() == umpire::Platform::host) {\
     ASAN_POISON_MEMORY_REGION((ptr), (size));\
@@ -22,12 +24,14 @@
   if (allocator->getPlatform() == umpire::Platform::host) {\
     ASAN_UNPOISON_MEMORY_REGION((ptr), (size));\
   }
-
 #else
-
 #define UMPIRE_POISON_MEMORY_REGION(allocator, ptr, size)
 #define UMPIRE_UNPOISON_MEMORY_REGION(allocator, ptr, size)
-
-#endif
+#endif // __has_feature(address_sanitizer)
+#else
+#define UMPIRE_POISON_MEMORY_REGION(allocator, ptr, size)
+#define UMPIRE_UNPOISON_MEMORY_REGION(allocator, ptr, size)
+#endif // defined(__has_feature)
+#endif  // defined(__clang__) || defined(__GNUC__)
 
 #endif // UMPIRE_memory_sanitizers_HPP
