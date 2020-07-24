a3e49c9e925409e9a4a9a3d260ca36d03e4f1bea
Marty McFadden <mcfadden8@llnl.gov>
2020-07-24 10:01:54 -0700

    Updated comment for how SANITIZER is enabled and cleaned up header file

--- src/umpire/util/memory_sanitizers.hpp
+++ src/umpire/util/memory_sanitizers.hpp
@@ -7,29 +7,51 @@
 #ifndef UMPIRE_memory_sanitizers_HPP
 #define UMPIRE_memory_sanitizers_HPP
 
-#include "umpire/config.hpp"
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
 
+#undef __UMPIRE_USE_MEMORY_SANITIZER__    // This may be defined below
+
+//
+// When a user compiles with "-fsanitize=memory", a couple of macros may be
+// set depending upon compiler and version.  The two compilers that most
+// broadly understand this option are clang and GNUC.  Exceptions to this are:
+//
+// GNUC 4 does not know about asan_interface.h.  Nor does the XL compiler,
+// even when it is using the clang front-end.
+//
+// The SYCL compiler (at least the one as of this writing) also does not
+// seem to know where <sanitizer/asan_interface> is so we exclude its inclusion.
+//
 #if (defined(__clang__) && !defined(__ibmxl__))  || (defined(__GNUC__) && __GNUC__ > 4)
-#if !defined(__SYCL_COMPILER_VERSION)
-#include <sanitizer/asan_interface.h>
-#endif
-#endif
 
-#undef UMPIRE_USE_MEMORY_SANITIZER
+#if !defined(__SYCL_COMPILER_VERSION)
+
+#include <sanitizer/asan_interface.h>
 
 #if defined(__SANITIZE_ADDRESS__)
-#undef UMPIRE_USE_MEMORY_SANITIZER
-#define UMPIRE_USE_MEMORY_SANITIZER
-#endif
+
+#undef __UMPIRE_USE_MEMORY_SANITIZER__
+#define __UMPIRE_USE_MEMORY_SANITIZER__
+
+#endif // #if defined(__SANITIZE_ADDRESS__)
 
 #if defined(__has_feature)
 #if __has_feature(address_sanitizer)
-#undef UMPIRE_USE_MEMORY_SANITIZER
-#define UMPIRE_USE_MEMORY_SANITIZER
-#endif // defined(__has_feature)
-#endif  // defined(__clang__) || defined(__GNUC__)
 
-#if defined(UMPIRE_USE_MEMORY_SANITIZER)
+#undef __UMPIRE_USE_MEMORY_SANITIZER__
+#define __UMPIRE_USE_MEMORY_SANITIZER__
+
+#endif // #if __has_feature(address_sanitizer)
+#endif // #if defined(__has_feature)
+
+#endif  // #if !defined(__SYCL_COMPILER_VERSION)
+
+#endif // #if (defined(__clang__) && !defined(__ibmxl__))  || (defined(__GNUC__) && __GNUC__ > 4)
+
+
+#if defined(__UMPIRE_USE_MEMORY_SANITIZER__)
+
 #define UMPIRE_POISON_MEMORY_REGION(allocator, ptr, size) \
   if (allocator->getPlatform() == umpire::Platform::host) {\
     ASAN_POISON_MEMORY_REGION((ptr), (size));\
@@ -39,9 +61,12 @@
   if (allocator->getPlatform() == umpire::Platform::host) {\
     ASAN_UNPOISON_MEMORY_REGION((ptr), (size));\
   }
-#else
+
+#else // !defined(__UMPIRE_USE_MEMORY_SANITIZER__)
+
 #define UMPIRE_POISON_MEMORY_REGION(allocator, ptr, size)
 #define UMPIRE_UNPOISON_MEMORY_REGION(allocator, ptr, size)
-#endif // __has_feature(address_sanitizer)
 
-#endif // UMPIRE_memory_sanitizers_HPP
+#endif // #if defined(__UMPIRE_USE_MEMORY_SANITIZER__)
+
+#endif // #ifndef UMPIRE_memory_sanitizers_HPP
