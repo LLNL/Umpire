39b534c6423d7127c45c633f6a298fddaa2d0962
David Beckingsale <davidbeckingsale@gmail.com>
2020-08-06 12:59:53 -0700

    Tweak entrypoint script

--- .github/actions/abi-compat/entrypoint.sh
+++ .github/actions/abi-compat/entrypoint.sh
@@ -2,18 +2,18 @@
 
 git submodule update --init --recursive
 
-mkdir build && cd build \ 
-  && cmake -DENABLE_DEVELOPER_DEFAULTS=On -DCMAKE_CXX_FLAGS="-Og" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++ -DBUILD_SHARED_LIBS=On .. \
-  && make -j 3 umpire \
-  && cd ..
+mkdir build && cd build 
+cmake -DENABLE_DEVELOPER_DEFAULTS=On -DCMAKE_CXX_FLAGS="-Og" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++ -DBUILD_SHARED_LIBS=On ..
+make -j 3 umpire
+cd ..
 
 git checkout origin/main
 git submodule update --init --recursive
 
-mkdir build-main && cd build-main \ 
-  && cmake -DENABLE_DEVELOPER_DEFAULTS=On -DCMAKE_CXX_FLAGS="-Og" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++ -DBUILD_SHARED_LIBS=On .. \
-  && make -j 3 umpire \
-  && cd ..
+mkdir build-main && cd build-main 
+cmake -DENABLE_DEVELOPER_DEFAULTS=On -DCMAKE_CXX_FLAGS="-Og" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++ -DBUILD_SHARED_LIBS=On ..
+make -j 3 umpire
+cd ..
 
 abi-dumper build-main/lib/libumpire.so -o umpire-main.dump -lver 3.0.0
 abi-dumper build/lib/libumpire.so -o umpire-dev.dump -lver 4.0.0
