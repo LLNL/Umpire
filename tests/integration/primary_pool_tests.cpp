a64923161d2a6138b64eb35a87e2c6320d087f59
David Beckingsale <davidbeckingsale@gmail.com>
2020-08-21 08:49:34 -0700

    Add tests for Operations against all Strategies

--- tests/integration/primary_pool_tests.cpp
+++ tests/integration/primary_pool_tests.cpp
@@ -10,78 +10,12 @@
 
 #include "camp/camp.hpp"
 #include "gtest/gtest.h"
+#include "test_helpers.hpp"
 #include "umpire/ResourceManager.hpp"
 #include "umpire/config.hpp"
 #include "umpire/strategy/DynamicPoolList.hpp"
 #include "umpire/strategy/DynamicPoolMap.hpp"
 #include "umpire/strategy/QuickPool.hpp"
-
-template <typename T>
-struct tag_to_string {
-};
-
-template <>
-struct tag_to_string<umpire::strategy::DynamicPoolList> {
-  static constexpr const char* value = "DynamicPoolList";
-};
-
-template <>
-struct tag_to_string<umpire::strategy::DynamicPoolMap> {
-  static constexpr const char* value = "DynamicPoolMap";
-};
-
-template <>
-struct tag_to_string<umpire::strategy::QuickPool> {
-  static constexpr const char* value = "QuickPool";
-};
-
-struct host_resource_tag {
-};
-
-template <>
-struct tag_to_string<host_resource_tag> {
-  static constexpr const char* value = "HOST";
-};
-
-#if defined(UMPIRE_ENABLE_DEVICE)
-struct device_resource_tag {
-};
-
-template <>
-struct tag_to_string<device_resource_tag> {
-  static constexpr const char* value = "DEVICE";
-};
-#endif
-
-#if defined(UMPIRE_ENABLE_UM)
-struct um_resource_tag {
-};
-
-template <>
-struct tag_to_string<um_resource_tag> {
-  static constexpr const char* value = "UM";
-};
-#endif
-
-#if defined(UMPIRE_ENABLE_PINNED)
-struct pinned_resource_tag {
-};
-
-template <>
-struct tag_to_string<pinned_resource_tag> {
-  static constexpr const char* value = "PINNED";
-};
-#endif
-
-#if defined(UMPIRE_ENABLE_FILE_RESOURCE)
-struct file_resource_tag {
-};
-
-template <>
-struct tag_to_string<file_resource_tag> {
-  static constexpr const char* value = "FILE";
-};
-#endif
 
 using ResourceTypes = camp::list<host_resource_tag
 #if defined(UMPIRE_ENABLE_DEVICE)
---
+++ tests/integration/test_helpers.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "umpire/config.hpp"
+
+template <typename T>
+struct tag_to_string {
+};
+
+template <>
+struct tag_to_string<umpire::strategy::DynamicPoolList> {
+  static constexpr const char* value = "DynamicPoolList";
+};
+
+template <>
+struct tag_to_string<umpire::strategy::DynamicPoolMap> {
+  static constexpr const char* value = "DynamicPoolMap";
+};
+
+template <>
+struct tag_to_string<umpire::strategy::QuickPool> {
+  static constexpr const char* value = "QuickPool";
+};
+
+struct host_resource_tag {
+};
+
+template <>
+struct tag_to_string<host_resource_tag> {
+  static constexpr const char* value = "HOST";
+};
+
+#if defined(UMPIRE_ENABLE_DEVICE)
+struct device_resource_tag {
+};
+
+template <>
+struct tag_to_string<device_resource_tag> {
+  static constexpr const char* value = "DEVICE";
+};
+#endif
+
+#if defined(UMPIRE_ENABLE_UM)
+struct um_resource_tag {
+};
+
+template <>
+struct tag_to_string<um_resource_tag> {
+  static constexpr const char* value = "UM";
+};
+#endif
+
+#if defined(UMPIRE_ENABLE_PINNED)
+struct pinned_resource_tag {
+};
+
+template <>
+struct tag_to_string<pinned_resource_tag> {
+  static constexpr const char* value = "PINNED";
+};
+#endif
+
+#if defined(UMPIRE_ENABLE_FILE_RESOURCE)
+struct file_resource_tag {
+};
+
+template <>
+struct tag_to_string<file_resource_tag> {
+  static constexpr const char* value = "FILE";
+};
+#endif
