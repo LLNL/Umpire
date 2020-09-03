becfbb966111c9f85a48585f10481c8cc0a77686
Marty McFadden <mcfadden8@llnl.gov>
2020-09-03 15:10:49 -0700

    Release 4.0.1

--- scripts/make_release_tarball.sh
+++ scripts/make_release_tarball.sh
@@ -7,7 +7,7 @@
 ##############################################################################
 
 TAR_CMD=gtar
-VERSION=4.0.0
+VERSION=4.0.1
 
 git archive --prefix=umpire-${VERSION}/ -o umpire-${VERSION}.tar HEAD 2> /dev/null
 
