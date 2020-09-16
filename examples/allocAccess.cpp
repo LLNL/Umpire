25893c6efd8b7ed3baa69130161c2a6cc1b14f57
Kristi Belcher <belcher6@lassen708.coral.llnl.gov>
2020-09-16 10:36:26 -0700

    updated example test file

--- examples/allocAccess.cpp
+++ examples/allocAccess.cpp
@@ -10,6 +10,7 @@
 #include "umpire/Allocator.hpp"
 #include "umpire/ResourceManager.hpp"
 #include "umpire/strategy/DynamicPoolMap.hpp"
+#include "umpire/strategy/QuickPool.hpp"
 #include "umpire/util/MemoryResourceTraits.hpp"
 
 #include "umpire/Umpire.cpp"
@@ -19,45 +20,74 @@
 using myResource = umpire::MemoryResourceTraits::resource_type;
 using cPlatform = camp::resources::Platform;
 
+void testAccess(umpire::Allocator a)
+{
+  std::cout << "Testing the host platform..." << std::endl;
+  if(is_accessible(cPlatform::host, a)) {
+    std::cout << "The allocator, " << a.getName() << 
+                 ", is accessible." << std::endl << std::endl;
+  } else {
+    std::cout << "The allocator is not accessible."
+              << std::endl << std::endl;
+  }
+  
+  std::cout << "Testing the cuda platform..." << std::endl;
+  if(is_accessible(cPlatform::cuda, a)) {
+    std::cout << "The allocator, " << a.getName() << 
+                 ", is accessible." << std::endl << std::endl;
+  } else {
+    std::cout << "The allocator is not accessible."
+              << std::endl << std::endl;
+  }
+ 
+  std::cout << "Testing the hip platform..." << std::endl;
+  if(is_accessible(cPlatform::hip, a)) {
+    std::cout << "The allocator, " << a.getName() << 
+                 ", is accessible." << std::endl << std::endl;
+  } else {
+    std::cout << "The allocator is not accessible."
+              << std::endl << std::endl;
+  }
+
+  std::cout << "Testing the omp_target platform..." << std::endl;
+  if(is_accessible(cPlatform::omp_target, a)) {
+    std::cout << "The allocator, " << a.getName() << 
+                 ", is accessible." << std::endl << std::endl;
+  } else {
+    std::cout << "The allocator is not accessible."
+              << std::endl << std::endl;
+  } 
+
+  std::cout << "Testing the sycl platform..." << std::endl;
+  if(is_accessible(cPlatform::sycl, a)) {
+    std::cout << "The allocator, " << a.getName() << 
+                 ", is accessible." << std::endl << std::endl;
+  } else {
+    std::cout << "The allocator is not accessible."
+              << std::endl << std::endl;
+  }
+  
+  std::cout << "---------------------------------" << std::endl; 
+  return; 
+}
+
 int main()
 {
   auto& rm = umpire::ResourceManager::getInstance();
   auto host_alloc = rm.makeAllocator<umpire::strategy::DynamicPoolMap>(
       "host_DynamicPoolMap", rm.getAllocator("HOST"));
 
-  double* data = static_cast<double*>(host_alloc.allocate(1024*sizeof(double)));
-
-  std::cout << "Testing the host platform..." << std::endl;
-  if(is_accessible(cPlatform::host, host_alloc)) {
-    std::cout << "The allocator, " << host_alloc.getName() << 
-                 ", is accessible." << std::endl << std::endl;
-  }
+  auto dev_alloc = rm.makeAllocator<umpire::strategy::QuickPool>(
+      "dev_QuickPool", rm.getAllocator("DEVICE"));
   
-  std::cout << "Testing the cuda platform..." << std::endl;
-  if(is_accessible(cPlatform::cuda, host_alloc)) {
-    std::cout << "The allocator, " << host_alloc.getName() << 
-                 ", is accessible." << std::endl << std::endl;
-  }
- 
-  std::cout << "Testing the hip platform..." << std::endl;
-  if(is_accessible(cPlatform::hip, host_alloc)) {
-    std::cout << "The allocator, " << host_alloc.getName() << 
-                 ", is accessible." << std::endl << std::endl;
-  } 
+  double* dataH = static_cast<double*>(host_alloc.allocate(1024*sizeof(double)));
+  testAccess(host_alloc);
 
-  std::cout << "Testing the omp_target platform..." << std::endl;
-  if(is_accessible(cPlatform::omp_target, host_alloc)) {
-    std::cout << "The allocator, " << host_alloc.getName() << 
-                 ", is accessible." << std::endl << std::endl;
-  } 
+  double* dataD = static_cast<double*>(dev_alloc.allocate(1024*sizeof(double)));
+  testAccess(dev_alloc);
 
-  std::cout << "Testing the sycl platform..." << std::endl;
-  if(is_accessible(cPlatform::sycl, host_alloc)) {
-    std::cout << "The allocator, " << host_alloc.getName() << 
-                 ", is accessible." << std::endl << std::endl;
-  } 
-
-  host_alloc.deallocate(data);
+  host_alloc.deallocate(dataH);
+  dev_alloc.deallocate(dataD);
 
   return 0;
 }
