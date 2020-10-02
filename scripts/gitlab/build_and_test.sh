9599f1d2c1bd21221402f142cc612b6ff84f4b51
Adrien M. Bernede <bernede1@llnl.gov>
2020-10-02 13:58:02 -0700

    Fix an issue in build_and_test when used locally (not in CI)

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -48,51 +48,55 @@ then
 
 fi
 
-# Host config file
-if [[ -z ${hostconfig} ]]
-then
-    # If no host config file was provided, we assume it was generated.
-    # This means we are looking of a unique one in project dir.
-    hostconfigs=( $( ls "${project_dir}/"hc-*.cmake ) )
-    if [[ ${#hostconfigs[@]} == 1 ]]
-    then
-        hostconfig_path=${hostconfigs[0]}
-        echo "Found host config file: ${hostconfig_path}"
-    elif [[ ${#hostconfigs[@]} == 0 ]]
-    then
-        echo "No result for: ${project_dir}/hc-*.cmake"
-        echo "Spack generated host-config not found."
-        exit 1
-    else
-        echo "More than one result for: ${project_dir}/hc-*.cmake"
-        echo "${hostconfigs[@]}"
-        echo "Please specify one with HOST_CONFIG variable"
-        exit 1
-    fi
-else
-    # Using provided host-config file.
-    hostconfig_path="${project_dir}/host-configs/${hostconfig}"
-fi
-
-# Build Directory
-if [[ -z ${build_root} ]]
-then
-    build_root=$(pwd)
-fi
-
-build_dir="${build_root}/build_${hostconfig//.cmake/}"
-
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "~~~~~ Host-config: ${hostconfig_path}"
-echo "~~~~~ Build Dir:   ${build_dir}"
-echo "~~~~~ Project Dir: ${project_dir}"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "~~~~ ENV ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-env
-
 # Build
 if [[ "${option}" != "--deps-only" && "${option}" != "--test-only" ]]
 then
+    # Host config file
+    if [[ -z ${hostconfig} ]]
+    then
+        # If no host config file was provided, we assume it was generated.
+        # This means we are looking of a unique one in project dir.
+        hostconfigs=( $( ls "${project_dir}/"hc-*.cmake ) )
+        if [[ ${#hostconfigs[@]} == 1 ]]
+        then
+            hostconfig_path=${hostconfigs[0]}
+            echo "Found host config file: ${hostconfig_path}"
+        elif [[ ${#hostconfigs[@]} == 0 ]]
+        then
+            echo "No result for: ${project_dir}/hc-*.cmake"
+            echo "Spack generated host-config not found."
+            exit 1
+        else
+            echo "More than one result for: ${project_dir}/hc-*.cmake"
+            echo "${hostconfigs[@]}"
+            echo "Please specify one with HOST_CONFIG variable"
+            exit 1
+        fi
+    else
+        # Using provided host-config file.
+        hostconfig_path="${project_dir}/host-configs/${hostconfig}"
+    fi
+
+    # Build Directory
+    if [[ -z ${build_root} ]]
+    then
+        build_root=$(pwd)
+    fi
+
+    build_dir="${build_root}/build_${hostconfig//.cmake/}"
+
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~ Host-config: ${hostconfig_path}"
+    echo "~ Build Dir:   ${build_dir}"
+    echo "~ Project Dir: ${project_dir}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo ""
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~ ENV ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+    env
+
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     echo "~~~~~ Building Umpire"
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
