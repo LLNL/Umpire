c57e181e833f1d06b8f8d3f766bd0ab418e95422
Kristi Belcher <belcher6@lassen709.coral.llnl.gov>
2020-10-16 14:13:13 -0700

    simple example to show getParent() functionality

---
+++ examples/get_parent.cpp
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include "umpire/Allocator.hpp"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/strategy/DynamicPool.hpp"
+
+void check_parent(umpire::Allocator alloc)
+{
+  umpire::strategy::AllocationStrategy* root = alloc.getAllocationStrategy();
+  while ((root->getParent() != nullptr)) {
+    root = root->getParent();
+    std::cout << root->getName() << std::endl;
+  }
+  std::cout << "(NULL)" << std::endl;
+  std::cout << "------------------" << std::endl;
+}
+
+/*
+ * This example exposes the parent allocator or resource that was used
+ * when creating a new allocator. The first parent will be NULL because
+ * this represents the memory resource being used. From that point on,
+ * the parent will be whatever allocator was used by the resource manager
+ * to create the next allocator in the chain.
+ */
+int main ()
+{
+  const int SIZE = 5;
+  auto& rm = umpire::ResourceManager::getInstance();
+
+  std::vector<umpire::Allocator> Alloc(SIZE);
+  Alloc[0] = rm.getAllocator("HOST");
+
+  for(int i = 1; i < SIZE; i++) {
+    Alloc[i] = rm.makeAllocator<umpire::strategy::DynamicPool>(
+      "HOST_pool_" + std::to_string(i), Alloc[i-1]);
+  }
+
+  for(int c = 0; c < SIZE; c++) {
+    std::cout << "Starting tree: " << (c+1) << "/" << SIZE << std::endl;
+    check_parent(Alloc[c]);
+  }
+
+  return 0;
+}
+
