c16dc7e478d25550fbb5197ba8bdbb2bacef9b00
Kristi Belcher <belcher6@lassen708.coral.llnl.gov>
2020-12-14 14:49:15 -0800

    don't need the explicit windows check anymore

--- src/umpire/util/io.cpp
+++ src/umpire/util/io.cpp
@@ -19,10 +19,6 @@
 #include "umpire/util/Macros.hpp"
 #include "umpire/util/OutputBuffer.hpp"
 
-#if defined(_MSC_VER)
-#define UMPIRE_ENABLE_FILESYSTEM
-#endif
-
 #if defined(UMPIRE_ENABLE_FILESYSTEM)
 #include <filesystem>
 #else
