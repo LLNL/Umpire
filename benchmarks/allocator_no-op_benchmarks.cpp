6d06cbce8c75bb3d970af612a770f023629f9d09
Kristi Belcher <belcher6@lassen607.coral.llnl.gov>
2020-12-28 13:03:52 -0800

    noop benchmark updates

--- benchmarks/allocator_no-op_benchmarks.cpp
+++ benchmarks/allocator_no-op_benchmarks.cpp
@@ -12,21 +12,57 @@
 static const int RangeLow{4};
 static const int RangeHi{1024};
 
-static void NoOpBenchmark(benchmark::State& st) {
-  auto& rm = umpire::ResourceManager::getInstance();
-  umpire::Allocator alloc = rm.getAllocator("NO_OP");
+class NoOpAllocatorBenchmark : public benchmark::Fixture
+{
+public:
+  using ::benchmark::Fixture::SetUp;
+  using ::benchmark::Fixture::TearDown;
 
-  const std::size_t size {static_cast<std::size_t>(st.range(0))};
-  void* allocation;
-    
-  while (st.KeepRunning()) {
-    allocation = alloc.allocate(size);
-    alloc.deallocate(allocation);
+  NoOpAllocatorBenchmark() {}
+
+  void SetUp(benchmark::State&) {
+    auto& rm = umpire::ResourceManager::getInstance();
+    alloc = rm.getAllocator("NO_OP");
   }
-}
+ 
+  void TearDown(benchmark::State&) {
+    alloc.getAllocationStrategy()->release();
+  }
+  
+  void NoOpAllocate(benchmark::State& st) {
+    size = static_cast<std::size_t>(st.range(0));
+    while (st.KeepRunning()) {
+      allocation = alloc.allocate(size);
+    }
+  }
 
-BENCHMARK(NoOpBenchmark)->Range(RangeLow, RangeHi);
-BENCHMARK(NoOpBenchmark)->Range(RangeLow, RangeHi);
-BENCHMARK(NoOpBenchmark)->Range(RangeLow, RangeHi);
+  void NoOpDeallocate(benchmark::State& st) {
+    while (st.KeepRunning()) {
+      alloc.deallocate(allocation);
+    }
+  }
+
+  void NoOpAllocDealloc(benchmark::State& st) {
+    size = static_cast<std::size_t>(st.range(0));
+    while (st.KeepRunning()) {
+      allocation = alloc.allocate(size);
+      alloc.deallocate(allocation);
+    }
+  }
+ 
+private: 
+  std::size_t size;
+  umpire::Allocator alloc;
+  void* allocation;
+};
+
+class NoOpResource : public NoOpAllocatorBenchmark {};
+BENCHMARK_DEFINE_F(NoOpResource, allocate)(benchmark::State& st) { NoOpAllocate(st); }
+BENCHMARK_DEFINE_F(NoOpResource, deallocate)(benchmark::State& st) { NoOpDeallocate(st); }
+BENCHMARK_DEFINE_F(NoOpResource, allocate_deallocate)(benchmark::State& st) { NoOpAllocDealloc(st); }
+
+BENCHMARK_REGISTER_F(NoOpResource, allocate)->Range(RangeLow, RangeHi);
+BENCHMARK_REGISTER_F(NoOpResource, deallocate)->Range(RangeLow, RangeHi);
+BENCHMARK_REGISTER_F(NoOpResource, allocate_deallocate)->Range(RangeLow, RangeHi);
 
 BENCHMARK_MAIN();
