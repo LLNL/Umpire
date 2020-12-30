23d483dfea40b39ce4ed4210ea959e3ec861b7f3
Kristi Belcher <belcher6@lassen190.coral.llnl.gov>
2020-12-30 11:45:41 -0800

    updating noop benchmark allocation ordering tests

--- benchmarks/allocator_no-op_benchmarks.cpp
+++ benchmarks/allocator_no-op_benchmarks.cpp
@@ -11,6 +11,7 @@
 
 static const int RangeLow{4};
 static const int RangeHi{1024};
+static const int Max_Allocations{10};
 
 class NoOpAllocatorBenchmark : public benchmark::Fixture
 {
@@ -28,22 +29,69 @@ public:
   void TearDown(benchmark::State&) {
   }
   
-  void NoOpAllocDealloc(benchmark::State& st) {
+  void NoOpSameOrder(benchmark::State& st) {
     size = static_cast<std::size_t>(st.range(0));
+    int i{0};
     while (st.KeepRunning()) {
-      allocation = alloc.allocate(size);
-      alloc.deallocate(allocation);
+      m_allocations[i++] = alloc.allocate(size);
+      if (i == Max_Allocations) {
+        for (int j{0}; j < i; j++) 
+          alloc.deallocate(m_allocations[j]);
+        i = 0;
+      }
+    }
+    for (int j{0}; j < i; j++) {
+      alloc.deallocate(m_allocations[j]);
+    }
+  }
+  void NoOpReverseOrder(benchmark::State& st) {
+    size = static_cast<std::size_t>(st.range(0));
+    int i{0};
+    while (st.KeepRunning()) {
+      m_allocations[i++] = alloc.allocate(size);
+      if (i == Max_Allocations) {
+        for (int j{i}; j < 0; j--) 
+          alloc.deallocate(m_allocations[j]);
+        i = 0;
+      }
+    }
+    for (int j{i};  j < 0; j--) {
+      alloc.deallocate(m_allocations[j]);
+    }
+  }
+  void NoOpShuffle(benchmark::State& st) {
+    size = static_cast<std::size_t>(st.range(0));
+    int i{0};
+    while (st.KeepRunning()) {
+      m_allocations[i++] = alloc.allocate(size);
+      if (i == Max_Allocations) {
+        for (int j{0}; j < (i/2); j++) {
+          alloc.deallocate(m_allocations[j]);
+          alloc.deallocate(m_allocations[(i/2)+j]);
+        }
+        i = 0;
+      }
+    }
+    //pseudo-random :)
+    for(int j{0}; j < (i/2); j++) {
+      alloc.deallocate(m_allocations[j]);
+      alloc.deallocate(m_allocations[(i/2)+j]);
     }
   }
  
 private: 
   std::size_t size;
   umpire::Allocator alloc;
-  void* allocation;
+  void* m_allocations[Max_Allocations];
 };
 
 class NoOpResource : public NoOpAllocatorBenchmark {};
-BENCHMARK_DEFINE_F(NoOpResource, allocate_deallocate)(benchmark::State& st) { NoOpAllocDealloc(st); }
-BENCHMARK_REGISTER_F(NoOpResource, allocate_deallocate)->Range(RangeLow, RangeHi);
+BENCHMARK_DEFINE_F(NoOpResource, same_order)(benchmark::State& st) { NoOpSameOrder(st); }
+BENCHMARK_DEFINE_F(NoOpResource, reverse_order)(benchmark::State& st) { NoOpReverseOrder(st); }
+BENCHMARK_DEFINE_F(NoOpResource, shuffle)(benchmark::State& st) { NoOpShuffle(st); }
+
+BENCHMARK_REGISTER_F(NoOpResource, same_order)->Range(RangeLow, RangeHi);
+BENCHMARK_REGISTER_F(NoOpResource, reverse_order)->Range(RangeLow, RangeHi);
+BENCHMARK_REGISTER_F(NoOpResource, shuffle)->Range(RangeLow, RangeHi);
 
 BENCHMARK_MAIN();
