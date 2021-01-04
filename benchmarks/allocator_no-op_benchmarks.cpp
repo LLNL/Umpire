23fcdffd53e1997f1e07baa6dd5fb986c7675170
Kristi Belcher <belcher6@lassen330.coral.llnl.gov>
2021-01-04 12:13:24 -0800

    adding a random-shuffle method

--- benchmarks/allocator_no-op_benchmarks.cpp
+++ benchmarks/allocator_no-op_benchmarks.cpp
@@ -8,6 +8,7 @@
 #include "umpire/config.hpp"
 #include "umpire/Allocator.hpp"
 #include "umpire/ResourceManager.hpp"
+#include "algorithm"
 
 static const int RangeLow{4};
 static const int RangeHi{1024};
@@ -61,7 +62,7 @@ public:
       alloc.deallocate(m_allocations[j]);
     }
   }
-  void NoOpShuffle(benchmark::State& st) {
+  void NoOpRandomOrder(benchmark::State& st) {
     size = static_cast<std::size_t>(st.range(0));
     int i{0};
     while (st.KeepRunning()) {
@@ -81,6 +82,25 @@ public:
     }
   }
  
+  void NoOpShuffle(benchmark::State& st) {
+    size = static_cast<std::size_t>(st.range(0));
+    int i{0};
+    while (st.KeepRunning()) {
+      m_allocations[i++] = alloc.allocate(size);
+      if (i == Max_Allocations) {
+        std::random_shuffle ( &m_allocations[0], &m_allocations[Max_Allocations]);
+        for (int j{0}; j < i; j++) {
+          alloc.deallocate(m_allocations[j]);
+        }
+        i = 0;
+      }
+    }
+    
+    for(int j{0}; j < i; j++) {
+      alloc.deallocate(m_allocations[j]);
+    }
+  }
+ 
 private: 
   std::size_t size;
   umpire::Allocator alloc;
@@ -90,10 +110,12 @@ private:
 class NoOpResource : public NoOpAllocatorBenchmark {};
 BENCHMARK_DEFINE_F(NoOpResource, same_order)(benchmark::State& st) { NoOpSameOrder(st); }
 BENCHMARK_DEFINE_F(NoOpResource, reverse_order)(benchmark::State& st) { NoOpReverseOrder(st); }
+BENCHMARK_DEFINE_F(NoOpResource, random_order)(benchmark::State& st) { NoOpRandomOrder(st); }
 BENCHMARK_DEFINE_F(NoOpResource, shuffle)(benchmark::State& st) { NoOpShuffle(st); }
 
 BENCHMARK_REGISTER_F(NoOpResource, same_order)->Range(RangeLow, RangeHi);
 BENCHMARK_REGISTER_F(NoOpResource, reverse_order)->Range(RangeLow, RangeHi);
+BENCHMARK_REGISTER_F(NoOpResource, random_order)->Range(RangeLow, RangeHi);
 BENCHMARK_REGISTER_F(NoOpResource, shuffle)->Range(RangeLow, RangeHi);
 
 BENCHMARK_MAIN();
