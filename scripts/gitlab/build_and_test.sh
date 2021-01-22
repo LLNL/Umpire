f865e18659c0410e2ec7fc00ce55f571e8c41408
Kristi Belcher <belcher6@lassen709.coral.llnl.gov>
2021-01-21 16:59:51 -0800

    potential fix so that no-op runs in CI; adding to Windows

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -143,7 +143,7 @@ then
     if [[ "${option}" != "--build-only" ]] && grep -q -i "ENABLE_DEVELOPER_BENCHMARKS.*ON" ${hostconfig_path}
     then
         date
-        ctest -C Benchmark -R no-op_stress_test
+        ctest -C Benchmark
         date
     fi
 
