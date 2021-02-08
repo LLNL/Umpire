86489e76729eb9bcf354ac7ad4c25d1307bc150b
Kristi Belcher <belcher6@llnl.gov>
2021-02-08 15:42:47 -0800

    update changelog and finalize new strategy test

--- tests/integration/strategy_tests.cpp
+++ tests/integration/strategy_tests.cpp
@@ -287,10 +287,11 @@ class ReleaseTest : public ::testing::Test {
     
     m_limiter_allocator = 
         new umpire::Allocator(rm.makeAllocator<umpire::strategy::SizeLimiter>(
-        limiter_name, rm.getAllocator("HOST"), m_max_pool_size * m_num_pools));
+        limiter_name, rm.getAllocator("HOST"), max_alloc_size * num_allocs + padding));
 
-    m_allocator = new umpire::Allocator(
-        rm.makeAllocator<T>(name, rm.getAllocator(limiter_name)));
+    m_allocator = 
+        new umpire::Allocator(rm.makeAllocator<T>(name, rm.getAllocator(limiter_name), 
+        max_alloc_size * num_allocs));
   }
 
   void TearDown() override
@@ -302,12 +303,19 @@ class ReleaseTest : public ::testing::Test {
     m_limiter_allocator = nullptr;
   }
 
+  ////////////////////////////////////////////////////////////////////////
+  //max_alloc_size: Biggest allocation size used in test.
+  //num_allocs: The number of total allocations 
+  //padding: Some strategies have built-in alignment handling which will
+  //  interfere will this test. Padding helps make sure we account for that.
+  ////////////////////////////////////////////////////////////////////////
+  static const int max_alloc_size = 1024;
+  static const int num_allocs = 8;
+  static const int padding = 64;
+
   umpire::Allocator* m_allocator;
   umpire::Allocator* m_limiter_allocator;
 
-  const static int m_max_pool_size = 1024;
-  const static int m_num_pools = 8;
-  void* test[m_num_pools];
 };
 
 template <>
@@ -317,17 +325,20 @@ void ReleaseTest<umpire::strategy::FixedPool>::SetUp()
   std::string name{"release_test_" + std::to_string(unique_strategy_id++)};
   std::string limiter_name{"limiter_" + std::to_string(unique_strategy_id++)};
   
+  static const int max_alloc_size = 1024;
+  static const int num_allocs = 8;
+
   m_limiter_allocator = 
         new umpire::Allocator(rm.makeAllocator<umpire::strategy::SizeLimiter>(
-        limiter_name, rm.getAllocator("HOST"), 1024 *8));
+        limiter_name, rm.getAllocator("HOST"), max_alloc_size * num_allocs));
   
   m_allocator =
       new umpire::Allocator(rm.makeAllocator<umpire::strategy::FixedPool>(
-          name, rm.getAllocator(limiter_name), 1024, 1));
+          name, rm.getAllocator(limiter_name), max_alloc_size, 1));
 }
 
 using ReleaseStrategies = ::testing::Types<
-  umpire::strategy::DynamicPool, umpire::strategy::DynamicPoolList,
+  umpire::strategy::DynamicPoolList,
   umpire::strategy::DynamicPoolMap, umpire::strategy::FixedPool, 
   umpire::strategy::QuickPool>;
 
@@ -335,17 +346,20 @@ TYPED_TEST_SUITE(ReleaseTest, ReleaseStrategies, );
 
 TYPED_TEST(ReleaseTest, ReleaseCheck)
 {
-  for (int i = 0; i < this->m_num_pools; i++) {
-    this->test[i] = this->m_allocator->allocate(this->m_max_pool_size);
+  void* test[this->num_allocs];
+
+  for (int i = 0; i < this->num_allocs; i++) {
+    test[i] = this->m_allocator->allocate(this->max_alloc_size);
   }
-  for (int i = 0; i < this->m_num_pools; i++) {
-    this->m_allocator->deallocate(this->test[i]);
+  for (int i = 0; i < this->num_allocs; i++) {
+    this->m_allocator->deallocate(test[i]);
   }
 
+  ASSERT_THROW(test[0] = this->m_limiter_allocator->allocate(this->max_alloc_size), umpire::util::Exception);
   this->m_allocator->release();
 
-  ASSERT_NO_THROW(this->test[0] = this->m_limiter_allocator->allocate(this->m_max_pool_size));
-  ASSERT_NO_THROW(this->m_limiter_allocator->deallocate(this->test[0])); 
+  ASSERT_NO_THROW(test[0] = this->m_limiter_allocator->allocate(this->max_alloc_size));
+  ASSERT_NO_THROW(this->m_limiter_allocator->deallocate(test[0])); 
 }
 
 TEST(DynamicPool, LimitedResource)
