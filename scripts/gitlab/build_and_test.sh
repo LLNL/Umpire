594e89a0159af0cb5cb1581143df6c55f7ff8c29
Adrien M. Bernede <bernede1@llnl.gov>
2021-02-09 17:45:32 -0800

    Replaces local xml to junit conversion with BLT one

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -112,7 +112,7 @@ then
     $cmake_exe \
       -C ${hostconfig_path} \
       ${project_dir}
-    if ! $cmake_exe --build . -j; then
+    if ! $cmake_exe --build . -j 12 ; then
       echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
       echo "Compilation failed, running make VERBOSE=1"
       echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -136,7 +136,7 @@ then
     cd ${build_dir}
 
     date
-    ctest --output-on-failure -T test 2>&1 | tee tests_output.txt
+    ctest --output-on-failure --no-compress-output -T test -VV -j 12 2>&1 | tee tests_output.txt
     date
 
     # If Developer benchmarks enabled, run the no-op benchmark and show output
@@ -155,20 +155,8 @@ then
 
     echo "Copying Testing xml reports for export"
     tree Testing
-    cp Testing/*/Test.xml ${project_dir}
-
-    # Convert CTest xml to JUnit (on toss3 only)
-    if [[ ${sys_type} == *toss_3* ]]; then
-        if [[ -n ${py_env_path} ]]; then
-            . ${py_env_path}/bin/activate
-
-            python3 ${project_dir}/scripts/gitlab/convert_to_junit.py \
-            ${project_dir}/Test.xml \
-            ${project_dir}/scripts/gitlab/junit.xslt > ${project_dir}/junit.xml
-        else
-            echo "ERROR: needs python env with lxml, please set PYTHON_ENVIRONMENT_PATH"
-        fi
-    fi
+    xsltproc -o junit.xml ../blt/tests/ctest-to-junit.xsl Testing/*/Test.xml ${project_dir}
+    mv junit.xml ${project_dir}/junit.xml
 
     if grep -q "Errors while running CTest" ./tests_output.txt
     then
