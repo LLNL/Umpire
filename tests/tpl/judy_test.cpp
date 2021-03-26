82f12942e47894b906203d9c90738629d6f82c0d
Marty McFadden <mcfadden8@llnl.gov>
2021-03-26 22:29:35 +0000

    updates

--- tests/tpl/judy_test.cpp
+++ tests/tpl/judy_test.cpp
@@ -4,6 +4,9 @@
 //
 // SPDX-License-Identifier: (MIT)
 //////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <cassert>
+#include <cstdint>
 
 #include "umpire/tpl/judy/judy.h"
 
@@ -14,16 +17,43 @@ namespace {
   static constexpr uint64_t bad_judy_base{ 0x0000200000070000 };
 }
 
+void addkey(Judy* array, uint64_t key)
+{
+  JudySlot* cell{ judy_cell(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
+  assert( cell != nullptr && "addkey: judy_cell returned NULL!" );
+  uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+  *val = key+1;    // Just make sure Val is non-zero
+}
+
+uint64_t getval(Judy* array, uint64_t key)
+{
+  JudySlot* cell{ judy_strt(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
+  assert( cell != nullptr && "getval: judy_cell returned NULL!" );
+
+  // confirm the key above was indeed found
+  uint64_t key_check{0};
+  judy_key(array, reinterpret_cast<unsigned char*>(&key_check), judy_max);
+  assert( key == key_check && "Key not found!");
+
+  uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+  assert(*val == key+1 && "getval: value is incorect!");
+
+  return *val;
+}
+
 int main(int, char**)
 {
   Judy* array{ judy_open(judy_max_levels, judy_depth) };
-  JudySlot* last;
-  uint64_t ptr;
+  uint64_t key;
 
-  for (ptr = bad_judy_base; ptr < bad_judy_base+0x37; ptr++) {
+  for (key = bad_judy_base; key < bad_judy_base+0x37; key++) {
+    addkey(array, key);
+  }
+
+  for (key = bad_judy_base; key < bad_judy_base+0x37; key++) {
+    getval(array, key);
   }
 
   judy_close(array);
   return 0;
 }
-
