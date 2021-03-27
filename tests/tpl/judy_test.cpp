6eead32bf82fce4d047190ee2fac0a6de7f5a5ec
Marty McFadden <mcfadden8@llnl.gov>
2021-03-27 06:56:54 -0700

    updates

--- tests/tpl/judy_test.cpp
+++ tests/tpl/judy_test.cpp
@@ -27,16 +27,17 @@ void addkey(Judy* array, uint64_t key)
 
 uint64_t getval(Judy* array, uint64_t key)
 {
-  JudySlot* cell{ judy_strt(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
+  // JudySlot* cell{ judy_strt(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
+  JudySlot* cell{ judy_slot(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
   assert( cell != nullptr && "getval: judy_cell returned NULL!" );
+
+  uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+  assert(*val == key+1 && "getval: value is incorect!");
 
   // confirm the key above was indeed found
   uint64_t key_check{0};
   judy_key(array, reinterpret_cast<unsigned char*>(&key_check), judy_max);
   assert( key == key_check && "Key not found!");
-
-  uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
-  assert(*val == key+1 && "getval: value is incorect!");
 
   return *val;
 }
@@ -46,11 +47,11 @@ int main(int, char**)
   Judy* array{ judy_open(judy_max_levels, judy_depth) };
   uint64_t key;
 
-  for (key = bad_judy_base; key < bad_judy_base+0x37; key++) {
+  for (key = bad_judy_base; key < bad_judy_base+0x10000; key++) {
     addkey(array, key);
   }
 
-  for (key = bad_judy_base; key < bad_judy_base+0x37; key++) {
+  for (key = bad_judy_base; key < bad_judy_base+0x10000; key++) {
     getval(array, key);
   }
 
