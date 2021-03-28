d8c50b74bf23421efd899632c2ae3bd6feb7a463
Marty McFadden <mcfadden8@llnl.gov>
2021-03-28 09:51:39 -0700

    Updates

--- tests/tpl/judy_test.cpp
+++ tests/tpl/judy_test.cpp
@@ -4,57 +4,96 @@
 //
 // SPDX-License-Identifier: (MIT)
 //////////////////////////////////////////////////////////////////////////////
-#include <iostream>
 #include <cassert>
 #include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 #include "umpire/tpl/judy/judy.h"
 
-namespace {
-  static constexpr unsigned int judy_depth{1}; // Judy: number of Integers in a key
-  static constexpr unsigned int judy_max_levels{sizeof(uintptr_t)}; // Judy: max height of stack
-  static constexpr unsigned int judy_max{judy_depth * JUDY_key_size}; // Judy: length of key in bytes
-  static constexpr uint64_t bad_judy_base{ 0x0000200000070000 };
-}
+struct JudyTest {
+  void addkey(uint64_t key)
+  {
+    // judy_cell:  insert a string into the judy array, return cell pointer.
+    JudySlot* cell{ judy_cell(array, reinterpret_cast<unsigned char*>(&key), key_length) };
 
-void addkey(Judy* array, uint64_t key)
-{
-  JudySlot* cell{ judy_cell(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
-  assert( cell != nullptr && "addkey: judy_cell returned NULL!" );
-  uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
-  *val = key+1;    // Just make sure Val is non-zero
-}
+    if ( cell == nullptr ) {
+      std::cout << std::setfill('0') << std::setw(16) << std::hex << key;
+    }
 
-uint64_t getval(Judy* array, uint64_t key)
-{
-  // JudySlot* cell{ judy_strt(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
-  JudySlot* cell{ judy_slot(array, reinterpret_cast<unsigned char*>(&key), judy_max) };
-  assert( cell != nullptr && "getval: judy_cell returned NULL!" );
+    assert( cell != nullptr && "addkey: judy_cell returned NULL!" );
+    uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+    *val = key+1;    // Just make up a non-zero value, doesn't matter what
+  }
 
-  uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
-  assert(*val == key+1 && "getval: value is incorect!");
+  void check_value_and_key(uint64_t key)
+  {
+    // judy_strt is called by umpire (doFindOrBefore).  For testing purposes, I
+    // am checking that I can get the value from both judy_strt and judy_slot.
+    // I have confirmed that I am able to obtain the value from both judy_strt
+    // and judy_slot below.  However, neither call seems to be setting up the
+    // judy-stack adequately to allow the key to be successfully obtained.
+    // Strange... This only happens when enough (consecutive) keys have been
+    // generated to cause all of the array nodes to be decomposed into a full
+    // radix tree (and only on rzansel!).  This is the reason for UM-851.
+    //
+    {
+      uint64_t k{ key };
 
-  // confirm the key above was indeed found
-  uint64_t key_check{0};
-  judy_key(array, reinterpret_cast<unsigned char*>(&key_check), judy_max);
-  assert( key == key_check && "Key not found!");
+      // judy_strt: retrieve the cell pointer greater than or equal to given key
+      JudySlot* cell{
+        judy_strt(array, reinterpret_cast<unsigned char*>(&k), key_length) };
+      assert(cell != nullptr && "cell from judy_strt NULL!");
 
-  return *val;
-}
+      // judy_key:   retrieve the string value for the most recent judy query.
+      uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+      assert(*val == key+1 && "value from judy_strt is wrong!"); // WILL FAIL
+    }
+
+    {
+      uint64_t k{ key };
+
+      // judy_slot: retrieve the cell pointer, or return NULL for a given key.
+      JudySlot* cell{
+        judy_slot(array, reinterpret_cast<unsigned char*>(&k), key_length) };
+      assert(cell != nullptr && "cell from judy_slot NULL!");
+
+      // judy_key:   retrieve the string value for the most recent judy query.
+      uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+      assert(*val == key+1 && "value from judy_slot is wrong!"); // WILL FAIL
+    }
+  }
+
+  //  judy_open:  open a new judy array returning a judy object.
+  JudyTest() : array{ judy_open(key_length, key_depth) }
+  {
+  }
+
+  ~JudyTest()
+  {
+    judy_close(array);
+  }
+
+  const unsigned int key_depth{1};
+  const unsigned int key_length{sizeof(uintptr_t)};
+  const uint64_t bad_judy_base{ 0x0000200000070000 };
+  Judy* array;
+};
 
 int main(int, char**)
 {
-  Judy* array{ judy_open(judy_max_levels, judy_depth) };
+  JudyTest judy;
   uint64_t key;
 
-  for (key = bad_judy_base; key < bad_judy_base+0x10000; key++) {
-    addkey(array, key);
+  for (key = judy.bad_judy_base; key < judy.bad_judy_base+0x10000; key++) {
+    judy.addkey(key);
   }
 
-  for (key = bad_judy_base; key < bad_judy_base+0x10000; key++) {
-    getval(array, key);
+  for (key = judy.bad_judy_base; key < judy.bad_judy_base+0x10000; key++) {
+    judy.check_value_and_key(key);
   }
 
-  judy_close(array);
   return 0;
 }
