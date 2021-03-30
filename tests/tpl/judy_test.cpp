bf0090b7773c1a28f916dd53ff0a3f39c61f2699
Marty McFadden <mcfadden8@llnl.gov>
2021-03-30 18:11:30 +0000

    Added tests

--- tests/tpl/judy_test.cpp
+++ tests/tpl/judy_test.cpp
@@ -4,108 +4,17 @@
 //
 // SPDX-License-Identifier: (MIT)
 //////////////////////////////////////////////////////////////////////////////
-#include <cassert>
+
+#include "gtest/gtest.h"
+
 #include <cstdint>
-#include <iomanip>
-#include <iostream>
 #include <sstream>
 #include <string>
 
 #include "umpire/tpl/judy/judy.h"
 
-struct JudyTest {
-  void write_key_value(const uint64_t key)
-  {
-    uint64_t k{ key };
-
-    // judy_cell:  insert a string into the judy array, return cell pointer.
-    JudySlot* cell{ judy_cell(array, reinterpret_cast<unsigned char*>(&k), key_length) };
-    assert( cell != nullptr && "write_key_value: judy_cell returned NULL!" );
-
-    uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
-    *val = key+1;    // Just make up a non-zero value, doesn't matter what
-
-    // perform check immediately, this should always work..
-    //
-    k = 0;
-    judy_key(array, reinterpret_cast<unsigned char*>(&k), key_length);
-    if ( k != key ) {
-      std::cout
-        << "Unexpected Key:"
-        << " Expected: " << hex_format(key) << ", "
-        << " Returned: " << hex_format(k) << ", "
-        << " Cell Location: " << hex_format(reinterpret_cast<uint64_t>(cell)) << ", "
-        << " Value: " << hex_format(key+1) << std::endl;
-    }
-    else {
-      std::cout
-        << "Key:" << hex_format(key) << ", "
-        << "Cell Location: " << hex_format(reinterpret_cast<uint64_t>(cell)) << ", "
-        << "Value: " << hex_format(key+1)
-        << std::endl;
-    }
-  }
-
-  void check_value_and_key(uint64_t key)
-  {
-    {
-      uint64_t k{ key };
-
-      // judy_strt: retrieve the cell pointer greater than or equal to given key
-      JudySlot* cell{ judy_strt(array, reinterpret_cast<unsigned char*>(&k), key_length) };
-      assert(cell != nullptr && "cell from judy_strt NULL!");
-
-      uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
-
-      if ( *val != key+1 ) {
-        std::cout
-          << "Unexpected Value from judy_strt:"
-          << " Cell Location: " << hex_format(reinterpret_cast<uint64_t>(cell)) << ", "
-          << " Expected Value: " << hex_format(key+1) << ", "
-          << " Returned Value: " << hex_format(*cell) << std::endl;
-      }
-
-      k = 0;
-      judy_key(array, reinterpret_cast<unsigned char*>(&k), key_length);
-
-      if ( k != key ) {
-        std::cout
-          << "Unexpected Key from judy_key after judy_strt:"
-          << " Expected: " << hex_format(key) << ", "
-          << " Returned: " << hex_format(k) << std::endl;
-      }
-    }
-
-    {
-      uint64_t k{ key };
-
-      // judy_slot: retrieve the cell pointer, or return NULL for a given key.
-      JudySlot* cell{
-        judy_slot(array, reinterpret_cast<unsigned char*>(&k), key_length) };
-      assert(cell != nullptr && "cell from judy_slot NULL!");
-
-      // judy_key:   retrieve the string value for the most recent judy query.
-      uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
-
-      if ( *val != key+1 ) {
-        std::cout
-          << "Unexpected Value from judy_slot:"
-          << " Cell Location: " << hex_format(reinterpret_cast<uint64_t>(cell)) << ", "
-          << " Expected Value: " << hex_format(key+1) << ", "
-          << " Returned Value: " << hex_format(*cell) << std::endl;
-      }
-
-      k = 0;
-      judy_key(array, reinterpret_cast<unsigned char*>(&k), key_length);
-      if ( k != key ) {
-        std::cout
-          << "Unexpected Key from judy_key after judy_slot:"
-          << " Expected: " << hex_format(key) << ", "
-          << " Returned: " << hex_format(k) << std::endl;
-      }
-    }
-  }
-
+class JudyTest : public ::testing::Test {
+ protected:
   //  judy_open:  open a new judy array returning a judy object.
   JudyTest() : array{ judy_open(key_length, key_depth) }
   {
@@ -116,31 +25,71 @@ struct JudyTest {
     judy_close(array);
   }
 
-  std::string hex_format(uint64_t n)
+  void TearDown() override
   {
-    std::stringstream ss;
-    ss << std::setfill('0') << std::setw(16) << std::hex << n;
-    return ss.str();
   }
 
   const unsigned int key_depth{1};
   const unsigned int key_length{sizeof(uintptr_t)};
   const uint64_t bad_judy_base{ 0x0000200000070000 };
+  const uint64_t max_keys{1000};
   Judy* array;
 };
 
-int main(int, char**)
+TEST_F(JudyTest, Construction)
 {
-  JudyTest judy;
-  uint64_t key;
+  EXPECT_TRUE(array != nullptr);
+}
 
-  for (key = judy.bad_judy_base; key < judy.bad_judy_base+57; key++) {
-    judy.write_key_value(key);
+TEST_F(JudyTest, WriteValues)
+{
+  EXPECT_TRUE(array != nullptr);
+
+  for (uint64_t key = bad_judy_base; key < bad_judy_base+max_keys; key++) {
+    uint64_t k{ key };
+
+    // judy_cell:  insert a string into the judy array, return cell pointer.
+    JudySlot* cell{ judy_cell(array, reinterpret_cast<unsigned char*>(&k), key_length) };
+    EXPECT_TRUE(cell != nullptr);
+
+    uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+    *val = key+1;    // Just make up a non-zero value, doesn't matter what
   }
 
-  for (key = judy.bad_judy_base; key < judy.bad_judy_base+57; key++) {
-    judy.check_value_and_key(key);
-  }
+  for (uint64_t key = bad_judy_base; key < bad_judy_base+max_keys; key++) {
+    {
+      uint64_t k{ key };
 
-  return 0;
+      // judy_strt: retrieve the cell pointer greater than or equal to given key
+      JudySlot* cell{ judy_strt(array, reinterpret_cast<unsigned char*>(&k), key_length) };
+      EXPECT_TRUE(cell != nullptr);
+
+      uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+
+      EXPECT_EQ(*val, key+1);
+
+      k = 0;
+      judy_key(array, reinterpret_cast<unsigned char*>(&k), key_length);
+
+      EXPECT_EQ(k, key );
+    }
+
+    {
+      uint64_t k{ key };
+
+      // judy_slot: retrieve the cell pointer, or return NULL for a given key.
+      JudySlot* cell{
+        judy_slot(array, reinterpret_cast<unsigned char*>(&k), key_length) };
+      EXPECT_TRUE(cell != nullptr);
+
+      // judy_key:   retrieve the string value for the most recent judy query.
+      uint64_t* val{ reinterpret_cast<uint64_t*>(cell) };
+
+      EXPECT_EQ(*val, key+1);
+
+      k = 0;
+      judy_key(array, reinterpret_cast<unsigned char*>(&k), key_length);
+      EXPECT_EQ(k, key );
+    }
+  }
 }
--- tests/unit/strategy/zero_byte_handler_tests.cpp
+++ tests/unit/strategy/zero_byte_handler_tests.cpp
@@ -7,50 +7,25 @@
 
 #include "gtest/gtest.h"
 
-#include <iostream>
 #include <vector>
 
 #include "umpire/ResourceManager.hpp"
 #include "umpire/Allocator.hpp"
 
-int main(int, char**)
-// TEST(ZeroByteHandlerTest, Construction)
+TEST(ZeroByteHandlerTest, Construction)
 {
-  std::size_t max_allocations{56};
+  std::size_t max_allocations{1000};
   auto& rm = umpire::ResourceManager::getInstance();
   auto alloc = rm.getAllocator("HOST");
   std::vector<void*> allocations;
 
   std::size_t i{0};
   for ( ; i < max_allocations; i++) {
-    allocations.push_back( alloc.allocate(0) );
+    ASSERT_NO_THROW(allocations.push_back(alloc.allocate(0)));
   }
 
-  if (i != max_allocations) {
-    std::cout << "Allocation #" << i << " failed" << std::endl;
-  }
-
-  if ( rm.hasAllocator(allocations[1]) != true ) {
-    std::cout << "OOPS, Allocation " << allocations[i] << " already missing" << std::endl;
-    return 1;
-  }
-
-  //
-  // This should cause a failure
-  //
-  allocations.push_back( alloc.allocate(0) );
-
-  if ( rm.hasAllocator(allocations[1]) != true ) {
-    std::cout << allocations[1] << " Not found" << std::endl;
-    return 2;
-  }
-  else {
-    std::cout << allocations[1] << " Hmm, Allocated and Found" << std::endl;
-  }
-
-#if 0
   for ( auto& a : allocations ) {
-    alloc.deallocate(a);
+    ASSERT_TRUE(rm.hasAllocator(a) == true);
+    ASSERT_NO_THROW(alloc.deallocate(a));
   }
-#endif
 }
