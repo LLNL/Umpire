7623d80e4d5da4b753377860156e51d0af8db676
Marty McFadden <mcfadden8@llnl.gov>
2021-04-06 14:55:13 -0700

    Added MPI tests for shared memory.

--- tests/unit/resource/shared_memory_resource_tests.cpp
+++ tests/unit/resource/shared_memory_resource_tests.cpp
@@ -5,13 +5,17 @@
 // SPDX-License-Identifier: (MIT)
 //////////////////////////////////////////////////////////////////////////////
 
+#include "umpire/config.hpp"
 #include "gtest/gtest.h"
+
+#include "mpi.h"
 
 #include <algorithm>
 #include <random>
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <utility>
 #include <vector>
 
 #include "umpire/Allocator.hpp"
@@ -22,12 +26,17 @@
 #include "umpire/resource/HostSharedMemoryResource.hpp"
 #include "umpire/util/MemoryResourceTraits.hpp"
 
-
 namespace {
-  bool initialized{false};
+  struct SharedMemoryState {
+    std::size_t largest_allocation_size;
+    std::size_t initial_size;
+    std::size_t num_allocations;
+    std::size_t allocation_sizes[1];
+  };
+  const std::string shmem_state_name{"SharedMemoryState"};
+  SharedMemoryState* shmem_state{nullptr};
+
   umpire::Allocator allocator;
-  std::size_t largest_allocation_size{0};
-  std::size_t initial_size{0};
   umpire::resource::HostSharedMemoryResource* shmem_resource{nullptr};
 
   class SharedMemoryTest : public ::testing::Test {
@@ -37,30 +46,67 @@ namespace {
     const std::size_t m_segment_size{  512ULL * 1024ULL * 1024ULL };
     const std::size_t m_max_allocs{ 1024 };
     const std::size_t m_max_elems{ m_segment_size / (m_max_allocs*sizeof(ArrayElement)) };
+    int m_rank;
 
     virtual void SetUp()
     {
-      if (!initialized) {
+      MPI_Comm_rank(MPI_COMM_WORLD, &m_rank);
+
+      if (shmem_resource == nullptr) {
         auto& rm = umpire::ResourceManager::getInstance();
         auto traits{umpire::get_default_resource_traits("SHARED")};
         ASSERT_EQ(traits.scope, umpire::MemoryResourceTraits::shared_scope::node);
         ASSERT_EQ(traits.resource, umpire::MemoryResourceTraits::resource_type::shared);
 
         traits.size = m_segment_size;
-        allocator = rm.makeResource("SHARED::node_allocator", traits);
+        ASSERT_NO_THROW( allocator = rm.makeResource("SHARED::node_allocator", traits); );
         auto base_strategy = umpire::util::unwrap_allocator<umpire::strategy::AllocationStrategy>(allocator);
         shmem_resource = dynamic_cast<umpire::resource::HostSharedMemoryResource*>(base_strategy);
-
-        initial_size = shmem_resource->getCurrentSize();
-        largest_allocation_size = find_largest_allocation_size();
-        initialized = true;
+        MPI_Barrier(MPI_COMM_WORLD);
       }
+
+      std::size_t state_size{ sizeof(SharedMemoryState) + (m_max_elems * sizeof(std::size_t)) };
+      ASSERT_NO_THROW( shmem_state = static_cast<SharedMemoryState*>(allocator.allocate(shmem_state_name, state_size)); );
+
+      if (m_rank == 0) {
+        shmem_state->initial_size = shmem_resource->getCurrentSize();
+        shmem_state->largest_allocation_size = find_largest_allocation_size();
+
+        std::random_device rd;
+        std::mt19937 gen{ rd() };
+        std::uniform_int_distribution<std::size_t> distrib(1, m_max_elems);
+
+        for ( std::size_t i = 0; i < m_max_allocs; ++i) {
+          shmem_state->allocation_sizes[i] = distrib(gen) * sizeof(ArrayElement);
+        }
+
+        std::vector<ArrayElement*> allocs{ alloc_until_full() };
+        shmem_state->num_allocations = allocs.size();
+        do_deallocations(allocs);
+      }
+
+      MPI_Barrier(MPI_COMM_WORLD);
+      ASSERT_EQ(shmem_resource->getCurrentSize(), shmem_state->initial_size);
+      MPI_Barrier(MPI_COMM_WORLD);
     }
 
     virtual void TearDown()
     {
-      ASSERT_EQ( initial_size, shmem_resource->getCurrentSize() );
-      ASSERT_EQ( largest_allocation_size, find_largest_allocation_size() );
+      MPI_Barrier(MPI_COMM_WORLD);
+      ASSERT_EQ( shmem_state->initial_size, shmem_resource->getCurrentSize() );
+
+      MPI_Barrier(MPI_COMM_WORLD);
+
+      if (m_rank == 0) {
+        ASSERT_EQ( shmem_state->largest_allocation_size, find_largest_allocation_size() );
+      }
+
+      MPI_Barrier(MPI_COMM_WORLD);
+
+      allocator.deallocate(shmem_state);
+      shmem_state = nullptr;
+
+      MPI_Barrier(MPI_COMM_WORLD);
     }
 
     std::size_t find_largest_allocation_size()
@@ -76,7 +122,7 @@ namespace {
       // insure that there are no problems within the internal accounting in the
       // implementation.
       //
-      std::size_t allocation_size{m_segment_size};
+      std::size_t allocation_size{m_segment_size - shmem_state->initial_size};
       void* ptr{nullptr};
 
       while (allocation_size != 0) {
@@ -95,116 +141,105 @@ namespace {
       return allocation_size;
     }
 
-    std::vector< std::pair< ArrayElement*, std::size_t > > allocs_until_full()
+    std::vector<ArrayElement*> alloc_until_full()
     {
-      std::random_device rd;
-      std::mt19937 gen{ rd() };
-      std::uniform_int_distribution<std::size_t> distrib(1, m_max_elems);
-      std::vector< std::pair<ArrayElement*, std::size_t> > allocs;
+      std::vector<ArrayElement*> allocs;
 
-      for ( std::size_t i = 0; i < m_max_allocs; ++i) {
-        try {
-          std::size_t num_elems{ distrib(gen) };
+      try {
+        for ( std::size_t i = 0; i < m_max_allocs; ++i) {
           std::stringstream name;
           name << "size_" << i;
-          void* ptr{ allocator.allocate(name.str(), num_elems * sizeof(ArrayElement)) };
-          allocs.push_back( std::make_pair(reinterpret_cast<int*>(ptr), num_elems) );
-        } catch (...) {
-          break;
-        }
+          void* ptr{ allocator.allocate(name.str(), shmem_state->allocation_sizes[i]) };
+          allocs.push_back( static_cast<ArrayElement*>(ptr) );
+        } 
+      } catch (...) {
+        ;
       }
 
       return allocs;
     }
 
-    void
-    do_deallocations(std::vector<std::pair<ArrayElement*, std::size_t>>& allocs)
+    void do_deallocations(std::vector<ArrayElement*>& allocs)
     {
       for ( auto& x : allocs ) {
-        ASSERT_NO_THROW( allocator.deallocate(x.first); );
+        ASSERT_NO_THROW( allocator.deallocate(x); );
       }
     }
 
   };
 
-  TEST_F(SharedMemoryTest, Construct)
+  TEST_F(SharedMemoryTest, UnitTests)
   {
-    SUCCEED();
-  }
-
-  TEST_F(SharedMemoryTest, AllocateTooMuch)
-  {
-    std::size_t allocation_size{m_segment_size+1};
-
-    ASSERT_THROW( allocator.allocate("AllocTooMuch", allocation_size), umpire::util::Exception);
-  }
-
-  TEST_F(SharedMemoryTest, AllocateLargest)
-  {
-    ASSERT_EQ( largest_allocation_size, find_largest_allocation_size() );
-    void* ptr{nullptr};
-
-    for ( int loop{0}; loop < 1000; loop++ ) {
-      ASSERT_NO_THROW( ptr = allocator.allocate("AllocLargest", largest_allocation_size); );
-      ASSERT_NE(ptr, nullptr);
-      ASSERT_NO_THROW( allocator.deallocate(ptr); );
-    }
-  }
-
-  TEST_F(SharedMemoryTest, MixedAllocationSizes)
-  {
-    // Do deallocations in same order
+    MPI_Barrier(MPI_COMM_WORLD);
     {
-      auto allocs = allocs_until_full();
-      ASSERT_EQ( allocs.size(), m_max_allocs );
-      ASSERT_GT(shmem_resource->getCurrentSize(), initial_size);
-
-      do_deallocations(allocs);
-      ASSERT_EQ( allocs.size(), m_max_allocs );
-      ASSERT_EQ(shmem_resource->getCurrentSize(), initial_size);
+      std::size_t allocation_size{m_segment_size+1};
+      ASSERT_THROW( allocator.allocate("AllocTooMuch", allocation_size), umpire::util::Exception);
     }
 
-    // Do deallocations in random order
+    MPI_Barrier(MPI_COMM_WORLD);
     {
-      auto allocs = allocs_until_full();
+      if (m_rank == 0) {
+        for ( int loop{0}; loop < 100; loop++ ) {
+          ASSERT_NO_THROW(
+            allocator.deallocate(allocator.allocate("AllocLargest",
+                                  shmem_state->largest_allocation_size)); );
+        }
+      }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    {
+      auto allocs = alloc_until_full();
+      int size;
+
       ASSERT_EQ( allocs.size(), m_max_allocs );
-      ASSERT_GT(shmem_resource->getCurrentSize(), initial_size);
+      ASSERT_GT(shmem_resource->getCurrentSize(), shmem_state->initial_size);
+
+      MPI_Comm_size(MPI_COMM_WORLD, &size);
+      for ( std::size_t i{0}; i < allocs.size(); i++ ) {
+        if ( i % size != m_rank )
+          continue;
+
+        ArrayElement* buffer{ allocs[i] };
+        std::size_t elems{ shmem_state->allocation_sizes[i] / sizeof(ArrayElement) };
+
+        buffer[0] = i+1;
+        buffer[elems-1] = i+1;
+      }
+
+      MPI_Barrier(MPI_COMM_WORLD);
+      for ( std::size_t i{0}; i < allocs.size(); i++ ) {
+        ArrayElement* buffer{ allocs[i] };
+        std::size_t elems{ shmem_state->allocation_sizes[i] / sizeof(ArrayElement) };
+
+        ASSERT_EQ( buffer[0], i+1 );
+        ASSERT_EQ( buffer[elems-1], i+1 );
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
 
       std::random_shuffle(allocs.begin(), allocs.end());
-
       do_deallocations(allocs);
-      ASSERT_EQ( allocs.size(), m_max_allocs );
-      ASSERT_EQ(shmem_resource->getCurrentSize(), initial_size);
+
+      MPI_Barrier(MPI_COMM_WORLD);
+      ASSERT_EQ(shmem_resource->getCurrentSize(), shmem_state->initial_size);
+
+      MPI_Barrier(MPI_COMM_WORLD);
     }
-  }
-
-  TEST_F(SharedMemoryTest, MixedAllocationSizesAndData)
-  {
-    auto allocs = allocs_until_full();
-    ArrayElement counter{1};
-
-    for ( auto& x : allocs ) {
-      ArrayElement* p{ reinterpret_cast<ArrayElement*>(x.first) };
-      std::size_t elems{x.second};
-      for (std::size_t i{0}; i < elems; ++i) {
-        p[i] = counter;
-      }
-      counter++;
-    }
-
-    // Now validate the data
-    counter = 1;
-    for ( auto& x : allocs ) {
-      ArrayElement* p{ reinterpret_cast<ArrayElement*>(x.first) };
-      std::size_t elems{x.second};
-      for (std::size_t j{0}; j < elems; ++j) {
-        ASSERT_EQ(p[j], counter);
-      }
-      counter++;
-    }
-
-    do_deallocations(allocs);
-    ASSERT_EQ( allocs.size(), m_max_allocs );
-    ASSERT_EQ(shmem_resource->getCurrentSize(), initial_size);
   }
 }
+
+int main(int argc, char * argv[])
+{
+  int result = 0;
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  MPI_Init(&argc, &argv);
+
+  result = RUN_ALL_TESTS();
+
+  MPI_Finalize();
+
+  return result;
+}
+
