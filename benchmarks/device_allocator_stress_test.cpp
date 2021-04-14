69c83d25bb83b59bc63d2e8aa96de47182c4db4d
Kristi Belcher <belcher6@llnl.gov>
2021-04-14 11:57:13 -0700

    adding more functions, better var names

--- benchmarks/device_allocator_stress_test.cpp
+++ benchmarks/device_allocator_stress_test.cpp
@@ -5,22 +5,21 @@
 // SPDX-License-Identifier: (MIT)
 //////////////////////////////////////////////////////////////////////////////
 #include <chrono>
+#include <assert.h>
 #include "umpire/ResourceManager.hpp"
 #include "umpire/DeviceAllocator.hpp"
 #include "umpire/Allocator.hpp"
 
 //constexpr int ALLOCATIONS {1<<10}; //testing over const number of allocations (1024)
 constexpr int N {1<<10};
-constexpr int NUM_THREADS {256};
+constexpr int THREADS_PER_BLOCK {256};
 
 __global__ void one_per_block(umpire::DeviceAllocator alloc, double** data_ptr)
 {
   if (threadIdx.x == 0) {
     double* data = static_cast<double*>(alloc.allocate(sizeof(double)));
-    //*data_ptr = data;
-
-    data[0] = 1024;
-    //*data = 1024;
+    *data_ptr = data;
+    *data = 1024;
   }
 }
 
@@ -30,7 +29,6 @@ __global__ void only_the_first(umpire::DeviceAllocator alloc, double** data_ptr)
   if (idx == 0) {
     double* data = static_cast<double*>(alloc.allocate(sizeof(double)));
     *data_ptr = data;
-
     data[0] = 512;
   }
 }
@@ -40,28 +38,40 @@ __global__ void each_one(umpire::DeviceAllocator alloc, double** data_ptr)
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < 1019) {
     double* data = static_cast<double*>(alloc.allocate(sizeof(double)));
-    //*data_ptr = data;
-
-    data[0] = 256;
+    *data_ptr = data;
+    *data = 256;
   }
 }
 
-//make threads_per_block evenly divisible by num_blocks - else throw error
-//rename variables for threads, n, etc
-//use functions!
+void event_timing(cudaEvent_t start, cudaEvent_t stop)
+{
+  float milliseconds {0};
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&milliseconds, start, stop);
+
+  std::cout << "Time: " << milliseconds << "ms" << std::endl;
+}
+
+static void CudaTest(const char *msg)
+{
+  cudaError_t e = cudaGetLastError();
+  cudaThreadSynchronize();
+  if (cudaSuccess != e) {
+    fprintf(stderr, "%s: %d\n", msg, e);
+    fprintf(stderr, "%s\n", cudaGetErrorString(e));
+    exit(-1);
+  }
+}
+
 int main(int, char**) {
   auto& rm = umpire::ResourceManager::getInstance();
   auto allocator = rm.getAllocator("UM");
   auto device_allocator = umpire::DeviceAllocator(allocator, N * sizeof(double));
 
-  //try using only stream, but double check sync
-  cudaStream_t stream1, stream2, stream3;
-  cudaStreamCreate(&stream1);
-  cudaStreamCreate(&stream2);
-  cudaStreamCreate(&stream3);
+  assert((N % THREADS_PER_BLOCK) != 0);
 
-  cudaError_t err;
-
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
   cudaEvent_t start, stop;
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
@@ -69,56 +79,33 @@ int main(int, char**) {
   double** ptr_to_data =
       static_cast<double**>(allocator.allocate(sizeof(double*)));
 
+  //Run kernel to allocate per first thread per block
   cudaEventRecord(start);
-  one_per_block<<<(N+NUM_THREADS-1)/NUM_THREADS, NUM_THREADS, 0, stream1>>>(device_allocator, ptr_to_data);
+  one_per_block<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK, 0, stream>>>(device_allocator, ptr_to_data);
   cudaEventRecord(stop);
 
-  err = cudaGetLastError();
-  if(err != cudaSuccess) {
-    std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
-  }
-
-  cudaEventSynchronize(stop);
-  float milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
-  
-  cudaStreamDestroy(stream1);
+  CudaTest("Error!");
+  event_timing(start, stop); 
   std::cout << "Retrieved value: " << (*ptr_to_data)[0] << std::endl;
-  std::cout << "Time: " << milliseconds << "ms" << std::endl;
 
+  //Run kernel to allocate with only thread 0
   cudaEventRecord(start);
-  only_the_first<<<(N+NUM_THREADS-1)/NUM_THREADS, NUM_THREADS, 0, stream2>>>(device_allocator, ptr_to_data);
+  only_the_first<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK, 0, stream>>>(device_allocator, ptr_to_data);
   cudaEventRecord(stop);
 
-  err = cudaGetLastError();
-  if(err != cudaSuccess) {
-    std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
-  }
-
-  cudaEventSynchronize(stop);
-  milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
-  
-  cudaStreamDestroy(stream2);
+  CudaTest("Error!");
+  event_timing(start, stop); 
   std::cout << "Retrieved value: " << (*ptr_to_data)[0] << std::endl;
-  std::cout << "Time: " << milliseconds << "ms" << std::endl;
 
+  //Run kernel to allocate per each thread
   cudaEventRecord(start);
-  each_one<<<(N+NUM_THREADS-1)/NUM_THREADS, NUM_THREADS, 0, stream3>>>(device_allocator, ptr_to_data);
+  each_one<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK, 0, stream>>>(device_allocator, ptr_to_data);
   cudaEventRecord(stop);
-
-  err = cudaGetLastError();
-  if(err != cudaSuccess) {
-    std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
-  }
-
-  cudaEventSynchronize(stop);
-  milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
   
-  cudaStreamDestroy(stream3);
+  CudaTest("Error!");
+  event_timing(start, stop); 
   std::cout << "Retrieved value: " << (*ptr_to_data)[0] << std::endl;
-  std::cout << "Time: " << milliseconds << "ms" << std::endl;
 
+  cudaStreamDestroy(stream);
   return 0;
 }
