14a6338fb478701a94d5f3c02040ca8574c2cdfe
David Beckingsale <davidbeckingsale@gmail.com>
2021-05-06 07:06:49 -0700

    Add cookbook example

---
+++ examples/cookbook/recipe_strategy_name.cpp
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+#include "umpire/Allocator.hpp"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/strategy/QuickPool.hpp"
+#include "umpire/util/Macros.hpp"
+
+int main(int, char**)
+{
+  auto& rm = umpire::ResourceManager::getInstance();
+  auto allocator = rm.getAllocator("HOST");
+
+  // _sphinx_tag_tut_strategy_name_start
+  //
+  auto pool = rm.makeAllocator<umpire::strategy::DynamicPool>(
+      "POOL", allocator);
+  std::cout << pool.getStrategyName() << std::endl;
+  // _sphinx_tag_tut_strategy_name_end
+
+  UMPIRE_USE_VAR(pool);
+
+  return 0;
+}
