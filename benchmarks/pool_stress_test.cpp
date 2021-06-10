9c6867cc658f02df2bbf98ac1f7dc0913f17ed3a
Kristi Belcher <belcher6@llnl.gov>
2021-06-10 13:02:29 -0700

    adding missing include

--- benchmarks/pool_stress_test.cpp
+++ benchmarks/pool_stress_test.cpp
@@ -6,6 +6,7 @@
 //////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <chrono>
+#include <map>
 #include <string>
 #include <random>
 #include <numeric>
