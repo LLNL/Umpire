9ab1d84563c37a45e9b4463758439fa0e8eb8aa4
Kristi Belcher <belcher6@llnl.gov>
2021-08-19 14:05:02 -0700

    adding an example use case (recipe) for getDeviceAllocator function

--- examples/cookbook/recipe_device_allocator.cpp
+++ examples/cookbook/recipe_device_allocator.cpp
@@ -13,6 +13,13 @@ const int THREADS_PER_BLOCK = 128;
 const int BLOCKS = 4;
 __device__ int* data{nullptr};
 
+/*
+ * Kernel that performs simple arithmetic and stores the
+ * result in the array of ints passed to the kernel.
+ *
+ * Note: Benign data race that writes to the device allocated
+ * array, data.
+ */
 __global__ void my_kernel(int* result)
 {
   unsigned int i{threadIdx.x+blockIdx.x*blockDim.x};
@@ -26,35 +33,66 @@ __global__ void my_kernel(int* result)
   result[i] = BLOCKS * data[blockIdx.x];
 }
 
+/*
+ * Function to check if array of ints matches up against expected
+ * results returned from kernel. 
+ *
+ * If error detected, returns true. Otherwise, returns false.
+ */
+bool checkIfErrorsDetected(int* results)
+{
+  for(int i = 0; i < BLOCKS; i++) {
+    for(int j = 0; j < THREADS_PER_BLOCK; j++) {
+      if (results[i*THREADS_PER_BLOCK+j] != i*BLOCKS) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+/*
+ * Function that performs the body of the test, then calls the
+ * checkIfErrorsDetected function to check results.
+ *
+ * Note: This function is called from main and does not have any
+ * record of a DeviceAllocator object. This is by design to show that
+ * a function which is called from main can then call a kernel to
+ * perform some test with a DeviceAllocator object. The kernel will 
+ * still have access to any DeviceAllocator object created in main,
+ * even if it is not explicitly passed to this intermediate function.
+ */
+void performTest(umpire::Allocator allocator)
+{
+  //create an array of integers which will be used by the gpu to do some simple arithmetic
+  int* results = static_cast<int*>(allocator.allocate(BLOCKS*THREADS_PER_BLOCK * sizeof(int)));
+  memset(results, 0, BLOCKS*THREADS_PER_BLOCK);
+
+  //Sync up the device and host side global pointers that keep track of DeviceAllocator objects
+  UMPIRE_SET_UP_DEVICE_ALLOCATOR_ARRAY();
+
+  //call kernel and sync
+  my_kernel<<<BLOCKS, THREADS_PER_BLOCK>>>(results);
+  cudaDeviceSynchronize();
+  
+  std::cout << "Checking results..." <<std::endl;
+  if (checkIfErrorsDetected(results)) {
+    std::cout << "There were errors detected!" << std::endl;
+  }
+  else {
+    std::cout << "SUCCESS!" << std::endl;
+  }
+}
+
 int main(int argc, char const* argv[])
 {
   auto& rm = umpire::ResourceManager::getInstance();
+
+  //create the allocators that will be used in this program
   auto allocator = rm.getAllocator("UM");
   auto device_allocator = rm.makeDeviceAllocator(allocator, BLOCKS*sizeof(int));
 
-  int* result = static_cast<int*>(allocator.allocate(BLOCKS*THREADS_PER_BLOCK * sizeof(int)));
-  memset(result, 0, BLOCKS*THREADS_PER_BLOCK);
-
-  UMPIRE_SET_UP_DEVICE_ALLOCATOR_ARRAY();
-  my_kernel<<<BLOCKS, THREADS_PER_BLOCK>>>(result);
-  cudaDeviceSynchronize();
-
-  std::cout << "Result: " <<std::endl;
-  for(int i = 0; i < THREADS_PER_BLOCK; i++)
-    for(int j = 0; j < BLOCKS; j++)
-      std::cout << result[i*BLOCKS+j] << " ";
-  std::cout << std::endl;
-  std::cout << std::endl;
-  for(int i = 0; i < BLOCKS; i++)
-    std::cout << result[i*THREADS_PER_BLOCK] << " ";
-  std::cout << std::endl;
-  std::cout << std::endl;
-  for(int i = 0; i < THREADS_PER_BLOCK; i++) {
-    for(int j = 0; j < BLOCKS; j++) {
-      if (result[i*BLOCKS+j] != j*BLOCKS)
-        std::cout << "ERROR detected!" << std::endl;
-    }
-  }
-
+  //call the function that performs the actual test
+  performTest(allocator);
   return 0;
 }
