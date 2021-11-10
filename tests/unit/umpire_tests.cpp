77593727048460c7e8975585564af9a99123ede8
Marty McFadden <mcfadden8@llnl.gov>
2021-11-10 11:41:09 -0800

    Remove process high watermark memory test
    For linux systems, the current process resident set size and its high
    watermark are obtained by examing "/proc/self/statm" and "VmHWM" in
    "/proc/self/status" respectively.
    
    For reasons that we do not understand, we are occastionally seeing high
    watermark values that are less than the resident set size.  Since these
    numbers appear to be unreliable on some platforms, the asserting test
    has been removed.

--- tests/unit/umpire_tests.cpp
+++ tests/unit/umpire_tests.cpp
@@ -10,12 +10,5 @@
 TEST(Umpire, ProcessorMemoryStatistics)
 {
   ASSERT_GE(umpire::get_process_memory_usage(), 0);
-
-  //
-  // Be careful with the following test.  The _hwm call must be called last when compared
-  // to the live amount of system memory in use
-  //
-  ASSERT_LE(umpire::get_process_memory_usage(), umpire::get_process_memory_usage_hwm());
-
   ASSERT_GE(umpire::get_device_memory_usage(0), 0);
 }
