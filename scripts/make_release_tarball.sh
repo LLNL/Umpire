d11bc7dd247df5371adef4277975b6d0858e0e1c
Marty McFadden <mcfadden8@users.noreply.github.com>
2022-01-04 12:24:59 -0800

    Update Copyright to 2022 (#688)

--- scripts/make_release_tarball.sh
+++ scripts/make_release_tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##############################################################################
-# Copyright (c) 2016-21, Lawrence Livermore National Security, LLC and Umpire
+# Copyright (c) 2016-22, Lawrence Livermore National Security, LLC and Umpire
 # project contributors. See the COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (MIT)
