b1ec1cf3e4de7c78bd2a8f0ece5b3b238fc40782
Kristi Belcher <belcher6@llnl.gov>
2022-01-27 14:15:41 -0800

    adding more to tests for DA

--- tests/integration/device_allocator.cpp
+++ tests/integration/device_allocator.cpp
@@ -21,11 +21,11 @@ __global__ void tester(double** data_ptr, const char* name)
 }
 
 class DeviceAllocator : public ::testing::TestWithParam<const char*> {
-  /*  void TearDown() override
-    {
-      ASSERT_NO_THROW(umpire::destroy_device_allocator());
-      ASSERT_EQ(umpire::UMPIRE_DEV_ALLOCS_h, nullptr);
-    }*/
+public:
+  static void TearDownTestSuite() {
+    ASSERT_NO_THROW(umpire::destroy_device_allocator());
+    ASSERT_EQ(umpire::UMPIRE_DEV_ALLOCS_h, nullptr);
+  }
 };
 
 TEST_P(DeviceAllocator, CreateAndAllocate)
@@ -53,39 +53,16 @@ TEST_P(DeviceAllocator, LaunchKernelTest)
   auto allocator = rm.getAllocator("UM");
 
   double** data_ptr = static_cast<double**>(allocator.allocate(sizeof(double*)));
-  tester<<<1, 16>>>(data_ptr, GetParam());
+
+  ASSERT_NO_THROW((tester<<<1, 16>>>(data_ptr, GetParam())));
   cudaDeviceSynchronize();
   ASSERT_EQ(*data_ptr[0], (double)(42 * 42));
+
+  auto my_da = umpire::get_device_allocator(GetParam());
+  ASSERT_NO_THROW(my_da.reset());
+  ASSERT_NO_THROW((tester<<<1, 16>>>(data_ptr, GetParam())));
 }
 
 const char* device_allocator_names[3] = {"da1", "da2", "da3"};
 
 INSTANTIATE_TEST_SUITE_P(DeviceAllocatorTests, DeviceAllocator, ::testing::ValuesIn(device_allocator_names));
-
-/*
-void launch_kernel(double** data_ptr, const char* name)
-{
-
-}
-*/
-/*
-  ASSERT_EQ(da1.getID(), -1);
-  ASSERT_EQ(da2.getID(), -2);
-  ASSERT_EQ(da3.getID(), -3);
-  //umpire::DeviceAllocator da2 = umpire::make_device_allocator(allocator, size, "da2");
-  //umpire::DeviceAllocator da3 = umpire::make_device_allocator(allocator, size, "da3");
-
-  for (int i = 0; i < 3; i++) {
-#if defined(UMPIRE_ENABLE_CUDA)
-    ASSERT_NO_THROW(umpire::UMPIRE_DEV_ALLOCS_h[i].reset());
-#elif defined(UMPIRE_ENABLE_HIP)
-    hipLaunchKernelGGL(tester, dim3(1), dim3(16), 0, 0, data_ptr, name);
-    hipDeviceSynchronize();
-    ASSERT_EQ(*data_ptr[0], (double)(42 * 42));
-#else
-    FAIL(); // If neither CUDA nor HIP is enabled, yet we are testing the DeviceAllocator, something is wrong!
-#endif
-  }
-
-}
-*/
