12baba08e2f44e80803b0a59b25151c3b3e750d2
David Beckingsale <davidbeckingsale@gmail.com>
2022-04-13 14:57:25 -0700

    Try again

--- .github/actions/abi-compat/entrypoint.sh
+++ .github/actions/abi-compat/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory /github/workspace/.radiuss-ci
 
 git submodule update --init --recursive
 
