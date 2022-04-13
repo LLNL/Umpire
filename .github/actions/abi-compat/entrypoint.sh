97a1ae45427419157821c88e03e93a3565145881
David Beckingsale <davidbeckingsale@gmail.com>
2022-04-13 15:01:19 -0700

    More directories

--- .github/actions/abi-compat/entrypoint.sh
+++ .github/actions/abi-compat/entrypoint.sh
@@ -2,6 +2,10 @@
 
 git config --global --add safe.directory /github/workspace
 git config --global --add safe.directory /github/workspace/.radiuss-ci
+git config --global --add safe.directory /github/workspace/blt
+git config --global --add safe.directory /github/workspace/scripts/radiuss-spack-configs
+git config --global --add safe.directory /github/workspace/scripts/uberenv
+git config --global --add safe.directory /github/workspace/src/tpl/umpire/camp
 
 git submodule update --init --recursive
 
