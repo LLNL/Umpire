364059b02d4e5fb6a9e8c35cab4d8a532ce9f458
David Beckingsale <davidbeckingsale@gmail.com>
2022-04-21 08:19:16 -0700

    Fix cpuDeviceId

--- tests/integration/operation_tests.cpp
+++ tests/integration/operation_tests.cpp
@@ -690,7 +690,11 @@ TYPED_TEST(AdviceTest, ReadMostly)
   auto m_advice_operation = op_registry.find("READ_MOSTLY", strategy, strategy);
 
   if (this->dest_allocator->getPlatform() == umpire::Platform::host) {
+#if defined(UMPIRE_ENABLE_CUDA)
     device = cudaCpuDeviceId;
+#elif defined(UMPIRE_ENABLE_HIP)
+    device = hipCpuDeviceId;
+#endif
   }
 
   ASSERT_NO_THROW({ m_advice_operation->apply(this->source_array, nullptr, device, this->m_size); });
@@ -706,7 +710,11 @@ TYPED_TEST(AdviceTest, PreferredLocation)
   auto m_advice_operation = op_registry.find("PREFERRED_LOCATION", strategy, strategy);
 
   if (this->dest_allocator->getPlatform() == umpire::Platform::host) {
+#if defined(UMPIRE_ENABLE_CUDA)
     device = cudaCpuDeviceId;
+#elif defined(UMPIRE_ENABLE_HIP)
+    device = hipCpuDeviceId;
+#endif
   }
 
   ASSERT_NO_THROW({ m_advice_operation->apply(this->source_array, nullptr, device, this->m_size); });
@@ -722,7 +730,11 @@ TYPED_TEST(AdviceTest, AccessedBy)
   auto m_advice_operation = op_registry.find("ACCESSED_BY", strategy, strategy);
 
   if (this->dest_allocator->getPlatform() == umpire::Platform::host) {
+#if defined(UMPIRE_ENABLE_CUDA)
     device = cudaCpuDeviceId;
+#elif defined(UMPIRE_ENABLE_HIP)
+    device = hipCpuDeviceId;
+#endif
   }
 
   ASSERT_NO_THROW({ m_advice_operation->apply(this->source_array, nullptr, device, this->m_size); });
