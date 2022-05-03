11a1a2c7960c96ed997a1d786c75cb5ae5a13091
Kristi Belcher <belcher6@llnl.gov>
2022-05-03 15:55:38 -0700

    is_device_allocator now returns false if no DeviceAllocator exists yet

--- src/umpire/device_allocator_helper.cpp
+++ src/umpire/device_allocator_helper.cpp
@@ -42,13 +42,23 @@ __host__ __device__ inline int convert_to_array_index(int neg_id)
 __host__ __device__ inline int get_index(const char* name)
 {
   int index{-1};
+
 #if !defined(__CUDA_ARCH__)
+  if (UMPIRE_DEV_ALLOCS_h == nullptr) {
+    UMPIRE_LOG(Warning, "No DeviceAllocators have been created yet.");
+    return index;   
+  }
+
   for (int i = 0; i < UMPIRE_TOTAL_DEV_ALLOCS; i++) {
     if (strcmp(UMPIRE_DEV_ALLOCS_h[i].getName(), name) == 0) {
       index = i;
     }
   }
 #else
+  if (UMPIRE_DEV_ALLOCS == nullptr) {
+    return index;   
+  }
+
   for (int i = 0; i < UMPIRE_TOTAL_DEV_ALLOCS; i++) {
     const char* temp = UMPIRE_DEV_ALLOCS[i].getName();
     int curr = 0;
