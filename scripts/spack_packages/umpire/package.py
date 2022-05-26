1fdde16e82f28d0a3317fed08b926b93294d59af
David Beckingsale <davidbeckingsale@gmail.com>
2022-05-26 15:48:23 -0700

    Depend on camp@main

--- scripts/spack_packages/umpire/package.py
+++ scripts/spack_packages/umpire/package.py
@@ -86,7 +86,7 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
                    when='cuda_arch={0}'.format(sm_))
 
     depends_on('camp@0.1.0', when='@main')
-    depends_on('camp@2022.03.0')
+    depends_on('camp@main')
 
     conflicts('+numa', when='@:0.3.2')
     conflicts('~c', when='+fortran', msg='Fortran API requires C API')
