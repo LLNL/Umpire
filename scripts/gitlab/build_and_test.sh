f2c86ac981df79a7b43247fddf7ef820deec4c2d
David Beckingsale <davidbeckingsale@gmail.com>
2022-05-27 14:56:06 -0700

    Update conditional

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -199,7 +199,7 @@ then
         echo "ERROR: install directory not found : ${install_dir}" && exit 1
     fi
 
-    if [[ ! ( grep -q -i "UMPIRE_ENABLE_HIP.*ON" ${hostconfig_path} || grep -q -i "UMPIRE_ENABLE_MPI.*ON" ${hostconfig_path} ) ]]
+    if [ ! ( grep -q -i "UMPIRE_ENABLE_HIP.*ON" ${hostconfig_path} || grep -q -i "UMPIRE_ENABLE_MPI.*ON" ${hostconfig_path} ) ]
         cd ${install_dir}/examples/umpire/using-with-cmake
         mkdir build && cd build
         if ! $cmake_exe -C ../host-config.cmake ..; then
