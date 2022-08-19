06c72cdc48433f5d2a4b19513002ed8a9e7175f6
Marty McFadden <mcfadden8@llnl.gov>
2022-08-19 13:37:16 -0700

    Added reproducer that will cause fork/MPI_Init/static initialization interaction.

---
+++ tests/debug/pinned_fork_repro.cpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-22, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#include <stdlib.h>
+#include "mpi.h"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/strategy/QuickPool.hpp"
+
+static auto& rm = umpire::ResourceManager::getInstance();
+static auto pool_alloc = rm.makeAllocator<umpire::strategy::QuickPool>("quickpool", rm.getAllocator("PINNED"));
+static void* dyn_pool_data{pool_alloc.allocate(512)};
+
+int main(int ac, char** av)
+{
+  MPI_Init(&ac, &av);
+  system("hostname");
+
+  auto& rm = umpire::ResourceManager::getInstance();
+  auto alloc = rm.getAllocator("quickpool");
+
+  char* my_array = static_cast<char*>(alloc.allocate(1024));
+
+  alloc.deallocate(my_array);
+  MPI_Finalize();
+}
