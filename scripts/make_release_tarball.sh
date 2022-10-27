93b1441aaa258c1dcd211a552b75cff6461a2a8a
Marty McFadden <mcfadden8@users.noreply.github.com>
2022-10-27 14:09:09 -0700

    Release 2022.10.0 (#791)
    ## New Features
    
    - New HIP Advise operations have been added for setting and unsetting of the
      `READ_MOSTLY`, `PREFERRED_LOCATION`, and `ACCESSED_BY` advice.
    
      For HIP versions >= 5, operations to set and unset `COARSE_GRAIN` has also been added.
    
    - `getCurrentSize` and `getTotalSize` methods were added to the `DeviceAllocator` API.
    
    - New event tracking has been added that can stream events to JSON (for replays)
      and SQLITE.
    
    - `UMAP` allocation resource has been added.
    
    ## Improvements
    
    - Using `try_get` for async device operations.
    
    ## Bug Fixes
    
    - Fixed build problem for Fortan builds by properly installing the Umpire module files.
    
    - Fixed build problem on certain configurations where the `std::filesystem` check was returning incorrect results.
    
    - Instead of throwing an exception, the `is_device_allocator` helper function
      now returns `false` if the `DeviceAllocator` object has not yet been instantiated.
    
    - Fixed `FixedMallocPool` on Windows, allowing the `QuickPool` allocator to be safely copied.
    
    ## Build Configuration Updates
    
    - Add `UMPIRE_DISABLE_ALLOCATIONMAP_DEBUG` (default is `OFF`) option that allows users to disable
      the AllocationMap from dumping all records when an allocation is not found in a debug build.
    
    - Using `UMPIRE_ENABLE_MPI` instead of `ENABLE_MPI` for Umpire-specific MPI capabilities
    
    - Using `UMPIRE_ENABLE_IPC_SHARED_MEMORY` instead of `ENABLE_IPC_SHARED_MEMORY` to indicate this is an Umpire-specific implementation feature.

--- scripts/make_release_tarball.sh
+++ scripts/make_release_tarball.sh
@@ -7,7 +7,7 @@
 ##############################################################################
 
 TAR_CMD=gtar
-VERSION=2022.03.1
+VERSION=2022.10.0
 
 git archive --prefix=umpire-${VERSION}/ -o umpire-${VERSION}.tar HEAD 2> /dev/null
 
