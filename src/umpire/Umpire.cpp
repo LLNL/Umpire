938a586fe2004013d69e734f4c3265458fd9b05e
Kristi Belcher <belcher6@llnl.gov>
2023-02-28 12:33:29 -0800

    adding error message in cmake

--- src/umpire/Umpire.cpp
+++ src/umpire/Umpire.cpp
@@ -206,10 +206,10 @@ umpire::MemoryResourceTraits get_default_resource_traits(const std::string& name
   return traits;
 }
 
-// Using unused attribute to silence warning from rocm compiler
 void* find_pointer_from_name(Allocator allocator, const std::string& name)
 {
   void* ptr{nullptr};
+  // Using unused attribute to silence warning from rocm compiler
   UMPIRE_USE_VAR(allocator);
 
 #if defined(UMPIRE_ENABLE_IPC_SHARED_MEMORY)
