91f619f528f3ff0d755ed01d7f6685658bef2343
David Beckingsale <davidbeckingsale@gmail.com>
2023-04-17 13:47:40 -0700

    Add SyncBeforeFree strategy

---
+++ src/umpire/strategy/SyncBeforeFree.cpp
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-23, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+
+#include "umpire/strategy/SyncBeforeFree.hpp"
+
+namespace umpire {
+namespace strategy {
+
+SyncBeforeFree::SyncBeforeFree(const std::string& name, int id, Allocator allocator, camp::resources::Resource r)
+      : AllocationStrategy{name, id, allocator.getAllocationStrategy(), "SyncBeforeFree"},
+      m_allocator{allocator.getAllocationStrategy()},
+      m_resource{r}
+      {}
+
+void* SyncBeforeFree::allocate(std::size_t bytes)
+{
+  return m_allocator->allocate_internal(bytes);
+}
+
+void SyncBeforeFree::deallocate(void* p, std::size_t size)
+{
+  m_resource.wait();
+  m_allocator->deallocate_internal(p, size);
+}
+
+Platform SyncBeforeFree::getPlatform() noexcept
+{
+  return m_allocator->getPlatform();
+}
+
+MemoryResourceTraits SyncBeforeFree::getTraits() const noexcept
+{
+  return m_allocator->getTraits();
+}
+
+
+}
+}
---
+++ src/umpire/strategy/SyncBeforeFree.hpp
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-23, Lawrence Livermore National Security, LLC and Umpire
+// project contributors. See the COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (MIT)
+//////////////////////////////////////////////////////////////////////////////
+#ifndef UMPIRE_SyncBeforeFree_HPP
+#define UMPIRE_SyncBeforeFree_HPP
+
+#include "umpire/strategy/AllocationStrategy.hpp"
+
+#include "camp/resource.hpp"
+
+namespace umpire {
+namespace strategy {
+
+class SyncBeforeFree : public AllocationStrategy {
+  public:
+  SyncBeforeFree(const std::string& name, int id, Allocator allocator, camp::resources::Resource r);
+
+  void* allocate(std::size_t bytes) override;
+  void deallocate(void* ptr, std::size_t size) override;
+
+  Platform getPlatform() noexcept override;
+
+  MemoryResourceTraits getTraits() const noexcept override;
+
+  private:
+  strategy::AllocationStrategy* m_allocator;
+  camp::resources::Resource m_resource;
+};
+
+} // end of namespace strategy
+} // end of namespace umpire
+
+#endif
