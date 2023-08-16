c9e421bd7c989fec0ddddd9cffa84843627da20b
asidhu0 <asidhu359@gmail.com>
2023-08-16 12:26:19 -0700

    removed unused variable

--- src/umpire/strategy/DynamicSizePool.hpp
+++ src/umpire/strategy/DynamicSizePool.hpp
@@ -404,12 +404,10 @@ class DynamicSizePool : private umpire::strategy::mixins::AlignedAllocation {
 
   std::size_t getReleasableSize() const
   {
-    std::size_t nblocks = 0;
     std::size_t nbytes = 0;
     for (struct Block *temp = freeBlocks; temp; temp = temp->next) {
       if (temp->size == temp->blockSize) {
         nbytes += temp->blockSize;
-        nblocks++;
       }
     }
     return nbytes;
