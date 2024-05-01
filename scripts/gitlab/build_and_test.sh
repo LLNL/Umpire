f372afc5131c7c44d79c664b8b0c826e5dc33508
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2024-05-01 15:14:18 +0200

    Point at branch with autopush fix, manually push dependencies to mirror

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -85,6 +85,8 @@ then
 
     ./scripts/uberenv/uberenv.py --spec="${spec}" ${prefix_opt}
 
+    ${prefix_opt}/spack/bin/spack -D ${prefix_opt}/spack_env buildcache push --only dependencies gitlab_ci
+
 fi
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "~~~~~ Dependencies Built"
