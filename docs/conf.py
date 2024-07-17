7561acf2162200748e8718a0557e411423c867e3
Adrien Bernede <51493078+adrienbernede@users.noreply.github.com>
2024-07-17 18:45:08 +0200

    No need to explicitly report status to GitHub anymore (#893)

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
+# These patterns also affect html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
