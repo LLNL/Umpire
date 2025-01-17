d36fd82a36a465717b907e0bc45462f03293b99a
David Beckingsale <davidbeckingsale@gmail.com>
2025-01-17 12:53:15 -0800

    Forward allocation names in the NamedAllocationStrategy (#928)

--- examples/cookbook/recipe_shared_memory.cpp
+++ examples/cookbook/recipe_shared_memory.cpp
@@ -16,6 +16,7 @@
 #include "umpire/Umpire.hpp"
 #include "umpire/config.hpp"
 #include "umpire/resource/HostSharedMemoryResource.hpp"
+#include "umpire/strategy/NamedAllocationStrategy.hpp"
 #include "umpire/util/MemoryResourceTraits.hpp"
 
 //
@@ -58,6 +59,9 @@ int main(int ac, char** av)
   //
   auto node_allocator{rm.makeResource("SHARED::node_allocator", traits)};
 
+  auto named_node_allocator{
+      rm.makeAllocator<umpire::strategy::NamedAllocationStrategy>("My Node Allocator", node_allocator)};
+
   //
   // Resource of this allocator is SHARED
   //
@@ -82,6 +86,7 @@ int main(int ac, char** av)
   // Allocate shared memory
   //
   void* ptr{node_allocator.allocate("allocation_name_2", sizeof(uint64_t))};
+  void* ptr2{named_node_allocator.allocate("allocation two", 1024)};
   uint64_t* data{static_cast<uint64_t*>(ptr)};
 
   if (shared_rank == foreman_rank)
@@ -98,6 +103,7 @@ int main(int ac, char** av)
   UMPIRE_ASSERT(*data == 0xDEADBEEF);
 
   node_allocator.deallocate(ptr);
+  named_node_allocator.deallocate(ptr2);
 
   if (use_mpi) {
     MPI_Finalize();
