905b666eda72910d9fedc25dc838b193bcdad298
Kristi Belcher <belcher6@llnl.gov>
2025-07-02 15:01:51 -0700

    fixing noexcept error

--- src/umpire/resource/FileMemoryResource.cpp
+++ src/umpire/resource/FileMemoryResource.cpp
@@ -147,20 +147,17 @@ bool FileMemoryResource::isPageable() noexcept
   int pageableMem = 0;
   int cdev = 0;
   cudaError_t err = cudaGetDevice(&cdev);
-  if (err != cudaSuccess) {
-    UMPIRE_ERROR(umpire::runtime_error,
-                 fmt::format("Error when trying to get CUDA Device: {}", cudaGetErrorString(err)));
+  if (err != cudaSuccess) { // since it is noexcept, can't use UMPIRE_ERROR
+    UMPIRE_LOG(Debug, "Error when trying to get CUDA Device:" << cudaGetErrorString(err));
   }
 
   // Device supports coherently accessing pageable memory
   // without calling cudaHostRegister on it
   err = cudaDeviceGetAttribute(&pageableMem, cudaDevAttrPageableMemoryAccess, cdev);
-  if (err != cudaSuccess) {
-    UMPIRE_ERROR(
-        runtime_error,
-        fmt::format("cudaDeviceGetAttribute(pageableMem = {}, cudaDevAttrPageableMemoryAccess = {}, cdev = "
-                    "{}) failed with error: {}",
-                    pageableMem, static_cast<int>(cudaDevAttrPageableMemoryAccess), cdev, cudaGetErrorString(err)));
+  if (err != cudaSuccess) { // since it is noexcept, can't use UMPIRE_ERROR
+    UMPIRE_LOG(Debug, "cudaDeviceGetAttribute(pageableMem = " << pageableMem << "cudaDevAttrPageableMemoryAccess = " 
+                      << static_cast<int>(cudaDevAttrPageableMemoryAccess) << "cdev = " << cdev
+                      << ", failed with error:" << cudaGetErrorString(err));
   }
   if (pageableMem)
     return true;
