30433c15454f019a338b541472690bf188433cda
Kristi <belcher6@llnl.gov>
2025-10-10 08:10:28 -0700

    Shared Memory Clarifications (#1006)
    Added documentation clarifications for using both shared memory allocators, added cmake messages about the default, and fixed naming shim example when both types are enabled.

--- examples/cookbook/recipe_naming_shim.cpp
+++ examples/cookbook/recipe_naming_shim.cpp
@@ -14,18 +14,27 @@
 #include "umpire/resource/HostSharedMemoryResource.hpp"
 #include "umpire/strategy/NamingShim.hpp"
 #include "umpire/util/MemoryResourceTraits.hpp"
+#include "umpire/util/shared_memory_helper.hpp"
 
 int main(int, char**)
 {
   auto& rm = umpire::ResourceManager::getInstance();
-  auto traits{umpire::get_default_resource_traits("SHARED")};
+  auto traits{umpire::get_default_resource_traits("SHARED::POSIX")};
   traits.size = 1 * 1024 * 1024;                                   // Maximum size of this Allocator
   traits.scope = umpire::MemoryResourceTraits::shared_scope::node; // default
-  auto node_allocator{rm.makeResource("SHARED::node_allocator", traits)};
+
+  auto node_allocator{rm.makeResource("SHARED::POSIX::ipc_node_allocator", traits)};
+
+  // Use the matchesSharedMemoryResource function to double check the type of shared memory
+  if (umpire::util::matchesSharedMemoryResource("SHARED::POSIX::ipc_node_allocator", "POSIX")) {
+    std::cout << "Confirmed this is an IPC Shared Memory allocator..." <<std::endl;
+  }
+
   auto shim{rm.makeAllocator<umpire::strategy::NamingShim>("shim", node_allocator)};
 
   void* ptr = shim.allocate(1024);
   std::cout << "Ptr = " << ptr << std::endl;
-  std::cout << "Total Memory Allocated: " << umpire::get_total_bytes_allocated() << std::endl;
   shim.deallocate(ptr);
+
+  return 0;
 }
