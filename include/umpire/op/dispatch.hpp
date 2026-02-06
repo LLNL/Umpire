45b61ab4b2106cf71c72644debb554d69092005c
Kristi Belcher <belcher6@llnl.gov>
2026-02-06 08:57:32 -0800

    removing unnecessary checks in dispatch.hpp

--- include/umpire/op/dispatch.hpp
+++ include/umpire/op/dispatch.hpp
@@ -209,11 +209,6 @@ struct op_caller {
       std::size_t length = get_arg<1>(args...);
       check_memset_bounds(src, src_record, length);
 #endif
-    } else if constexpr (std::is_same_v<Op<resource::host_platform>, device_memset<resource::host_platform>>) {
-#ifdef UMPIRE_ENABLE_BOUNDS_CHECKS
-      std::size_t length = get_arg<1>(args...);
-      check_memset_bounds(src, src_record, length);
-#endif
     }
 
     // Dispatch based on platform
@@ -232,11 +227,6 @@ struct op_caller {
     // Operation-specific handling
     if constexpr (std::is_same_v<Op<resource::host_platform>, memset<resource::host_platform>>) {
       // For memset, we expect args to be {value, size}
-#ifdef UMPIRE_ENABLE_BOUNDS_CHECKS
-      std::size_t length = get_arg<1>(args...);
-      check_memset_bounds(src, src_record, length);
-#endif
-    } else if constexpr (std::is_same_v<Op<resource::host_platform>, device_memset<resource::host_platform>>) {
 #ifdef UMPIRE_ENABLE_BOUNDS_CHECKS
       std::size_t length = get_arg<1>(args...);
       check_memset_bounds(src, src_record, length);
