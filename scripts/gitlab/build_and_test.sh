45826b6314019801b6c71abf48050d8aa01bc6bc
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2026-05-12 15:48:07 +0200

    Section information is printed in gray for reduced visual impact

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -97,9 +97,9 @@ section_start ()
     # Push section ID onto stack
     section_id_stack+=("${section_id}")
 
-    echo "${section_indent}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "${section_indent}~ TIME                      | TOTAL    | SECTION  "
-    echo "${section_indent}~ ${current_time} | ${total_elapsed_formatted} | ${section_title}"
+    echo "\033[90m${section_indent}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m"
+    echo "\033[90m${section_indent}~ TIME                      | TOTAL    | SECTION  \033[0m"
+    echo "\033[90m${section_indent}~ ${current_time} | ${total_elapsed_formatted} | ${section_title}\033[0m"
     echo -e "\e[0Ksection_start:${timestamp}:${section_id}[collapsed=${collapsed}]\r\e[0K${section_indent}~ ${section_title}"
 
     # Increase indentation for nested sections
@@ -131,9 +131,9 @@ section_end ()
     local section_elapsed=$((timestamp - section_start))
     local section_elapsed_formatted=$(date -d @${section_elapsed} -u +%H:%M:%S)
 
-    echo -e "\e[0Ksection_end:${timestamp}:${section_id}\r\e[0K"
-    echo "${section_indent}~ ${current_time} | ${total_elapsed_formatted} | ${section_elapsed_formatted}"
-    echo "${section_indent}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo -e "\e[0Ksection_end:${timestamp}:${section_id}\r\e[0K\033[0m"
+    echo "\033[90m${section_indent}~ ${current_time} | ${total_elapsed_formatted} | ${section_elapsed_formatted}\033[0m"
+    echo "\033[90m${section_indent}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
     # Clean up stored time
     unset section_start_times[${section_id}]
